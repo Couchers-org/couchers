f35ee7d5cad88109f99bc0d1d047e4eef984893d
Aapeli <git@aapelivuorinen.com>
2020-05-25 22:55:53 +1000

    Fix up boxes and remove team from index

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -60,6 +60,8 @@
                     <p class="content">Our platform is developer-led. We will build this right, make it scalable, and listen to the community to fix bugs</p>
                   </article>
                 </div>
+              </div>
+              <div class="tile">
                 <div class="tile is-parent">
                   <article class="tile is-child notification is-warning">
                     <p class="subtitle">Problem</p>
@@ -70,8 +72,7 @@
                     <p class="content">Institute public accountability: members will know who they can trust, the community can filter out detrimental people</p>
                   </article>
                 </div>
-              </div>
-		<div class="tile is-parent">
+                <div class="tile is-parent">
                   <article class="tile is-child notification is-danger">
                     <p class="subtitle">Problem</p>
                     <p><a class="title" href="/issues/reviews">Disfunctional reference system</a></p>
@@ -81,7 +82,6 @@
                     <p class="content">Anonymous reviews, destigmatise negative reviews, a new score that more reflects if people are trustworthy and good for the community</p>
                   </article>
                 </div>
-              <div class="tile">
                 <div class="tile is-parent">
                   <article class="tile is-child notification is-success">
                     <p class="subtitle">Problem</p>
@@ -100,172 +100,8 @@
 
       <section class="section">
         <div class="container">
-          <h2 class="title is-2">Who we are</h2>
+          <h2 class="title is-2">The team</h2>
           <p class="content">We aren't just a bunch of randos of the internet. We promise.</p>
-          <div class="columns">
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="/img/itsi-head.jpg" alt="Photo of Itsi with CouchSurfing friends">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="/img/itsi-head.jpg" alt="Headshot of Itsi">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Itsi Weinstock</p>
-                      <p class="subtitle is-6">Community Organizer</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Itsi is a pretty badass dude. <a href="https://github.com/itsiw">@itsiw</a> on GitHub.<br />
-                    Some fun info regarding CouchSurfing and travel...
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="/img/aapeli-head.jpg" alt="Photo of Aapeli with CouchSurfing friends">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="/img/aapeli-head.jpg" alt="Headshot of Aapeli">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Aapeli Vuorinen</p>
-                      <p class="subtitle is-6">Software Architect</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Aapeli is some rando. <a href="https://github.com/aapeliv">@aapeliv</a> on GitHub.<br />
-                    Lived in 4 countries, visited 30+... Most fun trip: ... Personal website links, etc. Twitter?
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="https://bulma.io/images/placeholders/480x480.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Whoe else?</p>
-                      <p class="subtitle is-6">Third wheel</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Some stuff about person 3. Some <a href="/">links</a> or <a href="/">@handles</a>.<br />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="/img/itsi-head.jpg" alt="Photo of Itsi with CouchSurfing friends">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="/img/itsi-head.jpg" alt="Headshot of Itsi">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Itsi Weinstock</p>
-                      <p class="subtitle is-6">Community Organizer</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Itsi is a pretty badass dude. <a href="https://github.com/itsiw">@itsiw</a> on GitHub.<br />
-                    Some fun info regarding CouchSurfing and travel...
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="/img/aapeli-head.jpg" alt="Photo of Aapeli with CouchSurfing friends">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="/img/aapeli-head.jpg" alt="Headshot of Aapeli">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Aapeli Vuorinen</p>
-                      <p class="subtitle is-6">Software Architect</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Aapeli is some rando. <a href="https://github.com/aapeliv">@aapeliv</a> on GitHub.<br />
-                    Lived in 4 countries, visited 30+... Most fun trip: ... Personal website links, etc. Twitter?
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column">
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image is-3by3 headshot">
-                    <img src="https://bulma.io/images/placeholders/480x480.png" alt="Placeholder image">
-                  </figure>
-                </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
-                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
-                      </figure>
-                    </div>
-                    <div class="media-content">
-                      <p class="title is-4">Whoe else?</p>
-                      <p class="subtitle is-6">Third wheel</p>
-                    </div>
-                  </div>
-
-                  <div class="content">
-                    Some stuff about person 3. Some <a href="/">links</a> or <a href="/">@handles</a>.<br />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
     </div>
