cce03e7e1b517730227addc4368d9f221e121961
Aapeli <git@aapelivuorinen.com>
2020-05-26 16:49:38 +1000

    Tweak title and subtitle sizes

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -5,7 +5,7 @@
         <div class="container">
           <div class="box">
             <h1 class="title is-1">$SiteName</h1>
-            <p class="subtitle">Like CouchSurfing, but better</p>
+            <p class="subtitle is-3">Like CouchSurfing, but better</p>
           </div>
         </div>
       </div>
@@ -14,7 +14,7 @@
       <section class="section">
         <div class="container">
           <h2 class="title is-2">$SiteName: help build the new online platform for couch-surfing</h2>
-          <h3 class="title is-3">Non-profit. Community-focussed. Well built.</h3>
+          <p class="subtitle is-4">Non-profit. Community-focussed. Well built.</p>
           <p class="content">We are building a new platform that takes all the best features from existing platforms while tackling their main issues. Free for everyone to use forever. Modern, useful, and evolving with the community's needs. We will need your expertise and ideas to bring this project to the rest of the community.</p>
           <p><a class="button is-primary" href = "/signup">Sign up for $SiteName</a></p>
         </div>
