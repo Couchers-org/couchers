856d0042e3d5eb333741786ccd3050458d341a74
Aapeli <git@aapelivuorinen.com>
2020-05-27 18:07:46 +1000

    Tabs to spaces

--- site/pages/signup.vue
+++ site/pages/signup.vue
@@ -11,119 +11,119 @@
     </section>
 
     <section class="section">
-			<div class="field">
-			  <label class="label">Name</label>
-			  <div class="control">
-			    <input class="input" type="text" placeholder="Your name">
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Email</label>
-			  <div class="control">
-			    <input class="input" type="email" placeholder="Email address">
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Would you like to help in building Couchers.org?</label>
-			  <div class="control">
-	        <label class="radio">
-	          <input type="radio" name="contribute" value="yes">
-	          <label for="male">Yes, I'd like to contribute</label><br>
-					  <input type="radio" name="contribute" value="no">
-					  <label for="female">No, just let me know when you've got something ready to use</label><br>
-	        </label>
-			  </div>
-			</div>
-			<div class="field">
-			  <div class="label">
-			    <label class="label">Would you like to participate in possible future questionaires about what is important to you about the platform and community?</label>
-			  </div>
-			  <div class="field">
-			    <div class="field">
-			      <div class="control">
-			        <label class="radio">
-			          <input type="radio" name="participate" value="yes">
-			          Yes
-			        </label>
-			        <label class="radio">
-			          <input type="radio" name="participate" value="no">
-			          No
-			        </label>
-			      </div>
-			    </div>
-			  </div>
-			</div>
-			<div class="control">
-			  <button class="button is-primary">Submit</button>
-			</div>
-		</section>
-		<section class="section">
-			<div class="field">
-			  <label class="label">Age</label>
-			  <div class="control">
-			    <input class="input" type="text" placeholder="Your age">
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Gender</label>
-			  <div class="control">
-			    <div class="select">
-			      <select>
-			        <option>Male</option>
-			        <option>Female</option>
-			        <option>Genderqueer/Nonbinary</option>
-			        <option>Gender not listed here</option>
-			      </select>
-			    </div>
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Country of birth</label>
-			  <div class="control">
-			    <input class="input" type="text" placeholder="Country of birth">
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Country of residence</label>
-			  <div class="control">
-			    <input class="input" type="text" placeholder="Country of residence">
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">If you have technical expertise relevant to building this platform, what is your expertise and experience?</label>
-			  <div class="control">
-			    <textarea class="textarea" placeholder="Textarea"></textarea>
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">If you have non-profit or business experience that could help with growing this platform, what is your experience?</label>
-			  <div class="control">
-			    <textarea class="textarea" placeholder="Textarea"></textarea>
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Briefly describe your experience as a couch-surfer</label>
-			  <p>How many times you've surfed, hosted, used other features of apps. Have you been part of communities? Anything else you'd like to tell us.</p>
-			  <div class="control">
-			    <textarea class="textarea" placeholder="Textarea"></textarea>
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Please describe any problems you've had or seen with other platforms such as CouchSurfing</label>
-			  <div class="control">
-			    <textarea class="textarea" placeholder="Textarea"></textarea>
-			  </div>
-			</div>
-			<div class="field">
-			  <label class="label">Please describe any features you'd like to see in a new platform</label>
-			  <p>Or share any ideas you have that would improve the couch-surfing experience.</p>
-			  <div class="control">
-			    <textarea class="textarea" placeholder="Textarea"></textarea>
-			  </div>
-			</div>
-			<div class="control">
-			  <button class="button is-primary">Submit</button>
-			</div>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Your name">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" type="email" placeholder="Email address">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Would you like to help in building Couchers.org?</label>
+        <div class="control">
+          <label class="radio">
+            <input type="radio" name="contribute" value="yes">
+            <label for="male">Yes, I'd like to contribute</label><br>
+            <input type="radio" name="contribute" value="no">
+            <label for="female">No, just let me know when you've got something ready to use</label><br>
+          </label>
+        </div>
+      </div>
+      <div class="field">
+        <div class="label">
+          <label class="label">Would you like to participate in possible future questionaires about what is important to you about the platform and community?</label>
+        </div>
+        <div class="field">
+          <div class="field">
+            <div class="control">
+              <label class="radio">
+                <input type="radio" name="participate" value="yes">
+                Yes
+              </label>
+              <label class="radio">
+                <input type="radio" name="participate" value="no">
+                No
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="control">
+        <button class="button is-primary">Submit</button>
+      </div>
+    </section>
+    <section class="section">
+      <div class="field">
+        <label class="label">Age</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Your age">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Gender</label>
+        <div class="control">
+          <div class="select">
+            <select>
+              <option>Male</option>
+              <option>Female</option>
+              <option>Genderqueer/Nonbinary</option>
+              <option>Gender not listed here</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Country of birth</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Country of birth">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Country of residence</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Country of residence">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">If you have technical expertise relevant to building this platform, what is your expertise and experience?</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">If you have non-profit or business experience that could help with growing this platform, what is your experience?</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Briefly describe your experience as a couch-surfer</label>
+        <p>How many times you've surfed, hosted, used other features of apps. Have you been part of communities? Anything else you'd like to tell us.</p>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Please describe any problems you've had or seen with other platforms such as CouchSurfing</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Please describe any features you'd like to see in a new platform</label>
+        <p>Or share any ideas you have that would improve the couch-surfing experience.</p>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+      <div class="control">
+        <button class="button is-primary">Submit</button>
+      </div>
     </section>
 
     <section class="section">
