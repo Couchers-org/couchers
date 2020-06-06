535ec78a23c6e910c6ff18b9a0fe49d63fe61625
Aapeli <git@aapelivuorinen.com>
2020-06-06 21:45:02 +1000

    User grpc-web, not text encoding

--- generate_protos.sh
+++ generate_protos.sh
@@ -4,6 +4,6 @@ find pb -name '*.proto' | protoc -I. \
   --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
   \
   --js_out="import_style=commonjs,binary:frontend/src" \
-  --grpc-web_out="import_style=typescript,mode=grpcwebtext:frontend/src" \
+  --grpc-web_out="import_style=typescript,mode=grpcweb:frontend/src" \
   \
   $(xargs)
