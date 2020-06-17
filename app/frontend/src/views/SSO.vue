44b781d5df111ee12edaa5eca4a8f56473de81d4
Aapeli <git@aapelivuorinen.com>
2020-06-18 09:10:39 +1000

    Do SSO frontend

--- app/frontend/src/views/SSO.vue
+++ app/frontend/src/views/SSO.vue
@@ -1,8 +1,15 @@
 <template>
   <v-content>
     <v-container fill-height>
-      Hi
-      <v-btn v-on:click="doSSO">Do SSO</v-btn>
+      <v-col class="mx-auto" cols="12" sm="10" md="8" lg="6" xl="4">
+        <v-alert v-for="error in errorMessages" type="error" :key="error">
+          {{ error }}
+        </v-alert>
+        <div v-if="loading">
+          <p class="subtitle-1 text-center">Loading...</p>
+          <v-progress-linear indeterminate color="primary"></v-progress-linear>
+        </div>
+      </v-col>
     </v-container>
   </v-content>
 </template>
@@ -19,8 +26,18 @@ import Router from '../router'
 
 export default Vue.extend({
   data: () => ({
-    //
+    loading: true,
+    errorMessages: []
   }),
+
+  created () {
+    if (Store.getters.authenticated) {
+      this.doSSO()
+    } else {
+      this.loading = false
+      this.errorMessages = ['Please log in.']
+    }
+  },
 
   methods: {
     doSSO() {
@@ -30,9 +47,11 @@ export default Vue.extend({
       req.setSig(this.$route.query.sig)
 
       client.sSO(req, null).then(res => {
-        console.log(res.getRedirectUrl())
-        console.log(res)
-      }).catch(console.error)
+        window.location.href = res.getRedirectUrl()
+      }).catch(err => {
+        this.loading = false
+        this.errorMessages = [err.message]
+      })
     },
   },
 })
