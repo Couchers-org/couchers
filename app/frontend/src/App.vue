40614e00a83b286e6ee21ba32c6042cefc3f3be6
Aapeli <git@aapelivuorinen.com>
2020-06-20 06:30:27 +1000

    Clip nav drawer to under title bar and add button to show and hide it

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -5,13 +5,14 @@
       <span>This is a development version. <b>Please</b> report <b>any</b> bugs and glitches you notice to us at <a href="mailto:bugs@couchers.org">bugs@couchers.org</a>, or message us on the <a href="https://community.couchers.org/u/aapeli">forum</a>.</span>
     </v-system-bar>
 
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="primary" dark clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         Couchers.org
       </div>
     </v-app-bar>
 
-    <v-navigation-drawer app permanent>
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list-item>
         <v-list-item-avatar>
           <v-avatar :color="color" />
@@ -135,7 +136,7 @@ export default Vue.extend({
   name: 'App',
 
   data: () => ({
-    //
+    drawer: null
   }),
 
   created () {
