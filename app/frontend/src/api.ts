03d7834fd3501af132a006229ed7a1789490f0f8
Aapeli <git@aapelivuorinen.com>
2020-06-20 08:17:52 +1000

    Clean up API URL

--- app/frontend/src/api.ts
+++ app/frontend/src/api.ts
@@ -1,3 +1,5 @@
+const URL = "http://localhost:8888"
+
 import { AuthClient } from './pb/AuthServiceClientPb'
 import { APIClient } from './pb/ApiServiceClientPb'
 
@@ -11,6 +13,6 @@ const opts = {
 // There seems to be an error in the `opts` parameter's type, so have to ignore that line.
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-export const client = new APIClient('http://localhost:8888', null, opts)
+export const client = new APIClient(URL, null, opts)
 
-export const authClient = new AuthClient("http://127.0.0.1:8888")
+export const authClient = new AuthClient(URL)
