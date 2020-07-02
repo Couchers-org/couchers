afa41a26d571cb1f827b2ddec21ab2510c6c6384
Aapeli <git@aapelivuorinen.com>
2020-07-02 17:59:02 +1000

    Add dummy search box

---
+++ app/frontend/src/components/SearchBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-text-field v-model="searchText" flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search"></v-text-field>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    searchText: ""
+  }),
+
+  methods: {
+    search() {
+      this.$emit('search', this.searchText)
+    }
+  }
+})
+</script>
