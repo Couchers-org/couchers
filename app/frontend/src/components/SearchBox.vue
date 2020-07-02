92e231999fe735e24af693cf32dabb986fadd8d5
Aapeli <git@aapelivuorinen.com>
2020-07-02 18:07:58 +1000

    Add dummy search page

--- app/frontend/src/components/SearchBox.vue
+++ app/frontend/src/components/SearchBox.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-text-field v-model="searchText" flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search"></v-text-field>
+  <v-text-field v-model="query" flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search" v-on:keyup.enter="search"></v-text-field>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import Router from '../router'
+
 export default Vue.extend({
   data: () => ({
-    searchText: ""
+    query: ""
   }),
 
   methods: {
     search() {
-      this.$emit('search', this.searchText)
+      Router.push({ name: 'Search', query: { q: this.query } })
     }
   }
 })
---
+++ app/frontend/src/views/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-content>
+      You searched for "{{ query }}"
+  </v-content>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    //
+  }),
+
+  computed: {
+    query: function () {
+      return this.$route.query.q
+    },
+  },
+
+  methods: {
+  },
+})
+</script>
