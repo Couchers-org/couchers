4cbcf9dc531f37f4e9a11eef72bdf15be87cecc2
Christian Häggström <kalvdans@gmail.com>
2020-07-09 00:25:10 +0200

    Skip Python output as they are autogenerated anyway

--- app/generate_protos.sh
+++ app/generate_protos.sh
@@ -1,8 +1,4 @@
 find pb -name '*.proto' | protoc -I. \
-  --python_out=backend/src \
-  --grpc_python_out=backend/src \
-  --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
-  \
   --js_out="import_style=commonjs,binary:frontend/src" \
   --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
   \
