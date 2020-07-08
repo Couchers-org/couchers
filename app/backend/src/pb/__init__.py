059fe35f359e30a62faa7bb85387c8915a064c22
Christian Häggström <kalvdans@gmail.com>
2020-07-09 00:40:47 +0200

    Convert to use pathlib

--- app/backend/src/pb/__init__.py
+++ app/backend/src/pb/__init__.py
@@ -1,17 +1,16 @@
-from os.path import dirname, join, abspath
+from pathlib import Path
 import os
-from glob import glob
 from grpc_tools import protoc
 import pkg_resources
 
-this_dir = dirname(__file__)
-backend_src = join(this_dir, "..")
-app_dir = join(backend_src, "..", "..")
+this_dir = Path(__file__).parent
+backend_src = this_dir / ".."
+app_dir = backend_src / ".." / ".."
 
-protoc_args = (["dummy_argv0", "-I" + app_dir,
+protoc_args = (["dummy_argv0", "-I" + str(app_dir),
                 "-I" + pkg_resources.resource_filename('grpc_tools', '_proto'),
-                "--python_out", backend_src,
-                "--grpc_python_out", backend_src] +
-               glob(join(app_dir, "pb", "*.proto")))
+                "--python_out", str(backend_src),
+                "--grpc_python_out", str(backend_src)] +
+               [str(x) for x in (app_dir / "pb").glob("*.proto")])
 
 protoc.main(protoc_args)
