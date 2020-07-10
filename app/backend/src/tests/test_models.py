7e319479dbde42f1dab8ad697d9008b3f455bf03
Lucas <lucaslevin@gmail.com>
2020-07-10 16:31:24 +0100

    Fuzz joined and last_active for display

--- app/backend/src/tests/test_models.py
+++ app/backend/src/tests/test_models.py
@@ -1,5 +1,5 @@
 from unittest.mock import patch
-from datetime import date
+from datetime import date, datetime
 
 from couchers.models import User
 
@@ -9,8 +9,28 @@ class FakeDate(date):
         return date.__new__(date, *args, **kwargs)
 
 @patch("couchers.models.date", FakeDate)
-def test_user_birthdate():
+def test_user_age():
     FakeDate.today = classmethod(lambda cls: date(2019, 7, 5))
     assert User(birthdate = date(1990, 7, 4)).age == 29
     assert User(birthdate = date(1990, 7, 31)).age == 28
     assert User(birthdate = date(1992, 2, 29)).age == 27
+
+def test_user_display_joined():
+    assert (User(joined=datetime(2020, 7, 10, 16, 34, 1, 1))
+            .display_joined == datetime(2020, 7, 10, 16, 0, 1, 1))
+    assert (User(joined=datetime(2025, 7, 10, 16, 59, 1, 1))
+            .display_joined == datetime(2025, 7, 10, 16, 0, 1, 1))
+    assert (User(joined=datetime(2020, 7, 10, 16, 0, 1, 1))
+            .display_joined == datetime(2020, 7, 10, 16, 0, 1, 1))
+    assert (User(joined=datetime(2020, 7, 10, 0, 0, 0, 0))
+            .display_joined == datetime(2020, 7, 10, 0, 0, 0, 0))
+
+def test_user_display_last_active():
+    assert (User(last_active=datetime(2020, 7, 10, 16, 34, 1, 1))
+            .display_last_active == datetime(2020, 7, 10, 16, 30, 1, 1))
+    assert (User(last_active=datetime(2025, 7, 10, 16, 59, 1, 1))
+            .display_last_active == datetime(2025, 7, 10, 16, 45, 1, 1))
+    assert (User(last_active=datetime(2020, 7, 10, 16, 16, 1, 1))
+            .display_last_active == datetime(2020, 7, 10, 16, 15, 1, 1))
+    assert (User(last_active=datetime(2020, 7, 10, 0, 0, 0, 0))
+            .display_last_active == datetime(2020, 7, 10, 0, 0, 0, 0))
