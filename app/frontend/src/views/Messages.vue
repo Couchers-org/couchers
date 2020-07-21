a606af9436a784d820d16336f1430d170d81e4be
Aapeli <git@aapelivuorinen.com>
2020-07-21 13:16:12 +1000

    Clean up

--- app/frontend/src/views/Messages.vue
+++ app/frontend/src/views/Messages.vue
@@ -2,54 +2,63 @@
   <v-main>
     <v-container fluid>
       DEBUG your user ID: <v-text-field v-model="myUserId"></v-text-field>
-      <v-card>
-        <v-card max-width="450" tile>
-          <v-subheader
-            ><v-text-field
-              class="mx-3 my-1"
-              v-model="searchQuery"
-              flat
-              single-line
-              hide-details
-              label="Search"
-              v-on:keyup.enter="search"
-            ></v-text-field
-          ></v-subheader>
-          <v-list v-if="loading != 0" three-line>
-            <v-subheader>Loading...</v-subheader>
-          </v-list>
-          <v-list v-if="loading == 0" three-line>
-            <v-subheader v-if="!conversations.length">Empty!</v-subheader>
-            <template v-for="(conversation, index) in conversations">
-              <v-divider :key="index"></v-divider>
-              <v-list-item
-                :key="index + 'divider'"
-                @click="selectConversation(conversation)"
-              >
-                <v-list-item-avatar>
-                  <v-avatar :color="conversationAvatar(conversation)" />
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title
-                    v-html="conversationTitle(conversation)"
-                  ></v-list-item-title>
-                  <v-list-item-subtitle
-                    v-html="conversationSubtitle(conversation)"
-                  ></v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-avatar>
-                  <v-chip
-                    color="primary"
-                    v-text="conversationChip(conversation)"
-                    small
-                    outlined
-                  />
-                </v-list-item-avatar>
-              </v-list-item>
-            </template>
-          </v-list>
-        </v-card>
-      </v-card>
+      <v-container fluid>
+        <v-row dense>
+          <v-col cols="4">
+            <v-card max-width="450" tile>
+              <v-subheader
+                ><v-text-field
+                  class="mx-3 my-1"
+                  v-model="searchQuery"
+                  flat
+                  single-line
+                  hide-details
+                  label="Search"
+                  v-on:keyup.enter="search"
+                ></v-text-field
+              ></v-subheader>
+              <v-list v-if="loading != 0" three-line>
+                <v-subheader>Loading...</v-subheader>
+              </v-list>
+              <v-list v-if="loading == 0" three-line>
+                <v-subheader v-if="!conversations.length">Empty!</v-subheader>
+                <template v-for="(conversation, index) in conversations">
+                  <v-divider :key="index + 'divider'"></v-divider>
+                  <v-list-item
+                    :key="conversation.getGroupChatId()"
+                    @click="selectConversation(conversation.getGroupChatId())"
+                  >
+                    <v-list-item-avatar>
+                      <v-avatar :color="conversationAvatar(conversation)" />
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-html="conversationTitle(conversation)"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle
+                        v-html="conversationSubtitle(conversation)"
+                      ></v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-avatar>
+                      <v-chip
+                        color="primary"
+                        v-text="conversationChip(conversation)"
+                        small
+                        outlined
+                      />
+                    </v-list-item-avatar>
+                  </v-list-item>
+                </template>
+              </v-list>
+            </v-card>
+          </v-col>
+          <v-col cols="8">
+            <v-card tile>
+              <v-subheader>Select a conversation...</v-subheader>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
   </v-main>
 </template>
@@ -74,50 +83,11 @@ import { client, conversations } from "../api"
 
 export default Vue.extend({
   data: () => ({
+    loading: 0,
     myUserId: 2, // TODO TODO TODO
     searchQuery: null as null | string,
     conversations: [] as Array<GroupChat>,
     userCache: {} as { [userId: number]: User },
-    conversationsOld: [
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-        title: "Brunch this weekend?",
-        subtitle:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-      },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        subtitle:
-          "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
-      },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-        title: "Oui oui",
-        subtitle:
-          "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
-      },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-        title: "Birthday gift",
-        subtitle:
-          "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
-      },
-      {
-        avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
-        title: "Recipe to try",
-        subtitle:
-          "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
-      },
-    ],
-    loading: 0,
-    friends: [] as Array<string>,
-    receivedRequests: [] as Array<FriendRequest.AsObject>,
-    sentRequests: [] as Array<FriendRequest.AsObject>,
-    errorMessage: "",
-    errorVisible: false,
-    successMessage: "",
-    successVisible: false,
   }),
 
   created() {
@@ -130,6 +100,7 @@ export default Vue.extend({
     getUser(userId: number) {
       if (!(userId in this.userCache)) {
         console.log("pretend to fetch user")
+        // TODO: do this async stuff
         return null
       } else {
         return this.userCache[userId]
@@ -138,11 +109,11 @@ export default Vue.extend({
 
     search() {
       console.log("Search for", this.searchQuery)
+      // TODO
     },
 
     fetchData() {
       this.loading += 1
-      this.errorMessage = ""
       const req = new ListGroupChatsReq()
       conversations
         .listGroupChats(req)
@@ -168,7 +139,6 @@ export default Vue.extend({
                 console.error(err)
               })
           })
-          console.log(res.toObject())
         })
         .catch((err) => {
           this.loading -= 1
@@ -204,6 +174,10 @@ export default Vue.extend({
 
     conversationSubtitle(conversation: GroupChat) {
       return conversation.getLatestMessage().getText() || "<i>No messages</i>"
+    },
+
+    selectConversation(conversationId: number) {
+      console.log("selecting conversation id", conversationId)
     },
   },
 })
