77e7a61b39d08f75d023f99173f0a7e9521ff396
Aapeli <git@aapelivuorinen.com>
2020-07-21 11:18:46 +1000

    Expand dummy data handling and add sample conversations

--- app/backend/src/app.py
+++ app/backend/src/app.py
@@ -1,70 +1,24 @@
-import json
 import logging
-import traceback
 from concurrent import futures
-from datetime import date
 
 import grpc
-from couchers.crypto import hash_password
-from couchers.db import session_scope
 from couchers.interceptors import LoggingInterceptor, intercept_server
-from couchers.models import Base, User
+from couchers.models import Base
 from couchers.servicers.api import API
 from couchers.servicers.auth import Auth
-from couchers.utils import Timestamp_from_datetime
+from dummy_data import add_dummy_data
 from pb import api_pb2_grpc, auth_pb2_grpc
 from sqlalchemy import create_engine
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
-logging.basicConfig(format="%(asctime)s.%(msecs)03d: %(process)d: %(message)s", datefmt="%F %T", level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s.%(msecs)03d: %(process)d: %(message)s", datefmt="%F %T", level=logging.INFO)
 logging.info(f"Starting")
 
 engine = create_engine("sqlite:///db.sqlite", echo=False)
-
 Base.metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
 
-def add_dummy_data(file_name):
-    with session_scope(Session) as session:
-        with open(file_name, "r") as file:
-            users = json.loads(file.read())
-
-        for user in users:
-            new_user = User(
-                username=user["username"],
-                email=user["email"],
-                hashed_password=hash_password(user["password"]) if user["password"] else None,
-                name=user["name"],
-                city=user["city"],
-                verification=user["verification"],
-                community_standing=user["community_standing"],
-                birthdate=date(
-                    year=user["birthdate"]["year"],
-                    month=user["birthdate"]["month"],
-                    day=user["birthdate"]["day"]
-                ),
-                gender=user["gender"],
-                languages="|".join(user["languages"]),
-                occupation=user["occupation"],
-                about_me=user["about_me"],
-                about_place=user["about_place"],
-                countries_visited="|".join(user["countries_visited"]),
-                countries_lived="|".join(user["countries_lived"]),
-            )
-            session.add(new_user)
-
-logging.info(f"Adding dummy data")
-
-try:
-    add_dummy_data("src/dummy_data.json")
-except IntegrityError as e:
-    print("Failed to insert dummy data, is it already inserted?")
-
-with session_scope(Session) as session:
-    for user in session.query(User).all():
-        print(user)
+add_dummy_data(Session, "src/dummy_data.json")
 
 auth = Auth(Session)
 auth_server = grpc.server(futures.ThreadPoolExecutor(2))
---
+++ app/backend/src/dummy_data.py
@@ -0,0 +1,91 @@
+import json
+import logging
+from datetime import date
+
+from couchers.crypto import hash_password
+from couchers.db import get_user_by_field, session_scope
+from couchers.models import (Base, Conversation, FriendRelationship,
+                             FriendStatus, GroupChat, GroupChatRole,
+                             GroupChatSubscription, User, Message)
+from couchers.utils import Timestamp_from_datetime
+from sqlalchemy.exc import IntegrityError
+
+
+def add_dummy_data(Session, file_name):
+    try:
+        logging.info(f"Adding dummy data")
+        with session_scope(Session) as session:
+            with open(file_name, "r") as file:
+                data = json.loads(file.read())
+
+            for user in data["users"]:
+                new_user = User(
+                    username=user["username"],
+                    email=user["email"],
+                    hashed_password=hash_password(user["password"]) if user["password"] else None,
+                    name=user["name"],
+                    city=user["city"],
+                    verification=user["verification"],
+                    community_standing=user["community_standing"],
+                    birthdate=date(
+                        year=user["birthdate"]["year"],
+                        month=user["birthdate"]["month"],
+                        day=user["birthdate"]["day"]
+                    ),
+                    gender=user["gender"],
+                    languages="|".join(user["languages"]),
+                    occupation=user["occupation"],
+                    about_me=user["about_me"],
+                    about_place=user["about_place"],
+                    countries_visited="|".join(user["countries_visited"]),
+                    countries_lived="|".join(user["countries_lived"]),
+                )
+                session.add(new_user)
+
+            session.commit()
+
+            for username1, username2 in data["friendships"]:
+                friend_relationship = FriendRelationship(
+                    from_user_id=get_user_by_field(session, username1).id,
+                    to_user_id=get_user_by_field(session, username2).id,
+                    status=FriendStatus.accepted,
+                )
+                session.add(friend_relationship)
+            
+            session.commit()
+
+            for group_chat in data["group_chats"]:
+                # Create the chat
+                creator = group_chat["creator"]
+
+                conversation = Conversation()
+                session.add(conversation)
+
+                chat = GroupChat(
+                    conversation=conversation,
+                    title=group_chat["title"],
+                    creator_id=get_user_by_field(session, creator).id,
+                    is_dm=group_chat["is_dm"],
+                )
+                session.add(chat)
+
+                for participant in group_chat["participants"]:
+                    subscription = GroupChatSubscription(
+                        user_id=get_user_by_field(session, participant).id,
+                        group_chat=chat,
+                        role=GroupChatRole.admin if participant == creator else GroupChatRole.participant,
+                    )
+                    session.add(subscription)
+
+                for message in group_chat["messages"]:
+                    session.add(Message(
+                        conversation=chat.conversation,
+                        author_id=get_user_by_field(session, message["author"]).id,
+                        text=message["message"],
+                    ))
+
+            session.commit()
+
+
+    except IntegrityError as e:
+        logging.error("Failed to insert dummy data, is it already inserted?")
