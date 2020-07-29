23d19308545327d58e22667706ba35f92527b1b9
Aapeli <git@aapelivuorinen.com>
2020-07-29 17:24:01 +1000

    Make last_seen_message_id a foreign key to messages.id

--- app/backend/src/couchers/models.py
+++ app/backend/src/couchers/models.py
@@ -331,8 +331,7 @@ class GroupChatSubscription(Base):
 
     role = Column(Enum(GroupChatRole), nullable=False)
 
-    # TODO: should this be a ForeignKey("messages.id")?
-    last_seen_message_id = Column(Integer, nullable=False, default=0)
+    last_seen_message_id = Column(ForeignKey("messages.id"), nullable=False, default=0)
 
     user = relationship("User", backref="group_chat_subscriptions")
     group_chat = relationship("GroupChat", backref="subscriptions")
