70acb24369e9da1f8335f24215cbcdd901f802ad
Aapeli <git@aapelivuorinen.com>
2020-07-30 22:29:03 +1000

    Add axios and implement file upload to prescribed URL

--- app/frontend/src/components/EditableAvatar.vue
+++ app/frontend/src/components/EditableAvatar.vue
@@ -3,26 +3,48 @@
     <p>Current avatar:</p>
     <img :src="user.avatarUrl" />
     <v-file-input
+      v-model="image"
       :rules="avatarRules"
       accept="image/png, image/jpeg, image/bmp"
       placeholder="Pick a new avatar"
       prepend-icon="mdi-face"
       label="Avatar"
+      :disabled="loading"
+      show-size
     ></v-file-input>
-    <v-btn class="mx-2 my-2" v-on:click="upload" color="success">Upload</v-btn>
+    <v-progress-linear v-if="loading" :value="progress"></v-progress-linear>
+    <v-btn
+      class="mx-2 my-2"
+      :disabled="loading"
+      v-on:click="upload"
+      color="success"
+      >Upload</v-btn
+    >
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 
+import axios from "axios"
+
+import { Empty } from "google-protobuf/google/protobuf/empty_pb"
+
+import { client } from "../api"
+
 export default Vue.extend({
   props: ["user"],
 
   data: () => ({
+    image: null as null | File,
+    progress: 0,
+    loading: false,
     editing: false,
     avatarRules: [
-      value => !value || value.size < 16000000 || 'Avatar size should be less than 16 MB!',
+      (value) =>
+        !value ||
+        value.size < 16000000 ||
+        "Avatar size should be less than 16 MB!",
     ],
   }),
 
@@ -32,8 +54,29 @@ export default Vue.extend({
     },
 
     upload() {
-      this.$emit("save")
-      this.editing = false
+      client.initiateMediaUpload(new Empty()).then((res) => {
+        const formData = new FormData()
+        formData.append("file", this.image)
+        this.progress = 0
+        this.loading = true
+        axios
+          .post(res.getUploadUrl(), formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            onUploadProgress: (event) => {
+              this.progress = (event.loaded / event.total) * 100
+            },
+          })
+          .then((res) => {
+            this.$emit("save")
+            this.editing = false
+          })
+          .catch(console.error)
+          .finally(() => {
+            this.loading = false
+          })
+      })
     },
   },
 })
