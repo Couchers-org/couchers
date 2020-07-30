ed7b88dd4e7ae60926a890481ed0e2f724a7c23c
Aapeli <git@aapelivuorinen.com>
2020-07-30 22:31:52 +1000

    Remove unnecessary code

--- app/backend/src/media_server.py
+++ app/backend/src/media_server.py
@@ -1,15 +1,12 @@
 import logging
 import os
-from base64 import urlsafe_b64decode, urlsafe_b64encode
-from datetime import datetime, timedelta
-from io import BytesIO
-from urllib.parse import urlencode
+from base64 import urlsafe_b64decode
+from datetime import datetime
 
 import backoff
 import grpc
 import pyvips
-from couchers.crypto import generate_hash_signature, verify_hash_signature
-from couchers.utils import Timestamp_from_datetime
+from couchers.crypto import verify_hash_signature
 from flask import Flask, abort, request, send_file
 from flask_cors import CORS
 from pb import media_pb2, media_pb2_grpc
@@ -59,31 +56,6 @@ def send_confirmation_to_main_server(key, filename):
             filename=filename,
         )
         media_stub.UploadConfirmation(req)
-
-@app.route("/debug")
-def debug():
-    req = media_pb2.UploadRequest(
-        key="couchers",
-        type=media_pb2.UploadRequest.UploadType.IMAGE,
-        created=Timestamp_from_datetime(datetime.utcnow()),
-        expiry=Timestamp_from_datetime(datetime.utcnow() + timedelta(hours=1)),
-        max_width=2000,
-        max_height=1600,
-    ).SerializeToString()
-
-    data = urlsafe_b64encode(req).decode("utf8")
-    sig = urlsafe_b64encode(generate_hash_signature(req, MEDIA_SERVER_SECRET_KEY)).decode("utf8")
-
-    ret = {
-        "data": data,
-        "sig": sig,
-    }
-
-    return {
-        "url": f"http://127.0.0.1:5000/upload?" + urlencode(ret),
-        "data": data,
-        "sig": sig,
-    }
 
 @app.route("/upload", methods=["POST"])
 def upload():
