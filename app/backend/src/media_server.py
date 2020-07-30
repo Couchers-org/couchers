ee9d2f00c0f26db9c8e3c58da2cb350b6f6239a3
Aapeli <git@aapelivuorinen.com>
2020-07-30 17:54:40 +1000

    Fix media upload location

--- app/backend/src/media_server.py
+++ app/backend/src/media_server.py
@@ -1,8 +1,8 @@
 import logging
+import os
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from datetime import datetime, timedelta
 from io import BytesIO
-from os import environ
 from urllib.parse import urlencode
 
 import backoff
@@ -12,29 +12,31 @@ from couchers.crypto import generate_hash_signature, verify_hash_signature
 from couchers.utils import Timestamp_from_datetime
 from flask import Flask, abort, request
 from pb import media_pb2, media_pb2_grpc
+from werkzeug.utils import secure_filename
+
+# hex-encoded secret key, used for signatures that  verify main & media server
+# are talking to each other
+MEDIA_SERVER_SECRET_KEY = bytes.fromhex(os.environ["MEDIA_SERVER_SECRET_KEY"])
+
+# address of main server
+MAIN_SERVER_ADDRESS = os.environ["MAIN_SERVER_ADDRESS"]
+
+# whether to disable SSL, optional
+MAIN_SERVER_NO_SSL = os.environ.get("MAIN_SERVER_NO_SSL", "0")
+MAIN_SERVER_USE_SSL = MAIN_SERVER_NO_SSL == "0"
+
+MEDIA_UPLOAD_LOCATION = os.environ["MEDIA_UPLOAD_LOCATION"]
+
 
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-# Config values
-
-# hex-encoded secret key, used for signatures that  verify main & media server
-# are talking to each other
-MEDIA_SERVER_SECRET_KEY = bytes.fromhex(environ["MEDIA_SERVER_SECRET_KEY"])
-
-# address of main server
-MAIN_SERVER_ADDRESS = environ["MAIN_SERVER_ADDRESS"]
-
-# whether to disable SSL
-MAIN_SERVER_NO_SSL = environ.get("MAIN_SERVER_NO_SSL")
-MAIN_SERVER_USE_SSL = MAIN_SERVER_NO_SSL is None
-
-#call_creds = grpc.access_token_call_credentials(token)
-#comp_creds = grpc.composite_channel_credentials(grpc.local_channel_credentials(), call_creds)
+def get_path(filename):
+    return MEDIA_UPLOAD_LOCATION + "/" + filename
 
 @backoff.on_exception(backoff.expo, grpc.StatusCode.UNAVAILABLE, max_time=5)
-def send_confirmation_to_main_server(key):
+def send_confirmation_to_main_server(key, filename):
     if MAIN_SERVER_USE_SSL:
         channel = grpc.secure_channel(MAIN_SERVER_ADDRESS)
     else:
@@ -44,6 +46,7 @@ def send_confirmation_to_main_server(key):
     media_stub = media_pb2_grpc.MediaStub(channel)
     req = media_pb2.UploadConfirmationReq(
         key=key,
+        filename=filename,
     )
     media_stub.UploadConfirmation(req)
 
@@ -95,13 +98,17 @@ def upload():
     if req.expiry.ToDatetime() < now:
         abort(400, "Request expired")
 
+    if req.key != secure_filename(req.key):
+        # just a sanity check
+        abort(500, "Invalid key")
+
     request_file = request.files.get("file", None)
 
     if not request_file:
         abort(400, "No file provided")
 
     if req.type != media_pb2.UploadRequest.UploadType.IMAGE:
-        abort(500, "Unsupported upload type.")
+        abort(500, "Unsupported upload type")
 
     # handle image uploads
     img = pyvips.Image.new_from_buffer(request_file.read(), options="", access="sequential")
@@ -114,14 +121,16 @@ def upload():
     if scale < 1:
         img = img.resize(scale)
 
-    # strip removes EXIF and other metadata (e.g. GPS location)
-    img.write_to_file(f"{req.key}.jpg", strip=True)
+    filename = req.key + ".jpg"
+    path = get_path(filename)
+
+    # strip removes EXIF (e.g. GPS location) and other metadata
+    img.write_to_file(path, strip=True)
 
     # let the main server know the upload succeeded, or delete the file
     try:
-        send_confirmation_to_main_server(req.key)
+        send_confirmation_to_main_server(req.key, filename)
         return {"ok": True}
     except Exception as e:
-        # TODO(aapeli): delete file here
+        os.remove(path)
         raise e
-        return {"error": True}
