f2998a30a44bf47618b227608ad3a2be20324f50
Aapeli <git@aapelivuorinen.com>
2020-08-12 18:06:43 +1000

    Add basic media server testing fixture

---
+++ app/backend/src/tests/test_media_server.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+
+import pytest
+
+from media_server import create_app
+from nacl.utils import random as random_bytes
+
+@pytest.fixture
+def client(tmp_path):
+    app = create_app(
+        media_server_secret_key=random_bytes(32),
+        media_server_bearer_token=random_bytes(32),
+        main_server_address="localhost:8088",
+        main_server_use_ssl=False,
+        media_upload_location=tmp_path,
+        avatar_size=200,
+    )
+
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    rv = client.get('/')
+    assert b'404' in rv.data
