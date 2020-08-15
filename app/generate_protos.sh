4c1c2603bcf28f6f5e2e798a5e3cecadcf50d89b
Aapeli <git@aapelivuorinen.com>
2020-08-15 22:18:06 +1000

    Only generate python protos

--- app/generate_protos.sh
+++ app/generate_protos.sh
@@ -6,8 +6,8 @@ find pb -name '*.proto' | $PROTOC -I. \
   --python_out=media/src \
   --grpc_python_out=media/src \
   --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
-  \
-  --js_out="import_style=commonjs,binary:frontend/src" \
-  --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
-  \
   $(xargs)
+  # \
+  # --js_out="import_style=commonjs,binary:frontend/src" \
+  # --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
+  # \
