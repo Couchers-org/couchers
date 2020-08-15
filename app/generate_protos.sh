57370af1e045d37bce274393cc992313c3283e39
Aapeli <git@aapelivuorinen.com>
2020-08-15 22:34:58 +1000

    Back to develop generate_protos.sh

--- app/generate_protos.sh
+++ app/generate_protos.sh
@@ -1,25 +1,12 @@
-#!/bin/bash
-PROTOC=${PROTOC:-protoc}
-
-echo "Genearting protos..."
-
-if [[ -z "${DONT_GENERATE_PY}" ]]; then
-  echo "Genearting python protos..."
-  find pb -name '*.proto' | $PROTOC -I. \
-    --python_out=backend/src \
-    --grpc_python_out=backend/src \
-    --python_out=media/src \
-    --grpc_python_out=media/src \
-    --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
-    \
-    $(xargs)
-fi
-
-if [[ -z "${DONT_GENERATE_JS}" ]]; then
-  echo "Genearting javascript protos..."
-  find pb -name '*.proto' | $PROTOC -I. \
-    --js_out="import_style=commonjs,binary:frontend/src" \
-    --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
-    \
-    $(xargs)
-fi
+#!/bin/sh
+find pb -name '*.proto' | protoc -I. \
+  --python_out=backend/src \
+  --grpc_python_out=backend/src \
+  --python_out=media/src \
+  --grpc_python_out=media/src \
+  --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
+  \
+  --js_out="import_style=commonjs,binary:frontend/src" \
+  --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
+  \
+  $(xargs)
