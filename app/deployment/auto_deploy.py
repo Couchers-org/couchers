9d673d1911492ab39a3dc5cf52bffad3101bd54c
Nick <nicolasa183@gmail.com>
2020-08-19 00:49:03 +1000

    formatting

--- app/deployment/auto_deploy.py
+++ app/deployment/auto_deploy.py
@@ -6,12 +6,13 @@ import base64
 import os
 import subprocess
 
+
 def get_conf(config="./deploy_conf.yaml"):
     conf_file = open(config,"r").read()
     return yaml.safe_load(conf_file)
 
 
-def  get_database_container(docker_client, container_id):
+def get_database_container(docker_client, container_id):
     return docker_client.containers.get(container_id)
 
 
@@ -21,7 +22,7 @@ def main(config):
     #database_container = get_database_container(docker_client, config["containers"]["database_container"]["name"])
     #if not os.path.exists("/
     #database_container.exec_run("pg_dumpall > /var/lib/postgresql/data/
-    ecr_client = boto3.client('ecr', region_name=config["region"])
+    ecr_client = boto3.client("ecr", region_name=config["region"])
     auth = ecr_client.get_authorization_token()
     docker_client.login(username="AWS", password=base64.b64decode(auth["authorizationData"][0]["authorizationToken"]).decode().split(":")[1], registry="694904428454.dkr.ecr.us-east-1.amazonaws.com")
     for image in config["images"]:
@@ -29,8 +30,7 @@ def main(config):
         
     #run docker compose
     subprocess.Popen(["docker-compose", "up", "--force-recreate", "--build", "-d"],cwd=config["working_directory"])
-    subprocess.Popen(["docker", "image", "prune", "-f"])
-    
+    subprocess.Popen(["docker", "image", "prune", "-f"])  
 
 
 def pull_image(docker_client, image_repository):
