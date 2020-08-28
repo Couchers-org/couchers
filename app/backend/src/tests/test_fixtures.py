3a0841da965f0e8b6d9aafe8ebbc287f134c8ed2
Christian Häggström <kalvdans@gmail.com>
2020-08-28 09:09:36 +0200

    Add empty line after import to comply to codestyle

--- app/backend/src/tests/test_fixtures.py
+++ app/backend/src/tests/test_fixtures.py
@@ -129,6 +129,7 @@ class FakeChannel:
 
     def add_generic_rpc_handlers(self, generic_rpc_handlers):
         from grpc._server import _validate_generic_rpc_handlers
+
         _validate_generic_rpc_handlers(generic_rpc_handlers)
 
         self.handlers.update(generic_rpc_handlers[0]._method_handlers)
