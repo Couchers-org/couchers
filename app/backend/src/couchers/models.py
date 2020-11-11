771c1809efe8e551611fb9a1c2b90ccf1935d99b
Christian Häggström <kalvdans@gmail.com>
2020-11-11 22:27:44 +0100

    Revert unrelated database change

--- app/backend/src/couchers/models.py
+++ app/backend/src/couchers/models.py
@@ -15,7 +15,7 @@ meta = MetaData(
     naming_convention={
         "ix": "ix_%(column_0_label)s",
         "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
         "pk": "pk_%(table_name)s",
     }
