cd399bc2bf6cc6941fe2b29a210beb5dfcd1e4db
Antonius de Wit <marco@witinformatisering.nl>
2020-11-18 15:46:17 +0000

    add a .env file with a default api base url

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,9 @@
 /// <reference types="react-scripts" />
 /// <reference types="react-dom/experimental" />
 /// <reference types="react/experimental" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    REACT_APP_API_BASE_URL: string;
+  }
+}
