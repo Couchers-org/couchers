8418c75fe4197381562b5de3927d1f567b0f5058
Antonius de Wit <marco@witinformatisering.nl>
2020-11-21 16:16:08 +0000

    put the Avatar component in Storybook

--- app/frontend/src/components/Avatar.tsx
+++ app/frontend/src/components/Avatar.tsx
@@ -1,12 +1,14 @@
 import {
   Avatar as MuiAvatar,
   AvatarProps as MuiAvatarProps,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
-import { User } from '../pb/api_pb';
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import { User } from "../pb/api_pb";
 
-const useStyles = makeStyles({ root: { height: '3em', width: '3em', fontSize: 'unset' } });
+const useStyles = makeStyles({
+  root: { height: "3em", width: "3em", fontSize: "unset" },
+});
 
 export interface AvatarProps extends MuiAvatarProps {
   user: User.AsObject;
@@ -14,5 +16,7 @@ export interface AvatarProps extends MuiAvatarProps {
 
 export default function Avatar({ user, ...otherProps }: AvatarProps) {
   const classes = useStyles();
-  return <MuiAvatar {...otherProps} className={classes.root} src={user.avatarUrl} />;
+  return (
+    <MuiAvatar {...otherProps} className={classes.root} src={user.avatarUrl} />
+  );
 }
--- app/frontend/src/components/stories/Avatar.stories.tsx
+++ app/frontend/src/components/stories/Avatar.stories.tsx
@@ -1,16 +1,16 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Meta, Story } from '@storybook/react/types-6-0';
-import * as React from 'react';
+import { Meta, Story } from "@storybook/react/types-6-0";
+import * as React from "react";
 // @ts-ignore
-import imageFile from '../../stories/assets/funnycat.jpg';
+import imageFile from "../../stories/assets/funnycat.jpg";
 
-import Avatar, { AvatarProps } from '../Avatar';
+import Avatar, { AvatarProps } from "../Avatar";
 
 export default {
-  title: 'Components/Avatar',
+  title: "Components/Avatar",
   component: Avatar,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 } as Meta;
 
@@ -18,11 +18,15 @@ const user = {
   avatarUrl: imageFile,
 };
 
-const Template: Story<AvatarProps> = (args) => <>
-  <Avatar {...args} />
-  <div style={{ fontSize: '100px' }}><Avatar {...args} /></div>
-  <Avatar style={{ width: '100px', height: '100px' }} {...args} />
-</>;
+const Template: Story<AvatarProps> = (args) => (
+  <>
+    <Avatar {...args} />
+    <div style={{ fontSize: "100px" }}>
+      <Avatar {...args} />
+    </div>
+    <Avatar style={{ width: "100px", height: "100px" }} {...args} />
+  </>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
