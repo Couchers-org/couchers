37701f19f4e393671a866ee5f7ff16e2f44f8fea
Antonius de Wit <marco@witinformatisering.nl>
2020-11-21 16:12:05 +0000

    put the Avatar component in Storybook

--- app/frontend/src/components/Avatar.tsx
+++ app/frontend/src/components/Avatar.tsx
@@ -1,18 +1,18 @@
 import {
   Avatar as MuiAvatar,
   AvatarProps as MuiAvatarProps,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import React from "react";
-import { User } from "../pb/api_pb";
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { User } from '../pb/api_pb';
 
-const useStyles = makeStyles({ root: {} });
+const useStyles = makeStyles({ root: { height: '3em', width: '3em', fontSize: 'unset' } });
 
 export interface AvatarProps extends MuiAvatarProps {
   user: User.AsObject;
 }
 
-export default function Avatar({ user }: AvatarProps) {
+export default function Avatar({ user, ...otherProps }: AvatarProps) {
   const classes = useStyles();
-  return <MuiAvatar className={classes.root} src={user.avatarUrl} />;
+  return <MuiAvatar {...otherProps} className={classes.root} src={user.avatarUrl} />;
 }
---
+++ app/frontend/src/components/stories/Avatar.stories.tsx
@@ -0,0 +1,30 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/react/types-6-0';
+import * as React from 'react';
+// @ts-ignore
+import imageFile from '../../stories/assets/funnycat.jpg';
+
+import Avatar, { AvatarProps } from '../Avatar';
+
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const user = {
+  avatarUrl: imageFile,
+};
+
+const Template: Story<AvatarProps> = (args) => <>
+  <Avatar {...args} />
+  <div style={{ fontSize: '100px' }}><Avatar {...args} /></div>
+  <Avatar style={{ width: '100px', height: '100px' }} {...args} />
+</>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  user,
+};
