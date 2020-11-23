8cdb2032a8baf86edc256a347016ef348b013302
Antonius de Wit <marco@witinformatisering.nl>
2020-11-23 16:11:58 +0000

    create the messages page with empty tabs

--- app/frontend/src/features/messages/TabBar.tsx
+++ app/frontend/src/features/messages/TabBar.tsx
@@ -1,4 +1,4 @@
-import { Tab } from "@material-ui/core";
+import { Tab, Tabs } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import { TabList } from "@material-ui/lab";
@@ -11,22 +11,28 @@ export interface TabBarProps<T extends Record<string, string>> {
   labels: T;
 }
 
-export default function TabBar<T extends Record<string, string>>({ value, setValue, labels }: TabBarProps<T>) {
+export default function TabBar<T extends Record<string, string>>({
+  value,
+  setValue,
+  labels,
+}: TabBarProps<T>) {
   const handleChange = (event: any, newValue: keyof T) => {
     setValue(newValue);
   };
   const classes = useStyles();
   return (
-    <TabList
+    <Tabs
       value={value as any}
       onChange={handleChange}
       indicatorColor="primary"
       textColor="primary"
+      scrollButtons="auto"
+      variant="scrollable"
       centered
     >
       {Object.entries(labels).map(([value, label]) => (
         <Tab key={value} label={label} value={value} />
       ))}
-    </TabList>
+    </Tabs>
   );
 }
