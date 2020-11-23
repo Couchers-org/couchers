f16403dd4adedb412df7de1c1611905edbe3f6fd
Antonius de Wit <marco@witinformatisering.nl>
2020-11-23 11:36:42 +0000

    create the messages page with empty tabs

--- app/frontend/src/features/messages/Messages.stories.tsx
+++ app/frontend/src/features/messages/Messages.stories.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { store } from "../../store";
 
-import Messages from "./index";
+import Messages from "./Messages";
 
 export default {
   title: "Messages",
