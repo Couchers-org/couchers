1c1b86e885808a004c2821c9efd32b7d7f7d2281
Aapeli <git@aapelivuorinen.com>
2020-11-24 16:50:40 -0500

    Add a script to launch psql

---
+++ app/deployment/psql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+pushd ..
+
+# we need the following vars from this .env file:
+# - DATABASE_CONNECTION_STRING
+source backup.prod.env
+
+docker_image=${DB_DOCKER_IMAGE:-"postgis/postgis:13-3.0"}
+
+# --net=host is required so we can hit localhost from inside the container
+docker run --net=host -it $docker_image psql $DATABASE_CONNECTION_STRING
+
+popd
