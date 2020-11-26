7108d2a00a11bf4e28195eb613d4f9ec7e9590fa
Sherri Cui <sherrii.cui@pm.me>
2020-11-26 14:07:56 +0100

    add react router and redux to story

--- app/frontend/src/components/Navigation/Navigation.stories.tsx
+++ app/frontend/src/components/Navigation/Navigation.stories.tsx
@@ -1,11 +1,27 @@
-import { Meta, Story } from "@storybook/react";
+import { Meta, Story } from "@storybook/react/types-6-0";
 import * as React from "react";
+import { Provider } from "react-redux";
+import { MemoryRouter as Router } from "react-router-dom";
+import { store } from "../../store";
 
 import Navigation from ".";
 
 export default {
-	title: "Components/Navigation",
-	component: Navigation,
+  title: "Components/Navigation",
+  component: Navigation,
+  argTypes: {},
+  decorators: [
+    (storyFn) => {
+      return (
+        <Provider store={store}>
+          <Router>{storyFn()}</Router>
+        </Provider>
+      );
+    },
+  ],
 } as Meta;
 
-export const Primary: React.SFC<{}> = () => <Navigation />;
+const Template: Story<any> = (args) => <Navigation {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
