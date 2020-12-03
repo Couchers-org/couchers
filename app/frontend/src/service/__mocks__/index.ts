5037372b78e7b2bad19ab3693d73ff8dd6b6bac7
Antonius de Wit <marco@witinformatisering.nl>
2020-12-03 19:07:20 +0000

    add a messages tab for surfing

--- app/frontend/src/service/__mocks__/index.ts
+++ app/frontend/src/service/__mocks__/index.ts
@@ -6,7 +6,7 @@ import funnykid from "../../stories/assets/funnykid.jpg";
 import messages from "../../test/fixtures/messages.json";
 import users from "../../test/fixtures/users.json";
 
-export const user1 = {
+const user1 = {
   ...users[0],
   avatarUrl: funnycat,
 } as User.AsObject;
