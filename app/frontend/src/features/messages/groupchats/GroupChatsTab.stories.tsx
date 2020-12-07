225194a999666372d35f73cbda8e133a7e51663b
Antonius de Wit <marco@witinformatisering.nl>
2020-12-07 18:15:49 +0000

    add button to leave group chat

--- app/frontend/src/features/messages/groupchats/GroupChatsTab.stories.tsx
+++ app/frontend/src/features/messages/groupchats/GroupChatsTab.stories.tsx
@@ -1,11 +1,14 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 import * as React from "react";
 import { Provider } from "react-redux";
+import { setGroupChat } from ".";
+import { groupChat, mockedService } from "../../../stories/__mocks__/service";
+import { store } from "../../../stories/__mocks__/store";
 import messages from "../../../test/fixtures/messages.json";
-import { store } from "../../../store";
-
 import MessageView, { MessageProps } from "../messagelist/Message";
 import GroupChatsTab from "./GroupChatsTab";
+import GroupChatView from "./GroupChatView";
 
 const [message1] = messages;
 
@@ -15,12 +18,26 @@ export default {
   argTypes: {},
   decorators: [
     (storyFn) => {
-      return <Provider store={store}>{storyFn()}</Provider>;
+      return (
+        <Provider store={storyFn !== View ? store : store}>
+          {storyFn()}
+        </Provider>
+      );
     },
   ],
 } as Meta;
 
-const Template: Story = (args) => <GroupChatsTab {...args} />;
+mockedService.conversations.createGroupChat = async () => {
+  return 1234;
+};
+
+const Template: Story = (args) => {
+  store.dispatch(setGroupChat(null));
+  mockedService.conversations.leaveGroupChat = async () => {
+    return new Empty();
+  };
+  return <GroupChatsTab {...args} />;
+};
 
 export const Tab = Template.bind({});
 Tab.args = {};
@@ -31,3 +48,14 @@ const MessageTemplate: Story<MessageProps> = (args) => (
 
 export const Collapsed = MessageTemplate.bind({});
 Collapsed.args = { message: message1 };
+
+const GroupChatViewTemplate: Story<any> = (args) => {
+  store.dispatch(setGroupChat(groupChat));
+  mockedService.conversations.leaveGroupChat = async () => {
+    throw new Error("impossible to leave");
+  };
+  return <GroupChatView {...args} />;
+};
+
+export const View = GroupChatViewTemplate.bind({});
+View.args = {};
