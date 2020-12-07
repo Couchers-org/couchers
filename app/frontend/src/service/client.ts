1902260d8c60c15a878940772a07b4066891d1e5
Aapeli <git@aapelivuorinen.com>
2020-12-06 19:56:02 -0500

    Load grpc web tools only in development

--- app/frontend/src/service/client.ts
+++ app/frontend/src/service/client.ts
@@ -38,17 +38,19 @@ const apis = {
   jail: new JailPromiseClient(URL, null, opts),
 };
 
-// @ts-ignore
-const grpcWebTools = window.__GRPCWEB_DEVTOOLS__ || (() => {});
+if (process.env.NODE_ENV === "development") {
+  // @ts-ignore
+  const grpcWebTools = window.__GRPCWEB_DEVTOOLS__ || (() => {});
 
-grpcWebTools([
-  apis.api,
-  apis.bugs,
-  apis.sso,
-  apis.conversations,
-  apis.auth,
-  apis.requests,
-  apis.jail,
-]);
+  grpcWebTools([
+    apis.api,
+    apis.bugs,
+    apis.sso,
+    apis.conversations,
+    apis.auth,
+    apis.requests,
+    apis.jail,
+  ]);
+}
 
 export default apis;
