1febc37d4877b5c24f7a36f58df23e72fe7746a5
Antonius de Wit <marco@witinformatisering.nl>
2020-12-08 11:26:09 +0000

    wip

--- app/frontend/src/features/messages/groupchats/GroupChatList.tsx
+++ app/frontend/src/features/messages/groupchats/GroupChatList.tsx
@@ -4,21 +4,20 @@ import React from "react";
 import { GroupChat } from "../../../pb/conversations_pb";
 import { useAppDispatch, useTypedSelector } from "../../../store";
 import GroupChatListItem from "./GroupChatListItem";
-import { setGroupChat } from ".";
+import { groupChatsState, setGroupChat } from '.';
 
 const useStyles = makeStyles({ root: {} });
 
 export default function GroupChatList() {
-  const { groupChats } = useTypedSelector((state) => state.groupChats);
   const dispatch = useAppDispatch();
   const dispatchSetGroupChat = (groupChat: GroupChat.AsObject | null) =>
-    dispatch(setGroupChat(groupChat));
+    dispatch(setGroupChat(groupChat!));
 
   const classes = useStyles();
   return (
     <>
       <Box className={classes.root}>
-        {groupChats.map((groupChat) => (
+        {groupChatsState.groupChats.map((groupChat) => (
           <Link
             key={groupChat.groupChatId}
             onClick={() => dispatchSetGroupChat(groupChat)}
--- app/frontend/src/features/messages/groupchats/groupChatsSlice.ts
+++ app/frontend/src/features/messages/groupchats/groupChatsSlice.ts
@@ -1,4 +1,5 @@
-import { createAction, createSlice } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { flow, Instance, types } from 'mobx-state-tree';
 import {
   fetchGroupChats,
   fetchMessages,
@@ -6,7 +7,10 @@ import {
   sendMessage,
   setGroupChat,
 } from ".";
+import { User } from "../../../pb/api_pb";
 import { GroupChat, Message } from "../../../pb/conversations_pb";
+import { RootState } from "../../../reducers";
+import { service } from "../../../service";
 
 export const initialState = {
   groupChats: [] as GroupChat.AsObject[],
@@ -19,6 +23,96 @@ export const initialState = {
     loading: false,
   },
 };
+
+export const GroupChatsModel = types
+  .model({
+    groupChats: types.array(types.number),
+    error: types.string,
+    loading: types.boolean,
+    groupChatView: types.model({
+      error: types.string,
+      loading: types.boolean,
+      messages: types.array(types.frozen()),
+      groupChat: types.maybeNull(types.frozen()),
+    }),
+  })
+  .actions((self) => ({
+    fetchGroupChats: flow(function* () {
+      self.error = "";
+      self.loading = true;
+      try {
+        self.groupChats = yield service.conversations.listGroupChats();
+        self.loading = false;
+      } catch (error) {
+        self.error = error.message!;
+        self.loading = false;
+      }
+    }),
+  }));
+
+export type GroupChats = Instance<typeof GroupChatsModel>;
+export const groupChatsState = GroupChatsModel.create();
+
+//
+// export const fetchGroupChats = createAsyncThunk(
+//   "hostRequests/fetchGroupChats",
+//   async () => {
+//     const groupChats = await service.conversations.listGroupChats();
+//     return groupChats;
+//   }
+// );
+//
+// export const fetchMessages = createAsyncThunk(
+//   "hostRequests/fetchMessages",
+//   async (groupChat: GroupChat.AsObject) => {
+//     return service.conversations.getGroupChatMessages(groupChat.groupChatId);
+//   }
+// );
+//
+// export const setGroupChat = createAsyncThunk(
+//   "hostRequests/setGroupChat",
+//   async (groupChat: GroupChat.AsObject, thunkApi) => {
+//     if (groupChat) {
+//       thunkApi.dispatch(fetchMessages(groupChat));
+//     }
+//     return groupChat;
+//   }
+// );
+//
+// export const createGroupChat = createAsyncThunk<
+//   GroupChat.AsObject | null,
+//   { title: string; users: User.AsObject[] },
+//   { state: RootState }
+// >("hostRequests/createGroupChat", async ({ title, users }, thunkApi) => {
+//   const groupChatId = await service.conversations.createGroupChat(title, users);
+//   await thunkApi.dispatch(fetchGroupChats());
+//   return (
+//     thunkApi
+//       .getState()
+//       .groupChats.groupChats.find(
+//         (groupChat: GroupChat.AsObject) => groupChat.groupChatId === groupChatId
+//       ) || null
+//   );
+// });
+//
+// export const leaveGroupChat = createAsyncThunk(
+//   "hostRequests/leaveGroupChat",
+//   async (groupChat: GroupChat.AsObject, thunkApi) => {
+//     await service.conversations.leaveGroupChat(groupChat.groupChatId);
+//     thunkApi.dispatch(fetchGroupChats());
+//   }
+// );
+//
+// export const sendMessage = createAsyncThunk(
+//   "hostRequests/sendMessage",
+//   async (
+//     { groupChat, text }: { groupChat: GroupChat.AsObject; text: string },
+//     thunkApi
+//   ) => {
+//     await service.conversations.sendMessage(groupChat.groupChatId, text);
+//     thunkApi.dispatch(fetchMessages(groupChat));
+//   }
+// );
 
 export type RequestsState = typeof initialState;
 export const messagesFetched = createAction<Message.AsObject[]>(
