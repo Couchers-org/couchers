efdcc004feca4c2015f7658208ef20bd522ade25
Xiao Cui <19589883+navandstokes@users.noreply.github.com>
2020-12-09 16:51:02 +0100

    states and rerender

--- app/frontend/src/features/profile/ProfileForm.tsx
+++ app/frontend/src/features/profile/ProfileForm.tsx
@@ -1,11 +1,9 @@
 import { makeStyles } from "@material-ui/core";
-import { Autocomplete } from "@material-ui/lab";
 import { unwrapResult } from "@reduxjs/toolkit";
 import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import Alert from "../../components/Alert";
 import Button from "../../components/Button";
-import TextField from "../../components/TextField";
 import { updateUserProfile } from "./index";
 import ProfileTextInput from "./ProfileTextInput";
 import { UpdateUserProfileData } from "../../service/user";
@@ -74,29 +72,6 @@ export default function EditProfileForm() {
             defaultValue={user.occupation}
             inputRef={register}
           />
-          <Controller
-            control={control}
-            defaultValue={user.languagesList}
-            name="languages"
-            render={({ onChange, value }) => (
-              <Autocomplete
-                freeSolo
-                multiple
-                onChange={(_, value) => onChange(value)}
-                value={value}
-                options={["French", "English"]}
-                open={true}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    variant="outlined"
-                    helperText="Press 'Enter' to add"
-                    label="Languages"
-                  />
-                )}
-              />
-            )}
-          />
 
           <Controller
             control={control}
@@ -142,47 +117,29 @@ export default function EditProfileForm() {
             defaultValue={user.countriesVisitedList}
             name="countriesVisited"
             render={({ onChange, value }) => (
-              <Autocomplete
-                disableClearable={false}
-                freeSolo
-                multiple
+              <ProfileTagInput
                 onChange={(_, value) => onChange(value)}
                 value={value}
-                open={false}
-                options={[""]}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    helperText="Press 'Enter' to add"
-                    label="Countries I visited"
-                  />
-                )}
+                options={["Australia", "Austria", "Angola"]}
+                label="Countries I've Visited"
               />
             )}
           />
+
           <Controller
             control={control}
             defaultValue={user.countriesLivedList}
             name="countriesLived"
             render={({ onChange, value }) => (
-              <Autocomplete
-                disableClearable={false}
-                freeSolo
-                multiple
+              <ProfileTagInput
                 onChange={(_, value) => onChange(value)}
                 value={value}
-                open={false}
-                options={[""]}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    helperText="Press 'Enter' to add"
-                    label="Countries I lived in"
-                  />
-                )}
+                options={["Australia", "Austria", "Angola"]}
+                label="Countries I've Lived In"
               />
             )}
           />
+
           <div className={classes.buttonContainer}>
             <Button
               type="submit"
--- app/frontend/src/features/profile/ProfileTagInput.tsx
+++ app/frontend/src/features/profile/ProfileTagInput.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import {
+  Checkbox,
+  IconButton,
   InputBase,
   ButtonBase,
   Popper,
+  fade,
   makeStyles,
   createStyles,
 } from "@material-ui/core";
 import Autocomplete, {
   AutocompleteCloseReason,
 } from "@material-ui/lab/Autocomplete";
-import { DoneIcon, CloseIcon, ExpandMore } from "../../components/Icons";
+import { CloseIcon, ExpandMore } from "../../components/Icons";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -17,7 +20,6 @@ const useStyles = makeStyles((theme) =>
       border: "1px solid rgba(27,31,35,.15)",
       boxShadow: "0 3px 12px rgba(27,31,35,.15)",
       borderRadius: 3,
-      width: "100%",
       zIndex: 1,
       color: "#586069",
       backgroundColor: "#f6f8fa",
@@ -28,19 +30,65 @@ const useStyles = makeStyles((theme) =>
       borderWidth: 1,
       borderStyle: "solid",
       padding: "18.5px 14px",
+      margin: theme.spacing(1, 0),
       width: "100%",
       fontSize: "16px",
       justifyContent: "space-between",
     },
     tag: {
       padding: "0 14px",
-      margin: "18.5px 0",
+      margin: theme.spacing(1, 0),
       fontSize: "16px",
       display: "flex",
       alignItems: "center",
     },
     tagLabel: {
       marginLeft: theme.spacing(1),
+    },
+    header: {
+      borderBottom: "1px solid #e1e4e8",
+      padding: "8px 10px",
+      fontWeight: 600,
+    },
+    inputBase: {
+      padding: 10,
+      width: "100%",
+      borderBottom: "1px solid #dfe2e5",
+      "& input": {
+        borderRadius: 4,
+        backgroundColor: theme.palette.common.white,
+        padding: 8,
+        transition: theme.transitions.create(["border-color", "box-shadow"]),
+        border: "1px solid #ced4da",
+        fontSize: 14,
+        "&:focus": {
+          boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+          borderColor: theme.palette.primary.main,
+        },
+      },
+    },
+    paper: {
+      boxShadow: "none",
+      margin: 0,
+      color: "#586069",
+    },
+    option: {
+      minHeight: "auto",
+      alignItems: "flex-start",
+      padding: 8,
+      '&[aria-selected="true"]': {
+        backgroundColor: "transparent",
+      },
+      '&[data-focus="true"]': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+    popperDisablePortal: {
+      position: "relative",
+    },
+    checkbox: {
+      padding: 0,
+      marginRight: theme.spacing(1),
     },
   })
 );
@@ -87,6 +135,13 @@ export default function ProfileTagInput({
     setAnchorEl(null);
   };
 
+  const handleRemove = (tag: string) => {
+    onChange(
+      null,
+      value.filter((v) => v !== tag)
+    );
+  };
+
   const open = Boolean(anchorEl);
   const id = open ? "github-label" : undefined;
 
@@ -105,7 +160,13 @@ export default function ProfileTagInput({
       </ButtonBase>
       {valueState.map((tag) => (
         <div key={tag} className={classes.tag}>
-          <CloseIcon fontSize="small" />
+          <IconButton
+            aria-label={`Remove ${tag}`}
+            edge="start"
+            onClick={() => handleRemove(tag)}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
           <span className={classes.tagLabel}>{tag}</span>
         </div>
       ))}
@@ -116,28 +177,43 @@ export default function ProfileTagInput({
         placement="bottom-start"
         className={classes.popper}
       >
+        <div className={classes.header}>Press 'Enter' to add</div>
         <Autocomplete
-          disableClearable={false}
           open
+          onClose={handleClose}
           freeSolo
           multiple
-          onClose={handleClose}
-          onChange={onChange}
-          value={value}
-          options={value.concat(options)}
-          renderInput={(params) => <InputBase {...params} autoFocus />}
+          classes={{
+            paper: classes.paper,
+            option: classes.option,
+            popperDisablePortal: classes.popperDisablePortal,
+          }}
+          onChange={(_, newValue) => {
+            setPendingValue(newValue);
+          }}
+          value={pendingValue}
+          renderInput={(params) => (
+            <InputBase
+              ref={params.InputProps.ref}
+              inputProps={params.inputProps}
+              autoFocus
+              className={classes.inputBase}
+            />
+          )}
           disableCloseOnSelect
           disablePortal
+          renderTags={() => null}
           noOptionsText="No Languages"
+          options={options}
           renderOption={(option, { selected }) => (
             <>
-              <DoneIcon
-                style={{ visibility: selected ? "visible" : "hidden" }}
+              <Checkbox
+                color="primary"
+                size="small"
+                classes={{ root: classes.checkbox }}
+                checked={selected}
               />
-              <div>{option}</div>
-              <CloseIcon
-                style={{ visibility: selected ? "visible" : "hidden" }}
-              />
+              {option}
             </>
           )}
         />
