558bcdc7afc309dd9fa646b5343953a08ce07929
Almenon <almenon214@gmail.com>
2020-12-09 05:05:18 -0800

    simplify _abort_if_terrible_password

--- app/backend/src/couchers/servicers/account.py
+++ app/backend/src/couchers/servicers/account.py
@@ -37,19 +37,19 @@ def _check_password(user, field_name, request, context):
         context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.NO_PASSWORD)
 
 
-def _abort_if_terrible_password(field_name, request, context):
+def _abort_if_terrible_password(password, context):
     """
-    Internal utility function: given a request with a StringValue `field_name` field, aborts if password is unforgivably insecure
+    Internal utility function: given a password, aborts if password is unforgivably insecure
     """
-    if len(getattr(request, field_name).value) < 8:
+    if len(password) < 8:
         context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.PASSWORD_TOO_SHORT)
 
-    if len(getattr(request, field_name).value) > 256:
+    if len(password) > 256:
         # Hey, what are you trying to do? Give us a DDOS attack?
         context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.PASSWORD_TOO_LONG)
 
     # check for most common weak passwords (not meant to be an exhaustive check!)
-    if getattr(request, field_name).value.lower() in ("password", "12345678", "couchers", "couchers1"):
+    if password.lower() in ("password", "12345678", "couchers", "couchers1"):
         context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.INSECURE_PASSWORD)
 
 
@@ -74,7 +74,7 @@ class Account(account_pb2_grpc.AccountServicer):
                 # the user wants to unset their password
                 user.hashed_password = None
             else:
-                _abort_if_terrible_password("new_password", request, context)
+                _abort_if_terrible_password(request.new_password, context)
                 user.hashed_password = hash_password(request.new_password.value)
 
             session.commit()
