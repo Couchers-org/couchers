d4f398cbb2c63015c0b46710482d79494bb7ae61
amdwit <witinformatisering@gmail.com>
2020-12-11 21:34:51 +0000

    add scripts to fill the database

--- app/backend/src/createusers.py
+++ app/backend/src/createusers.py
@@ -17,7 +17,6 @@ from couchers.models import (
 )
 from couchers import config
 
-import names
 import random
 from couchers.crypto import hash_password
 from couchers.servicers.api import hostingstatus2sql
@@ -27,13 +26,13 @@ from datetime import date
 from couchers.db import get_user_by_field, session_scope
 import logging
 from faker import Faker
+
 fake = Faker()
 
 logger = logging.getLogger(__name__)
 
-
 cities = []
-countries=set()
+countries = set()
 worldpopulation = 0
 
 file_name = 'src/data/worldcities.csv'
@@ -51,13 +50,17 @@ with open(file_name, "r") as file:
 
 useramount = 1000000
 users = []
+hashedpassword = hash_password('MAGA2020')
+
 
 def getsomecountries(k):
-    return random.choices(list(countries),k=k)
+    return random.choices(list(countries), k=k)
+
 
 config.check_config()
 
 populationperuser = worldpopulation / useramount
+
 
 def generate_dummy_data():
     try:
@@ -69,15 +72,16 @@ def generate_dummy_data():
                 citylong = float(city[3])
                 for usernumber in range(0, usersforthiscity):
                     gender = random.choice(['Male', 'Female'])
-                    username = names.get_full_name(gender.lower())
+                    username = fake.name_male() if gender == 'Male' else fake.name_female()
                     userlat = citylat + (random.random() - .5) / 10
                     userlong = citylong + (random.random() - .5) / 10
                     new_user = User(
-                        username=username.lower().replace(' ', '')+str(random.random()),
-                        email=username.lower().replace(' ', '') +'_'+str(usernumber)+'_'+str(city[10])+ '@couchers.org',
-                        hashed_password=hash_password(username[0]),
+                        username=username.lower().replace(' ', '') + str(random.random()),
+                        email=username.lower().replace(' ', '') + '_' + str(usernumber) + '_' + str(
+                            city[10]) + '@couchers.org',
+                        hashed_password=hashedpassword,
                         name=username,
-                        city=city[0]+', '+city[4],
+                        city=city[0] + ', ' + city[4],
                         geom=create_coordinate(userlat, userlong),
                         geom_radius=random.random() / 10,
                         verification=random.random(),
@@ -90,15 +94,16 @@ def generate_dummy_data():
                         occupation="occupation",
                         about_me=fake.text(),
                         about_place=fake.text(),
-                        color=random.choice(['red','blue','white',None]),
+                        color=fake.safe_color_name(),
                         countries_visited="|".join(getsomecountries(32)),
                         countries_lived="|".join(getsomecountries(8)),
-                        hosting_status=hostingstatus2sql[random.randint(1,5)]
+                        hosting_status=hostingstatus2sql[random.randint(1, 5)]
                     )
                     session.add(new_user)
                     session.commit()
 
     except Exception as e:
-        logger.error("Failed to insert dummy data",e)
+        logger.error("Failed to insert dummy data", e)
+
 
 generate_dummy_data()
