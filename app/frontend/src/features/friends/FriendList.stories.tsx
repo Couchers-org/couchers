a90e0ebf1a18ea9bca492e2222b608043d91d740
Darren Vong <darrenvong@users.noreply.github.com>
2020-12-12 02:46:22 +0000

    feat: FriendList storybook entry

---
+++ app/frontend/src/features/friends/FriendList.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "../../stories/__mocks__/store";
+import FriendList from "./FriendList";
+
+export default {
+  title: "FriendList",
+  component: FriendList,
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </Provider>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<{}> = () => (
+  <div style={{ width: "50%" }}>
+    <FriendList />
+  </div>
+);
+
+export const WithFriends = Template.bind({});
+WithFriends.args = {};
