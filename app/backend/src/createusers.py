a0474560eaa1195d0a3e6983a9f21c1c630af6d4
amdwit <witinformatisering@gmail.com>
2020-12-12 12:07:01 +0000

    add scripts to fill the database

--- app/backend/src/createusers.py
+++ app/backend/src/createusers.py
@@ -37,25 +37,26 @@ countries = set()
 worldpopulation = 0
 
 # data from https://simplemaps.com/data/world-cities
-file_name = 'src/data/worldcities.csv'
+file_name = "src/data/worldcities.csv"
 with open(file_name, "r") as file:
     reader = csv.reader(file)
     try:
         for row in reader:
-            if row[0] != 'city' and row[9] != '':
+            if row[0] != "city" and row[9] != "":
                 cities.append(row)
                 worldpopulation += int(float(row[9]))
                 countries.add(row[4])
     except csv.Error as e:
-        sys.exit('file {}, line {}: {}'.format(file_name, reader.line_num, e))
+        sys.exit("file {}, line {}: {}".format(file_name, reader.line_num, e))
 
 useramount = 1000000
 users = []
-hashedpassword = hash_password('password')
+hashedpassword = hash_password("password")
 
 
 def getsomecountries(k):
     return random.choices(list(countries), k=k)
+
 
 config.check_config()
 
@@ -73,30 +74,30 @@ def generate_dummy_data():
                 citylat = float(city[2])
                 citylong = float(city[3])
                 for usernumber in range(0, usersforthiscity):
-                    gender = random.choice(['Male', 'Female'])
-                    username = fake.name_male() if gender == 'Male' else fake.name_female()
+                    gender = random.choice(["Male", "Female"])
+                    username = fake.name_male() if gender == "Male" else fake.name_female()
                     userlat = citylat + (2 * random.random() - 1) * cityradius / meterperdegree
                     userlong = citylong + (2 * random.random() - 1) * cityradius / meterperdegree
                     new_user = User(
-                        username=username.lower().replace(' ', '') + str(random.random()),
-                        email=username.lower().replace(' ', '') + '_' + str(random.random()) + '@couchers.org',
+                        username=username.lower().replace(" ", "") + str(random.random()),
+                        email=username.lower().replace(" ", "") + "_" + str(random.random()) + "@couchers.org",
                         hashed_password=hashedpassword,
                         name=username,
-                        city=city[0] + ', ' + city[4],
+                        city=city[0] + ", " + city[4],
                         geom=create_coordinate(userlat, userlong),
                         geom_radius=random.random() * cityradius / 10,
                         verification=random.random(),
                         community_standing=random.random(),
-                        birthdate=fake.date_between('-100y', '-15y'),
+                        birthdate=fake.date_between("-100y", "-15y"),
                         gender=gender,
-                        languages=', '.join(set([fake.language_name() for i in range(random.randrange(4))])),
+                        languages=", ".join(set([fake.language_name() for i in range(random.randrange(4))])),
                         occupation=fake.job(),
                         about_me=fake.text(),
                         about_place=fake.text(),
                         color=fake.safe_color_name(),
                         countries_visited="|".join(getsomecountries(32)),
                         countries_lived="|".join(getsomecountries(8)),
-                        hosting_status=hostingstatus2sql[random.randint(1, 5)]
+                        hosting_status=hostingstatus2sql[random.randint(1, 5)],
                     )
                     users.append(new_user)
                     if len(users) == 10000:
