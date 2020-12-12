6c6220a58c23741dd1a93a3b909869a80fadde39
Darren Vong <darrenvong@users.noreply.github.com>
2020-12-12 02:47:20 +0000

    feat: configure Storybook to use app theme
    Closes #403

--- app/frontend/.storybook/preview.js
+++ app/frontend/.storybook/preview.js
@@ -1,4 +1,15 @@
+import { ThemeProvider } from "@material-ui/core";
+import { theme } from "../src/theme";
+import "../src/App.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story, context) => (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  ),
+];
