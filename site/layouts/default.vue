4c885e23d2b29b91d12613b1d99288d5215943d0
ItsiW <itsi148@gmail.com>
2020-12-15 23:55:05 +1100

    Fix spelling link description

--- site/layouts/default.vue
+++ site/layouts/default.vue
@@ -128,7 +128,7 @@ export default {
     }
   },
   head () {
-    const description = "The new platform for couch-surfers. Non-profit. Community-focussed. Well built."
+    const description = "The new platform for couch-surfers. Non-profit. Community-focused. Well built."
     return {
       title: "Home",
       titleTemplate: '%s | Couchers.org',
