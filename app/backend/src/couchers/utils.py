4fdb50dd252931ca53a0c67491d600596c6750ee
Aapeli <git@aapelivuorinen.com>
2020-12-17 00:50:12 +0200

    Tweak cookie settings

--- app/backend/src/couchers/utils.py
+++ app/backend/src/couchers/utils.py
@@ -84,10 +84,10 @@ def create_session_cookie(token, expiry):
     # path so that it's accessible for all API requests, otherwise defaults to something like /org.couchers.auth/
     cookie["path"] = "/"
     # send only on requests from first-party domains
-    cookie["samesite"] = "Strict"
-    if not config["DEV"]:
-        # only set cookie on HTTPS sites in production
-        cookie["secure"] = True
+    # cookie["samesite"] = "Strict"
+    cookie["samesite"] = "None"
+    # only set cookie on HTTPS sites in production
+    cookie["secure"] = True
     # not accessible from javascript
     cookie["httponly"] = True
 
