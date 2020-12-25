10c131c9a61c5a7738ae1bff17a403dd0d32e375
Aapeli <git@aapelivuorinen.com>
2020-12-25 19:10:19 +0200

    Implement IsOnLand RPC

--- app/backend/src/couchers/servicers/gis.py
+++ app/backend/src/couchers/servicers/gis.py
@@ -2,9 +2,12 @@ import json
 import logging
 
 import grpc
+from sqlalchemy.sql import exists, func
 
 from couchers.db import session_scope
-from pb import gis_pb2_grpc
+from couchers.models import LandPolygon
+from couchers.utils import create_coordinate
+from pb import gis_pb2, gis_pb2_grpc
 from pb.google.api import httpbody_pb2
 
 logger = logging.getLogger(__name__)
@@ -47,3 +50,9 @@ class GIS(gis_pb2_grpc.GISServicer):
                 content_type="application/json",
                 data=data,
             )
+
+    def IsOnLand(self, request, context):
+        with session_scope() as session:
+            coordinate = create_coordinate(request.lat, request.lng)
+            on_land = session.query(exists().where(func.ST_Contains(LandPolygon.geom, coordinate))).scalar()
+        return gis_pb2.IsOnLandRes(on_land=on_land)
