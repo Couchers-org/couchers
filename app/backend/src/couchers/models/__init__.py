70bf5c44a2609a362e0e766f82ea598567833346
amdwit <witinformatisering@gmail.com>
2020-12-29 12:34:20 +0000

    process pr comments for base.py, absolute import, snake_case filename

---
+++ app/backend/src/couchers/models/__init__.py
@@ -0,0 +1,1 @@
+from couchers.models.core import *
---
+++ app/backend/src/couchers/models/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+Base = declarative_base(metadata=meta)
