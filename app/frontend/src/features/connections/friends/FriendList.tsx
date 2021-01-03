5b51eab9b2845ca9213221894035485e697d5e98
Darren Vong <darrenvong@users.noreply.github.com>
2021-01-03 20:14:10 +0000

    feat: refactor FriendList to use FriendTile and FriendSummaryView

--- app/frontend/src/features/connections/friends/FriendList.tsx
+++ app/frontend/src/features/connections/friends/FriendList.tsx
@@ -1,96 +1,43 @@
-import {
-  Box,
-  Card,
-  IconButton,
-  makeStyles,
-  Typography,
-} from "@material-ui/core";
+import { Box, IconButton } from "@material-ui/core";
 import React from "react";
-import { Link } from "react-router-dom";
-import Alert from "../../../components/Alert";
-import CircularProgress from "../../../components/CircularProgress";
-import TextBody from "../../../components/TextBody";
-import useFriendsBaseStyles from "./useFriendsBaseStyles";
 import { CloseIcon, EmailIcon } from "../../../components/Icons";
+import FriendSummaryView from "./FriendSummaryView";
+import FriendTile from "./FriendTile";
 import useFriendList from "./useFriendList";
 
-const useStyles = makeStyles((theme) => ({
-  actionButton: {
-    borderRadius: "100%",
-    minWidth: "auto",
-    height: theme.spacing(3),
-    width: theme.spacing(3),
-    padding: 0,
-  },
-  container: {
-    "& > :last-child": {
-      marginBottom: theme.spacing(1),
-    },
-  },
-  friendItem: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: `0 ${theme.spacing(1)}`,
-  },
-  friendLink: {
-    color: theme.palette.text.primary,
-    textDecoration: "none",
-  },
-  noFriendsText: {
-    marginLeft: theme.spacing(1),
-  },
-}));
+function CurrentFriendAction() {
+  return (
+    <Box>
+      <IconButton aria-label="Direct message">
+        <EmailIcon />
+      </IconButton>
+      <IconButton aria-label="Unfriend">
+        <CloseIcon />
+      </IconButton>
+    </Box>
+  );
+}
 
 function FriendList() {
-  const baseClasses = useFriendsBaseStyles();
-  const classes = useStyles();
   const { errors, isLoading, isError, data: friends } = useFriendList();
 
   return (
-    <Card>
-      <Box className={classes.container}>
-        <Typography className={baseClasses.header} variant="h2">
-          Your friends
-        </Typography>
-        {isError && (
-          <Alert className={baseClasses.errorAlert} severity="error">
-            {errors.join("\n")}
-          </Alert>
+    <FriendTile
+      title="Your friends"
+      errorMessage={isError ? errors.join("\n") : null}
+      isLoading={isLoading}
+      hasData={!!(friends && friends.length)}
+      noDataMessage="No friends available :("
+    >
+      {friends &&
+        friends.map((friend) =>
+          friend ? (
+            <FriendSummaryView key={friend.userId} friend={friend}>
+              <CurrentFriendAction />
+            </FriendSummaryView>
+          ) : null
         )}
-        {isLoading ? (
-          <CircularProgress className={baseClasses.circularProgress} />
-        ) : friends && friends.length ? (
-          friends.map((friend) =>
-            friend ? (
-              <Box className={classes.friendItem} key={friend.userId}>
-                <Link
-                  className={classes.friendLink}
-                  to={`/user/${friend.username}`}
-                >
-                  <Typography variant="h2" component="h3">
-                    {friend.name}
-                  </Typography>
-                  <TextBody>@{friend.username}</TextBody>
-                </Link>
-                <Box>
-                  <IconButton aria-label="Direct message">
-                    <EmailIcon />
-                  </IconButton>
-                  <IconButton aria-label="Unfriend">
-                    <CloseIcon />
-                  </IconButton>
-                </Box>
-              </Box>
-            ) : null
-          )
-        ) : (
-          <TextBody className={classes.noFriendsText}>
-            No friends available :(
-          </TextBody>
-        )}
-      </Box>
-    </Card>
+    </FriendTile>
   );
 }
 
