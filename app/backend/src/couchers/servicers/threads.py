1df908f963f4bd102c3cb31d36eea0429d203d8b
Christian Häggström <kalvdans@gmail.com>
2021-01-06 23:51:34 +0100

    pagination test and implementation

--- app/backend/src/couchers/servicers/threads.py
+++ app/backend/src/couchers/servicers/threads.py
@@ -27,38 +27,53 @@ def pack_thread_id(database_id, depth):
 
 class Communities(communities_pb2_grpc.CommunitiesServicer):
     def GetThread(self, request, context):
+        database_id, depth = unpack_thread_id(request.thread_id)
+        page_size = request.page_size or 1000
+        page_start = unpack_thread_id(int(request.page_token))[0] if request.page_token else 2**50
+
         with session_scope() as session:
-            database_id, depth = unpack_thread_id(request.thread_id)
             if depth == 0:
-                res = (session.query(Comment,
-                                     func.count(Reply.id))
+                res = (session.query(Comment, func.count(Reply.id))
+                       .outerjoin(Reply, Reply.comment_id == Comment.id)
                        .filter(Comment.thread_id == database_id)
-                       .filter(Reply.comment_id == Comment.id)
+                       .filter(Comment.id < page_start)
                        .group_by(Comment.id)
                        .order_by(Comment.created.desc())
+                       .limit(page_size + 1)
                        .all())
-                replies=[
+                replies = [
                     communities_pb2.Reply(thread_id=pack_thread_id(r.id, 1),
                                           content=r.content,
                                           author_user_id=r.author_user_id,
                                           created_time=Timestamp_from_datetime(r.created),
                                           num_replies=n)
-                    for r, n in res]
+                    for r, n in res[:page_size]]
 
             elif depth == 1:
-                res = session.query(Reply).filter(Reply.comment_id == database_id).order_by(Reply.created.desc()).all()
-                replies=[
+                res = (session.query(Reply)
+                       .filter(Reply.comment_id == database_id)
+                       .filter(Reply.id < page_start)
+                       .order_by(Reply.created.desc())
+                       .limit(page_size + 1)
+                       .all())
+                replies = [
                     communities_pb2.Reply(thread_id=pack_thread_id(r.id, 2),
                                           content=r.content,
                                           author_user_id=r.author_user_id,
                                           created_time=Timestamp_from_datetime(r.created),
                                           num_replies=0)
-                    for r in res]
+                    for r in res[:page_size]]
 
             else:
                 context.abort(grpc.StatusCode.INVALID_ARGUMENT, "bad depth")
 
-        return communities_pb2.GetThreadRes(replies=replies)
+            if len(res) > page_size:
+                # There's more!
+                next_page_token = str(replies[-1].thread_id)
+            else:
+                next_page_token = ""
+
+        return communities_pb2.GetThreadRes(replies=replies, next_page_token=next_page_token)
 
     def PostReply(self, request, context):
         with session_scope() as session:
--- app/backend/src/tests/test_threads.py
+++ app/backend/src/tests/test_threads.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 import contextlib
+import string
 
 import grpc
 import pytest
@@ -41,6 +42,10 @@ def test_threads_basic(db):
         session.add(Thread(id=1, title="foo"))
 
     with communities_session(token1) as api:
+        bat_id = api.PostReply(communities_pb2.PostReplyReq(
+            thread_id=PARENT_THREAD_ID,
+            content="bat")).thread_id
+
         cat_id = api.PostReply(communities_pb2.PostReplyReq(
             thread_id=PARENT_THREAD_ID,
             content="cat")).thread_id
@@ -60,7 +65,7 @@ def test_threads_basic(db):
 
         # Make some queries
         ret = api.GetThread(communities_pb2.GetThreadReq(thread_id=PARENT_THREAD_ID))
-        assert len(ret.replies) == 2
+        assert len(ret.replies) == 3
         assert ret.next_page_token == ""
         assert ret.replies[0].thread_id == dog_id
         assert ret.replies[0].content == "dog"
@@ -72,6 +77,11 @@ def test_threads_basic(db):
         assert ret.replies[1].author_user_id == user1.id
         assert ret.replies[1].num_replies == 3
 
+        assert ret.replies[2].thread_id == bat_id
+        assert ret.replies[2].content == "bat"
+        assert ret.replies[2].author_user_id == user1.id
+        assert ret.replies[2].num_replies == 0
+
         ret = api.GetThread(communities_pb2.GetThreadReq(thread_id=cat_id))
         assert len(ret.replies) == 3
         assert ret.next_page_token == ""
@@ -81,3 +91,46 @@ def test_threads_basic(db):
         assert len(ret.replies) == 3
         assert ret.next_page_token == ""
         assert [reply.thread_id for reply in ret.replies] == dogs[::-1]
+
+
+def pagination_test(api, parent_id):
+    # Post some data
+    for c in string.ascii_lowercase:
+        api.PostReply(communities_pb2.PostReplyReq(
+            thread_id=parent_id,
+            content=c))
+
+    # Get it with pagination
+    token = ""
+    accumulator = []
+    while True:
+        ret = api.GetThread(communities_pb2.GetThreadReq(thread_id=parent_id,
+                                                         page_size=5,
+                                                         page_token=token))
+        assert 0 < len(ret.replies) <= 5
+        accumulator += [x.content for x in ret.replies]
+        assert len(accumulator) <= len(string.ascii_lowercase)
+        if len(accumulator) == len(string.ascii_lowercase):
+            assert ret.next_page_token == ""
+            break
+        assert ret.next_page_token != ""
+        assert len(ret.replies) == 5
+        token = ret.next_page_token
+
+    assert ''.join(reversed(accumulator)) == string.ascii_lowercase
+
+    return ret.replies[0].thread_id   # to be used as a test one level deeper
+
+
+def test_threads_pagination(db):
+    user1, token1 = generate_user()
+
+    PARENT_THREAD_ID = 10
+
+    # Create a dummy Thread (should be replaced by pages later on)
+    with session_scope() as session:
+        session.add(Thread(id=1, title="foo"))
+
+    with communities_session(token1) as api:
+        comment_id = pagination_test(api, PARENT_THREAD_ID)
+        pagination_test(api, comment_id)
