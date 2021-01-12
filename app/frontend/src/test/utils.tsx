11ab28928eb0d956d5e030df6dca58bd752c7f7e
lucaslcode <lucaslevin@gmail.com>
2021-01-12 00:44:02 +0000

    leftover import

--- app/frontend/src/test/utils.tsx
+++ app/frontend/src/test/utils.tsx
@@ -1,5 +1,3 @@
-import { createAction } from "@reduxjs/toolkit";
-
 export function addDefaultUser() {
   window.localStorage.setItem("auth.authenticated", JSON.stringify(true));
   window.localStorage.setItem("auth.jailed", JSON.stringify(false));
@@ -8,8 +6,6 @@ export function addDefaultUser() {
     JSON.stringify(defaultUser.userId)
   );
 }
-
-export const reset = createAction("reset");
 
 export function wait(milliSeconds: number) {
   return new Promise((resolve) => setTimeout(resolve, milliSeconds));
