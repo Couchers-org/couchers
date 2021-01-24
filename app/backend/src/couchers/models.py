1d97a90e72ceb2abb51e00059e613660bc304878
ShmuelTreiger <shmueltreiger@gmail.com>
2021-01-24 00:25:27 -0700

    Additional Host Info

--- app/backend/src/couchers/models.py
+++ app/backend/src/couchers/models.py
@@ -54,6 +54,20 @@ class SmokingLocation(enum.Enum):
     no = 4
 
 
+class SleepingArrangement(enum.Enum):
+    private = 1
+    common = 2
+    shared_room = 3
+    shared_space = 4
+
+
+class ParkingDetails(enum.Enum):
+    free_onsite = 1
+    free_offsite = 2
+    paid_onsite = 3
+    paid_offsite = 4
+
+
 class User(Base):
     """
     Basic user and profile details
@@ -119,14 +133,28 @@ class User(Base):
     # hosting preferences
     max_guests = Column(Integer, nullable=True)
     last_minute = Column(Boolean, nullable=True)
+    has_pets = Column(Boolean, nullable=True)
     accepts_pets = Column(Boolean, nullable=True)
+    pet_details = Column(String, nullable=True)
+    has_kids = Column(Boolean, nullable=True)
     accepts_kids = Column(Boolean, nullable=True)
+    kid_details = Column(String, nullable=True)
+    has_housemates = Column(Boolean, nullable=True)
+    housemates_details = Column(String, nullable=True)
     wheelchair_accessible = Column(Boolean, nullable=True)
     smoking_allowed = Column(Enum(SmokingLocation), nullable=True)
+    smokes_at_home = Column(Boolean, nullable=True)
+    drinking_allowed = Column(Boolean, nullable=True)
+    drinks_at_home = Column(Boolean, nullable=True)
+    other_host_info = Column(String, nullable=True)
 
-    sleeping_arrangement = Column(String, nullable=True)
+    sleeping_arrangement = Column(Enum(SleepingArrangement), nullable=True)
+    sleeping_details = Column(String, nullable=True)
     area = Column(String, nullable=True)
     house_rules = Column(String, nullable=True)
+    parking = Column(Boolean, nullable=True)
+    parking_details = Column(Enum(ParkingDetails), nullable=True)
+    camping_OK = Column(Boolean, nullable=True)
 
     accepted_tos = Column(Integer, nullable=False, default=0)
 
