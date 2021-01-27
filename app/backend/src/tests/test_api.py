c4b262f189f33306fd9919dce53bc3795de40bef
ShmuelTreiger <shmueltreiger@gmail.com>
2021-01-26 19:12:24 -0700

    Added tests for other host info

--- app/backend/src/tests/test_api.py
+++ app/backend/src/tests/test_api.py
@@ -677,6 +677,7 @@ def test_hosting_preferences(db):
         assert not res.HasField("smokes_at_home")
         assert not res.HasField("drinking_allowed")
         assert not res.HasField("drinks_at_home")
+        assert not res.HasField("other_host_info")
         assert not res.HasField("sleeping_arrangement")
         assert res.HasField("area")
         assert not res.HasField("house_rules")
@@ -707,6 +708,7 @@ def test_hosting_preferences(db):
         assert not res.HasField("smokes_at_home")
         assert not res.HasField("drinking_allowed")
         assert not res.HasField("drinks_at_home")
+        assert not res.HasField("other_host_info")
         assert not res.HasField("sleeping_arrangement")
         assert res.HasField("area")
         assert res.area.value == "Public transit:/n/nParking:/n/nAccessibility:/n!"
@@ -740,6 +742,7 @@ def test_hosting_preferences(db):
         assert not res.HasField("smokes_at_home")
         assert not res.HasField("drinking_allowed")
         assert not res.HasField("drinks_at_home")
+        assert not res.HasField("other_host_info")
         assert not res.HasField("sleeping_arrangement")
         assert res.area.value == "area!"
         assert not res.HasField("house_rules")
