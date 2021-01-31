6dc74fc701c9c58138a6b45ca78ddb95f3886b59
Aapeli <git@aapelivuorinen.com>
2021-01-30 19:01:57 -0500

    Add scheduler continuation test

--- app/backend/src/tests/test_bg_jobs.py
+++ app/backend/src/tests/test_bg_jobs.py
@@ -1,7 +1,8 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from unittest.mock import create_autospec, patch
 
 import pytest
+import pytz
 from google.protobuf import empty_pb2
 from sqlalchemy.sql import func
 
@@ -10,7 +11,15 @@ from couchers.email import queue_email
 from couchers.email.dev import print_dev_email
 from couchers.jobs.enqueue import queue_job
 from couchers.jobs.worker import _run_job_and_schedule, process_job, run_scheduler, service_jobs
-from couchers.models import BackgroundJob, BackgroundJobState, BackgroundJobType, Email, LoginToken, SignupToken
+from couchers.models import (
+    BackgroundJob,
+    BackgroundJobState,
+    BackgroundJobType,
+    Email,
+    LoginToken,
+    RepeatedJob,
+    SignupToken,
+)
 from couchers.tasks import send_login_email
 from pb import auth_pb2
 from tests.test_fixtures import auth_api_session, db, generate_user, testconfig
@@ -192,6 +201,93 @@ def test_scheduler(db):
         assert session.query(BackgroundJob).filter(BackgroundJob.state != BackgroundJobState.pending).count() == 0
 
 
+def test_scheduler_continue(db):
+    MOCK_SCHEDULE = [
+        (BackgroundJobType.purge_login_tokens, timedelta(seconds=7)),
+        (BackgroundJobType.purge_signup_tokens, timedelta(seconds=11)),
+    ]
+
+    current_time = 0
+    end_time = 25
+
+    class EndOfTime(Exception):
+        pass
+
+    def mock_monotonic():
+        nonlocal current_time
+        return current_time
+
+    def mock_sleep(seconds):
+        nonlocal current_time
+        current_time += seconds
+        if current_time > end_time:
+            raise EndOfTime()
+
+    def mock_now():
+        nonlocal current_time
+        return pytz.utc.localize(datetime.utcfromtimestamp(current_time))
+
+    realized_schedule = []
+
+    def mock_run_job_and_schedule(sched, schedule_id):
+        nonlocal current_time
+        realized_schedule.append((current_time, schedule_id))
+        _run_job_and_schedule(sched, schedule_id)
+
+    with patch("couchers.jobs.worker._run_job_and_schedule", mock_run_job_and_schedule):
+        with patch("couchers.jobs.worker.SCHEDULE", MOCK_SCHEDULE):
+            with patch("couchers.jobs.worker.monotonic", mock_monotonic):
+                with patch("couchers.jobs.worker.sleep", mock_sleep):
+                    with patch("couchers.jobs.worker.now", mock_now):
+                        with patch("couchers.jobs.worker.func.now", mock_now):
+                            with pytest.raises(EndOfTime):
+                                run_scheduler()
+
+                            assert realized_schedule == [
+                                (0.0, 0),
+                                (0.0, 1),
+                                (7.0, 0),
+                                (11.0, 1),
+                                (14.0, 0),
+                                (21.0, 0),
+                                (22.0, 1),
+                            ]
+
+                            with session_scope() as session:
+                                assert (
+                                    session.query(BackgroundJob)
+                                    .filter(BackgroundJob.state == BackgroundJobState.pending)
+                                    .count()
+                                    == 7
+                                )
+                                assert (
+                                    session.query(BackgroundJob)
+                                    .filter(BackgroundJob.state != BackgroundJobState.pending)
+                                    .count()
+                                    == 0
+                                )
+
+                            end_time = 70
+                            realized_schedule = []
+
+                            with pytest.raises(EndOfTime):
+                                run_scheduler()
+
+                            assert realized_schedule == [
+                                (28.0, 0),
+                                (33.0, 1),
+                                (35.0, 0),
+                                (42.0, 0),
+                                (44.0, 1),
+                                (49.0, 0),
+                                (55.0, 1),
+                                (56.0, 0),
+                                (63.0, 0),
+                                (66.0, 1),
+                                (70.0, 0),
+                            ]
+
+
 def test_job_retry(db):
     queue_job(BackgroundJobType.purge_login_tokens, empty_pb2.Empty())
 
@@ -209,8 +305,7 @@ def test_job_retry(db):
     with patch("couchers.jobs.worker.JOBS", MOCK_JOBS):
         process_job()
         with session_scope() as session:
-            job = session.query(BackgroundJob).one()
-            job.next_attempt_after = func.now()
+            session.query(BackgroundJob).one().next_attempt_after = func.now()
         process_job()
         with session_scope() as session:
             session.query(BackgroundJob).one().next_attempt_after = func.now()
