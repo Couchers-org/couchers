f8c310828f25b535746b1c77d6a942a9189029d6
Aapeli <git@aapelivuorinen.com>
2021-02-03 10:16:04 -0500

    Clean up

--- app/backend/src/app_bg.py
+++ app/backend/src/app_bg.py
@@ -1,10 +1,4 @@
-import logging
-from concurrent import futures
-
-from couchers import config
 from couchers.jobs.worker import start_job_servicer
-
-logger = logging.getLogger(__name__)
 
 
 def run_background():
