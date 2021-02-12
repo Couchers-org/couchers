c3aae484a7a0bc9414eb792e6d44bb7efa841700
Darren Vong <darrenvong@users.noreply.github.com>
2021-02-12 00:41:45 +0000

    feat: switch complete reset password to use react query + tests

--- app/frontend/src/features/auth/password/CompleteResetPasswordPage.tsx
+++ app/frontend/src/features/auth/password/CompleteResetPasswordPage.tsx
@@ -1,36 +1,35 @@
 import { Typography } from "@material-ui/core";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 import { Error as GrpcError } from "grpc-web";
 import { useEffect } from "react";
+import { useMutation } from "react-query";
 import { Link, useParams } from "react-router-dom";
 
 import { loginRoute } from "../../../AppRoutes";
 import Alert from "../../../components/Alert";
 import { service } from "../../../service";
-import { useIsMounted, useSafeState } from "../../../utils/hooks";
 
 export default function CompleteResetPasswordPage() {
-  const isMounted = useIsMounted();
-  const [error, setError] = useSafeState<string | null>(isMounted, null);
-  const [loading, setLoading] = useSafeState(isMounted, false);
-  const [success, setSuccess] = useSafeState(isMounted, false);
   const { resetToken } = useParams<{ resetToken?: string }>();
 
-  useEffect(() => {
-    setLoading(true);
-    if (resetToken) {
-      service.account
-        .completePasswordReset(resetToken)
-        .then(() => {
-          setSuccess(true);
-        })
-        .catch((err: GrpcError) => setError(err.message))
-        .then(() => setLoading(false));
-    }
-  }, [resetToken, setError, setLoading, setSuccess]);
+  const {
+    error,
+    isLoading,
+    isSuccess,
+    mutate: completePasswordReset,
+  } = useMutation<Empty, GrpcError, string>((resetToken) =>
+    service.account.completePasswordReset(resetToken)
+  );
 
-  return loading ? (
+  useEffect(() => {
+    if (resetToken) {
+      completePasswordReset(resetToken);
+    }
+  }, [completePasswordReset, resetToken]);
+
+  return isLoading ? (
     <Typography variant="body1">Password reset in progress...</Typography>
-  ) : success ? (
+  ) : isSuccess ? (
     <>
       <Alert severity="success">
         Your password has been reset successfully!
@@ -40,6 +39,6 @@ export default function CompleteResetPasswordPage() {
       </Typography>
     </>
   ) : error ? (
-    <Alert severity="error">Error resetting password: {error}</Alert>
+    <Alert severity="error">Error resetting password: {error.message}</Alert>
   ) : null;
 }
