5e3c5a0bfdd21e1df2c581639f307105dcaadbaa
Tay0108 <kjkosecki@gmail.com>
2021-02-12 16:10:17 +0100

    fix: changing sendField marginBlock to marginTop & bottom (Safari bug)

--- app/frontend/src/features/messages/useSendFieldStyles.ts
+++ app/frontend/src/features/messages/useSendFieldStyles.ts
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 
 const useSendFieldStyles = makeStyles((theme) => ({
-  root: { marginBlock: theme.spacing(2) },
+  root: { marginTop: theme.spacing(2), marginBottom: theme.spacing(2) },
   container: {
     [theme.breakpoints.up("md")]: {
       display: "flex",
@@ -12,7 +12,6 @@ const useSendFieldStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     flexWrap: "wrap",
-    marginBottom: theme.spacing(2),
     "& > button": {
       marginInline: theme.spacing(2),
     },
