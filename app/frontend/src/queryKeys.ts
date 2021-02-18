01d5823656cabf15d75c969bcbd1101a8d0c17bd
lucaslcode <lucaslevin@gmail.com>
2021-02-18 23:09:05 +0000

    Refactor community page to use react-query

--- app/frontend/src/queryKeys.ts
+++ app/frontend/src/queryKeys.ts
@@ -1,1 +1,35 @@
 export const accountInfoQueryKey = "accountInfo";
+
+export const communityKey = (id: number) => ["community", id];
+export const subCommunitiesKey = (communityId: number) => [
+  "subCommunities",
+  communityId,
+];
+export const communityGroupsKey = (communityId: number) => [
+  "communityGroups",
+  communityId,
+];
+export const communityGuidesKey = (communityId: number) => [
+  "communityGuides",
+  communityId,
+];
+export const communityPlacesKey = (communityId: number) => [
+  "communityPlaces",
+  communityId,
+];
+export const communityDiscussionsKey = (communityId: number) => [
+  "communityDiscussions",
+  communityId,
+];
+export const communityAdminsKey = (communityId: number) => [
+  "communityAdmins",
+  communityId,
+];
+export const communityMembersKey = (communityId: number) => [
+  "communityMembers",
+  communityId,
+];
+export const communityNearbyUsersKey = (communityId: number) => [
+  "communityNearbyUsers",
+  communityId,
+];
