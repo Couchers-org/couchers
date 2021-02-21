23b49c9fb53304d3009f0be4cca846ea677f2c95
Darren Vong <darrenvong@users.noreply.github.com>
2021-02-21 01:05:55 +0000

    feat: Storybook entries for account management pages/screens

---
+++ app/frontend/src/features/auth/email/ChangeEmailPage.stories.tsx
@@ -0,0 +1,84 @@
+import { Meta, Story } from "@storybook/react";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import React from "react";
+
+import { GetAccountInfoRes } from "../../../pb/account_pb";
+import { mockedService } from "../../../stories/__mocks__/service";
+import ChangeEmailPage from "./ChangeEmailPage";
+
+export default {
+  title: "Me/Auth/ChangeEmailPage",
+  component: ChangeEmailPage,
+} as Meta;
+
+interface ChangeEmailPageArgs {
+  loginMethod?: GetAccountInfoRes.LoginMethod;
+  simulateGetAccountInfoLoading?: boolean;
+  shouldChangeEmailSucceed?: boolean;
+  shouldGetAccountInfoSucceed?: boolean;
+}
+
+const Template: Story<ChangeEmailPageArgs> = ({
+  loginMethod = GetAccountInfoRes.LoginMethod.PASSWORD,
+  simulateGetAccountInfoLoading = false,
+  shouldChangeEmailSucceed = true,
+  shouldGetAccountInfoSucceed = true,
+} = {}) => {
+  setMocks({
+    loginMethod,
+    simulateGetAccountInfoLoading,
+    shouldChangeEmailSucceed,
+    shouldGetAccountInfoSucceed,
+  });
+  return <ChangeEmailPage />;
+};
+
+export const HasPassword = Template.bind({});
+
+export const NoPassword = Template.bind({});
+NoPassword.args = {
+  loginMethod: GetAccountInfoRes.LoginMethod.MAGIC_LINK,
+};
+
+export const AccountInfoLoading = Template.bind({});
+AccountInfoLoading.args = {
+  simulateGetAccountInfoLoading: true,
+};
+
+export const ErrorGettingAccountInfo = Template.bind({});
+ErrorGettingAccountInfo.args = {
+  shouldGetAccountInfoSucceed: false,
+};
+
+export const ErrorChangingEmail = Template.bind({});
+ErrorChangingEmail.args = {
+  shouldChangeEmailSucceed: false,
+};
+
+export const ErrorChangingEmail2 = Template.bind({});
+ErrorChangingEmail2.storyName = "Error Changing Email (without password)";
+ErrorChangingEmail2.args = {
+  loginMethod: GetAccountInfoRes.LoginMethod.MAGIC_LINK,
+  shouldChangeEmailSucceed: false,
+};
+
+function setMocks({
+  loginMethod,
+  simulateGetAccountInfoLoading,
+  shouldChangeEmailSucceed,
+  shouldGetAccountInfoSucceed,
+}: Required<ChangeEmailPageArgs>) {
+  mockedService.account.getAccountInfo = () =>
+    shouldGetAccountInfoSucceed
+      ? simulateGetAccountInfoLoading
+        ? new Promise(() => void 0)
+        : Promise.resolve({
+            hasPassword: loginMethod === GetAccountInfoRes.LoginMethod.PASSWORD,
+            loginMethod,
+          })
+      : Promise.reject(new Error("Error getting account info"));
+  mockedService.account.changeEmail = () =>
+    shouldChangeEmailSucceed
+      ? Promise.resolve(new Empty())
+      : Promise.reject(new Error("Error completing email change"));
+}
---
+++ app/frontend/src/features/auth/password/ResetPasswordPage.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from "@storybook/react";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import React from "react";
+
+import { mockedService } from "../../../stories/__mocks__/service";
+import ResetPasswordPage from "./ResetPasswordPage";
+
+export default {
+  title: "Me/Auth/ResetPasswordPage",
+  component: ResetPasswordPage,
+} as Meta;
+
+interface ResetPasswordPageArgs {
+  shouldSucceed?: boolean;
+}
+
+const Template: Story<ResetPasswordPageArgs> = ({
+  shouldSucceed = true,
+} = {}) => {
+  setMocks({ shouldSucceed });
+  return <ResetPasswordPage />;
+};
+
+export const Success = Template.bind({});
+
+export const Failed = Template.bind({});
+Failed.args = {
+  shouldSucceed: false,
+};
+
+function setMocks({ shouldSucceed }: Required<ResetPasswordPageArgs>) {
+  mockedService.account.resetPassword = () =>
+    shouldSucceed
+      ? Promise.resolve(new Empty())
+      : Promise.reject(new Error("API error"));
+}
