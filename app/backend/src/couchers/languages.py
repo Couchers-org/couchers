a47fc90f57f0cadd442622315b58890fb0c49f1c
Aapeli <git@aapelivuorinen.com>
2021-02-22 12:44:17 -0500

    Clean up

--- app/backend/src/couchers/languages.py
+++ app/backend/src/couchers/languages.py
@@ -9,7 +9,7 @@ def get_language_list():
     Get list of allowed languages as a set.
     """
     with open(Path(__file__).parent / "constant_data" / "languages.json", "r") as f:
-        return set(language["code"] for language in json.loads(f.read()))
+        return set(language["code"] for language in json.load(f))
 
 
 def language_is_allowed(code):
