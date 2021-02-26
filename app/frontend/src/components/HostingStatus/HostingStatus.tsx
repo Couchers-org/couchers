8e3620bfee73b5d133ccbe49baadf8ab5c4d9456
Darren Vong <darrenvong@users.noreply.github.com>
2021-02-26 19:35:39 +0000

    feat: refactor user summary component to be more reusable

---
+++ app/frontend/src/components/HostingStatus/HostingStatus.tsx
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
+import React from "react";
+
+import { hostingStatusLabels } from "../../features/profile/constants";
+import { HostingStatus as THostingStatus } from "../../pb/api_pb";
+import { CouchIcon } from "../Icons";
+import IconText from "../IconText";
+
+const useStyles = makeStyles({
+  hostingAbilityContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
+export interface HostingStatusProps {
+  hostingStatus?: THostingStatus;
+}
+
+export default function HostingStatus({ hostingStatus }: HostingStatusProps) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.hostingAbilityContainer}>
+      {hostingStatus ? (
+        <IconText icon={CouchIcon} text={hostingStatusLabels[hostingStatus]} />
+      ) : (
+        <Skeleton width={100} />
+      )}
+    </div>
+  );
+}
--- app/frontend/src/components/UserSummary.tsx
+++ app/frontend/src/components/UserSummary.tsx
@@ -8,34 +8,28 @@ import { Skeleton } from "@material-ui/lab";
 import React from "react";
 
 import { COMMUNITY_STANDING } from "../features/constants";
-import { hostingStatusLabels } from "../features/profile/constants";
 import { User } from "../pb/api_pb";
 import Avatar from "./Avatar";
 import ScoreBar from "./Bar/ScoreBar";
-import { CouchIcon } from "./Icons";
-import IconText from "./IconText";
 
 export const useStyles = makeStyles((theme) => ({
-  root: { display: "flex", alignItems: "center", padding: 0 },
+  root: { display: "flex", alignItems: "center", padding: 0, width: "100%" },
   title: {
     marginBottom: theme.spacing(1),
   },
-  hostingAbilityContainer: {
-    display: "flex",
-    alignItems: "center",
-  },
   avatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
+    width: theme.spacing(9),
+    height: theme.spacing(9),
     marginInlineEnd: theme.spacing(2),
   },
 }));
 
 interface UserSummaryProps {
+  children?: React.ReactNode;
   user?: User.AsObject;
 }
 
-export default function UserSummary({ user }: UserSummaryProps) {
+export default function UserSummary({ children, user }: UserSummaryProps) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -49,25 +43,16 @@ export default function UserSummary({ user }: UserSummaryProps) {
       <ListItemText
         disableTypography
         primary={
-          <Typography variant="h2" noWrap className={classes.title}>
+          <Typography variant="h2" className={classes.title}>
             {!user ? <Skeleton /> : `${user.name}, ${user.age}, ${user.city}`}
           </Typography>
         }
         secondary={
           <>
-            <ScoreBar value={user?.verification || 0}>
+            <ScoreBar value={(user?.communityStanding || 0) * 100}>
               {COMMUNITY_STANDING}
             </ScoreBar>
-            <div className={classes.hostingAbilityContainer}>
-              {user ? (
-                <IconText
-                  icon={CouchIcon}
-                  text={hostingStatusLabels[user.hostingStatus]}
-                />
-              ) : (
-                <Skeleton width={100} />
-              )}
-            </div>
+            {children}
           </>
         }
       />
