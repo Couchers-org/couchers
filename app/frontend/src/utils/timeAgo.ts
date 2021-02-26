3f658a888020d29dfcefb093aa0394b43f86514f
lucaslcode <lucaslevin@gmail.com>
2021-02-26 23:38:36 +0000

    Fix build

--- app/frontend/src/utils/timeAgo.ts
+++ app/frontend/src/utils/timeAgo.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export function timeAgo(date: Date, fuzzed?: boolean = false) {
+export function timeAgo(date: Date, fuzzed: boolean = false) {
   //the backend fuzzes times to 15 minute intervals
   if (fuzzed && new Date().getTime() - +date < 15 * 60 * 1000) {
     return "< 15 minutes ago";
