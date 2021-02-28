ad8a9788000031b02fdaf3c9f4f7f8c4a8974a9d
lucaslcode <lucaslevin@gmail.com>
2021-02-28 23:50:59 +0000

    Implement grpc timeout and nice errors

--- app/frontend/src/components/Alert.tsx
+++ app/frontend/src/components/Alert.tsx
@@ -6,6 +6,8 @@ import {
 import classNames from "classnames";
 import React from "react";
 
+import { grpcErrorStrings } from "../constants";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     marginBottom: theme.spacing(2),
@@ -16,9 +18,28 @@ interface AlertProps extends MuiAlertProps {
   severity: MuiAlertProps["severity"];
 }
 
-export default function Alert({ className, ...otherProps }: AlertProps) {
+export default function Alert({
+  className,
+  children,
+  ...otherProps
+}: AlertProps) {
   const classes = useStyles();
+
   return (
-    <MuiAlert {...otherProps} className={classNames(classes.root, className)} />
+    <MuiAlert {...otherProps} className={classNames(classes.root, className)}>
+      {React.Children.map(children, (child) => {
+        if (typeof child !== "string")
+          throw new Error("Alert should only have string children");
+
+        // Search for the error in the ugly grpc error object keys
+        // Replace it with the nice error if found
+        const oldErrorKey = Object.keys(grpcErrorStrings).find((oldError) =>
+          child.includes(oldError)
+        );
+        if (oldErrorKey)
+          return grpcErrorStrings[oldErrorKey as keyof typeof grpcErrorStrings];
+        else return child;
+      })}
+    </MuiAlert>
   );
 }
