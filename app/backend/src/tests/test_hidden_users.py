c95cdeba75b2c15cdc53c846f91bfd18c03a21b8
ShmuelTreiger <shmueltreiger@gmail.com>
2021-02-28 04:28:42 -0800

    Adding is_delteed field

--- app/backend/src/tests/test_hidden_users.py
+++ app/backend/src/tests/test_hidden_users.py
@@ -4,7 +4,7 @@ from couchers.standardized_queries import query_users_who_arent_hidden
 
 def test_hidden_users(db):
     user1, token1 = generate_user()
-    #user2, token2 = generate_user(is_deleted=True)
+    user2, token2 = generate_user(is_deleted=True)
     #user3, token3 = generate_user(is_banned=True)
     user4, token4 = generate_user(accepted_tos=0)
 
