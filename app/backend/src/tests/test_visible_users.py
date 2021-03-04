ed83e6bc427dae082188fdb952c5da76c6a7bf1d
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-03 21:26:24 -0800

    Removed special SQL path

--- app/backend/src/tests/test_visible_users.py
+++ app/backend/src/tests/test_visible_users.py
@@ -11,5 +11,5 @@ def test_hidden_users(db):
     user4, token4 = generate_user(accepted_tos=0)
 
     with session_scope() as session:
-        visible_users = session.query(User).filter(User.is_visible_for_sql).all()
+        visible_users = session.query(User).filter(User.is_visible).all()
         assert len(visible_users) == 1
