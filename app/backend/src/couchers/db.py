615e840b0f439f05d0cff5b4812d05c41c0093bf
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-04 05:25:05 -0800

    Moving is_visible to first filter position

--- app/backend/src/couchers/db.py
+++ app/backend/src/couchers/db.py
@@ -135,7 +135,7 @@ def get_user_by_field(session, field):
     """
     Returns the user based on any of those three
     """
-    query = session.query(User)
+    query = session.query(User).filter(User.is_visible)
 
     if is_valid_user_id(field):
         logger.debug(f"Field matched to type user id")
@@ -150,7 +150,7 @@ def get_user_by_field(session, field):
         logger.debug(f"Field {field=}, didn't match any known types")
         return None
 
-    query = query.filter(User.is_visible).one_or_none()
+    query = query.one_or_none()
     return query
 
 
