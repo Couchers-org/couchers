2be8c91e5b2fd43a5f450e3b58af4e34ce866e4a
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-03 22:15:49 -0800

    Fixing the test, circumventing creating an already banned user

--- app/backend/src/tests/test_visible_users.py
+++ app/backend/src/tests/test_visible_users.py
@@ -1,15 +1,17 @@
-from sqlalchemy import not_
-
+from couchers.db import get_user_by_field
 from couchers.models import User
 from tests.test_fixtures import db, generate_user, session_scope
 
 
 def test_hidden_users(db):
     user1, token1 = generate_user()
-    user2, token2 = generate_user(is_deleted=True)
-    # user3, token3 = generate_user(is_banned=True)
+    user2, token2 = generate_user()
+    user3, token3 = generate_user(is_deleted=True)
     user4, token4 = generate_user(accepted_tos=0)
 
     with session_scope() as session:
+        user2 = get_user_by_field(session, user2.username)
+        user2.is_banned = True
+
         visible_users = session.query(User).filter(User.is_visible).all()
         assert len(visible_users) == 1
