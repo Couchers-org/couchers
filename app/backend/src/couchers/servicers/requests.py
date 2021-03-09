3c085dd4ff2822e1e78ab26152fad9233698a78f
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-08 18:45:22 -0800

    Adding in some missing tests

--- app/backend/src/couchers/servicers/requests.py
+++ app/backend/src/couchers/servicers/requests.py
@@ -60,7 +60,11 @@ class Requests(requests_pb2_grpc.RequestsServicer):
         with session_scope() as session:
             if request.to_user_id == context.user_id:
                 context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.CANT_REQUEST_SELF)
-            # just to check the host exists
+
+            # just to check guest and host exist and are visible
+            guest = session.query(User).filter(User.is_visible).filter(User.id == context.user_id).one_or_none()
+            if not guest:
+                context.abort(grpc.StatusCode.NOT_FOUND, errors.USER_NOT_FOUND)
             host = session.query(User).filter(User.is_visible).filter(User.id == request.to_user_id).one_or_none()
             if not host:
                 context.abort(grpc.StatusCode.NOT_FOUND, errors.USER_NOT_FOUND)
