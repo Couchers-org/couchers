16a7837c1cadf8e8f6159ba4a1197fd2b072e6e7
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-10 21:46:44 -0800

    Refactoring to 'blocking'

--- app/backend/src/tests/test_blocking.py
+++ app/backend/src/tests/test_blocking.py
@@ -4,7 +4,7 @@ from google.protobuf import empty_pb2
 
 from couchers import errors
 from pb import api_pb2, user_blocks_pb2
-from tests.test_fixtures import api_session, db, generate_user, testconfig, user_blocks_session
+from tests.test_fixtures import api_session, blocking_session, db, generate_user, testconfig
 
 
 @pytest.fixture(autouse=True)
@@ -12,12 +12,12 @@ def _(testconfig):
     pass
 
 
-def test_user_blocking(db):
+def test_blocking(db):
     user1, token1 = generate_user()
     user2, token2 = generate_user()
 
     # Test blocking
-    with user_blocks_session(token1) as user_blocks:
+    with blocking_session(token1) as user_blocks:
         with pytest.raises(grpc.RpcError) as e:
             user_blocks.BlockUser(user_blocks_pb2.BlockUserReq(user_id=user1.id))
         assert e.value.code() == grpc.StatusCode.INVALID_ARGUMENT
@@ -37,7 +37,7 @@ def test_user_blocking(db):
         assert e.value.details() == errors.USER_ALREADY_BLOCKED
 
     with api_session(token1) as api:
-        """""
+        """ ""
         with pytest.raises(grpc.RpcError) as e:
             api.GetUser(
                 api_pb2.GetUserReq(user=user2.username)
@@ -72,7 +72,7 @@ def test_user_blocking(db):
         """
 
     # Test Unblocking
-    with user_blocks_session(token1) as user_blocks:
+    with blocking_session(token1) as user_blocks:
         with pytest.raises(grpc.RpcError) as e:
             user_blocks.UnblockUser(user_blocks_pb2.UnblockUserReq(user_id=user1.id))
         assert e.value.code() == grpc.StatusCode.INVALID_ARGUMENT
@@ -99,7 +99,7 @@ def test_user_blocking(db):
         assert res.pending_friend_request_count == 1
 
     # Test re-blocking
-    with user_blocks_session(token1) as user_blocks:
+    with blocking_session(token1) as user_blocks:
         user_blocks.BlockUser(user_blocks_pb2.BlockUserReq(user_id=user2.id))
 
         blocked_user_list = user_blocks.GetBlockedUsers(empty_pb2.Empty())
