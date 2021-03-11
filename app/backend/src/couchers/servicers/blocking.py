50adc150b1fc4c47ce14fbc1bb866027bd99d65d
ShmuelTreiger <shmueltreiger@gmail.com>
2021-03-11 00:35:44 -0800

    Adding get blocked and blocking users

--- app/backend/src/couchers/servicers/blocking.py
+++ app/backend/src/couchers/servicers/blocking.py
@@ -1,5 +1,6 @@
 import grpc
 from google.protobuf import empty_pb2
+from sqlalchemy.sql import or_
 
 from couchers import errors, urls
 from couchers.db import session_scope
@@ -33,14 +34,6 @@ class Blocking(blocking_pb2_grpc.BlockingServicer):
 
         return empty_pb2.Empty()
 
-    def GetBlockedUsers(self, request, context):
-        with session_scope() as session:
-            blocked_users = session.query(UserBlocks).filter(UserBlocks.blocking_user_id == context.user_id).all()
-
-            return user_blocks_pb2.GetBlockedUsersRes(
-                blocked_user_ids=[blocked_user.id for blocked_user in blocked_users],
-            )
-
     def UnblockUser(self, request, context):
         if context.user_id == request.user_id:
             context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.CANT_UNBLOCK_SELF)
@@ -61,3 +54,28 @@ class Blocking(blocking_pb2_grpc.BlockingServicer):
             session.commit()
 
         return empty_pb2.Empty()
+
+    def GetBlockedUsers(self, request, context):
+        with session_scope() as session:
+            blocked_users = session.query(UserBlocks).filter(UserBlocks.blocking_user_id == context.user_id).all()
+
+            return blocking_pb2.GetBlockedUsersRes(
+                blocked_user_ids=[blocked_user.id for blocked_user in blocked_users],
+            )
+
+    def GetBlockedAndBLockingUsers(self, request, context):
+        with session_scope() as session:
+            relevant_user_blocks = (
+                session.query(UserBlocks)
+                .filter(or_(UserBlocks.blocking_user_id == context.id, UserBlocks.blocked_user_id == context.id))
+                .all()
+            )
+
+        return blocking_pb2.GetBlockedAndBlockingUserRes(
+            blocked_and_blocking_user_ids=[
+                user_block.blocking_user_id
+                if user_block.blocking_user_id != context.user_id
+                else user_block.blocked_user_id
+                for user_block in relevant_user_blocks
+            ]
+        )
