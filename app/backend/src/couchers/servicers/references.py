670366b60872e3f1a8ece4d8f62492a03a7c2fe8
Aapeli <git@aapelivuorinen.com>
2021-03-16 13:46:04 -0400

    Separate references into its own service

---
+++ app/backend/src/couchers/servicers/references.py
@@ -0,0 +1,105 @@
+import grpc
+from google.protobuf import empty_pb2
+from sqlalchemy.sql import and_, func, or_
+
+from couchers import errors
+from couchers.db import session_scope
+from couchers.models import HostRequest, Reference, ReferenceType, User
+from couchers.utils import Timestamp_from_datetime, now
+from pb import references_pb2, references_pb2_grpc
+
+reftype2sql = {
+    references_pb2.ReferenceType.FRIEND: ReferenceType.FRIEND,
+    references_pb2.ReferenceType.SURFED: ReferenceType.SURFED,
+    references_pb2.ReferenceType.HOSTED: ReferenceType.HOSTED,
+}
+
+reftype2api = {
+    ReferenceType.FRIEND: references_pb2.ReferenceType.FRIEND,
+    ReferenceType.SURFED: references_pb2.ReferenceType.SURFED,
+    ReferenceType.HOSTED: references_pb2.ReferenceType.HOSTED,
+}
+
+
+class References(references_pb2_grpc.APIServicer):
+    def WriteReference(self, request, context):
+        if context.user_id == request.to_user_id:
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT, errors.CANT_REFER_SELF)
+
+        reference = Reference(
+            from_user_id=context.user_id,
+            to_user_id=request.to_user_id,
+            reference_type=reftype2sql[request.reference_type],
+            text=request.text,
+            was_safe=request.was_safe,
+            rating=request.rating,
+        )
+        with session_scope() as session:
+            if not session.query(User).filter(User.id == request.to_user_id).one_or_none():
+                context.abort(grpc.StatusCode.NOT_FOUND, errors.USER_NOT_FOUND)
+
+            if (
+                session.query(Reference)
+                .filter(Reference.from_user_id == context.user_id)
+                .filter(Reference.to_user_id == request.to_user_id)
+                .filter(Reference.reference_type == reftype2sql[request.reference_type])
+                .one_or_none()
+            ):
+                context.abort(grpc.StatusCode.FAILED_PRECONDITION, errors.REFERENCE_ALREADY_GIVEN)
+            session.add(reference)
+        return empty_pb2.Empty()
+
+    def GetGivenReferences(self, request, context):
+        with session_scope() as session:
+            query = session.query(Reference)
+            query = query.filter(Reference.from_user_id == request.from_user_id)
+            if request.HasField("type_filter"):
+                query = query.filter(Reference.reference_type == reftype2sql[request.type_filter.value])
+            return paginate_references_result(request, query)
+
+    def GetReceivedReferences(self, request, context):
+        with session_scope() as session:
+            query = session.query(Reference)
+            query = query.filter(Reference.to_user_id == request.to_user_id)
+            if request.HasField("type_filter"):
+                query = query.filter(Reference.reference_type == reftype2sql[request.type_filter.value])
+            return paginate_references_result(request, query)
+
+    def AvailableWriteReferenceTypes(self, request, context):
+        available = {
+            ReferenceType.FRIEND,
+            ReferenceType.SURFED,
+            ReferenceType.HOSTED,
+        }
+
+        # Filter out already written ones.
+        with session_scope() as session:
+            query = session.query(Reference)
+            query = query.filter(Reference.from_user_id == context.user_id)
+            query = query.filter(Reference.to_user_id == request.to_user_id)
+            for reference in query.all():
+                available.remove(reference.reference_type)
+
+        # TODO: make surfing/hosted only available if you actually have been surfing/hosting
+        return references_pb2.AvailableWriteReferenceTypesRes(reference_types=[reftype2api[r] for r in available])
+
+
+def paginate_references_result(request, query):
+    total_matches = query.count()
+    references = query.order_by(Reference.time.desc()).offset(request.start_at).limit(request.number).all()
+    # order by time, pagination
+    return references_pb2.GetReferencesRes(
+        total_matches=total_matches,
+        references=[
+            references_pb2.Reference(
+                reference_id=reference.id,
+                from_user_id=reference.from_user_id,
+                to_user_id=reference.to_user_id,
+                reference_type=reftype2api[reference.reference_type],
+                text=reference.text,
+                # Fuzz reference written time
+                written_time=Timestamp_from_datetime(reference.time.replace(hour=0, minute=0, second=0, microsecond=0)),
+            )
+            for reference in references
+        ],
+    )
---
+++ app/backend/src/tests/test_references.py
@@ -0,0 +1,108 @@
+from datetime import timedelta
+
+import grpc
+import pytest
+from google.protobuf import empty_pb2
+
+from couchers import errors
+from couchers.utils import now, to_aware_datetime
+from pb import references_pb2
+from tests.test_fixtures import db, generate_user, references_session, testconfig
+
+
+@pytest.fixture(autouse=True)
+def _(testconfig):
+    pass
+
+
+def test_references(db):
+    user1, token1 = generate_user()
+    user2, token2 = generate_user()
+
+    alltypes = set(
+        [references_pb2.ReferenceType.FRIEND, references_pb2.ReferenceType.HOSTED, references_pb2.ReferenceType.SURFED]
+    )
+    # write all three reference types
+    for typ in alltypes:
+        req = references_pb2.WriteReferenceReq(to_user_id=user2.id, reference_type=typ, text="kinda weird sometimes")
+        with references_session(token1) as api:
+            res = api.WriteReference(req)
+        assert isinstance(res, empty_pb2.Empty)
+
+    # See what I have written. Paginate it.
+    seen_types = set()
+    for i in range(3):
+        req = references_pb2.GetGivenReferencesReq(from_user_id=user1.id, number=1, start_at=i)
+        with references_session(token1) as api:
+            res = api.GetGivenReferences(req)
+        assert res.total_matches == 3
+        assert len(res.references) == 1
+        assert res.references[0].from_user_id == user1.id
+        assert res.references[0].to_user_id == user2.id
+        assert res.references[0].text == "kinda weird sometimes"
+        assert abs(to_aware_datetime(res.references[0].written_time) - now()) <= timedelta(days=32)
+        assert res.references[0].reference_type not in seen_types
+        seen_types.add(res.references[0].reference_type)
+    assert seen_types == alltypes
+
+    # See what user2 have received. Paginate it.
+    seen_types = set()
+    for i in range(3):
+        req = references_pb2.GetReceivedReferencesReq(to_user_id=user2.id, number=1, start_at=i)
+        with references_session(token1) as api:
+            res = api.GetReceivedReferences(req)
+        assert res.total_matches == 3
+        assert len(res.references) == 1
+        assert res.references[0].from_user_id == user1.id
+        assert res.references[0].to_user_id == user2.id
+        assert res.references[0].text == "kinda weird sometimes"
+        assert res.references[0].reference_type not in seen_types
+        seen_types.add(res.references[0].reference_type)
+    assert seen_types == alltypes
+
+    # Check available types
+    with references_session(token1) as api:
+        res = api.AvailableWriteReferenceTypes(references_pb2.AvailableWriteReferenceTypesReq(to_user_id=user2.id))
+    assert res.reference_types == []
+
+    with references_session(token2) as api:
+        res = api.AvailableWriteReferenceTypes(references_pb2.AvailableWriteReferenceTypesReq(to_user_id=user1.id))
+    assert set(res.reference_types) == alltypes
+
+    # Forbidden to write a second reference of the same type
+    req = references_pb2.WriteReferenceReq(
+        to_user_id=user2.id, reference_type=references_pb2.ReferenceType.HOSTED, text="ok"
+    )
+    with references_session(token1) as api:
+        with pytest.raises(grpc.RpcError) as e:
+            api.WriteReference(req)
+        assert e.value.code() == grpc.StatusCode.FAILED_PRECONDITION
+        assert e.value.details() == errors.REFERENCE_ALREADY_GIVEN
+
+    # Nonexisting user
+    req = references_pb2.WriteReferenceReq(
+        to_user_id=0x7FFFFFFFFFFFFFFF, reference_type=references_pb2.ReferenceType.HOSTED, text="ok"
+    )
+    with references_session(token1) as api:
+        with pytest.raises(grpc.RpcError) as e:
+            api.WriteReference(req)
+        assert e.value.code() == grpc.StatusCode.NOT_FOUND
+        assert e.value.details() == errors.USER_NOT_FOUND
+
+    # yourself
+    req = references_pb2.WriteReferenceReq(
+        to_user_id=user1.id, reference_type=references_pb2.ReferenceType.HOSTED, text="ok"
+    )
+    with references_session(token1) as api:
+        with pytest.raises(grpc.RpcError) as e:
+            api.WriteReference(req)
+        assert e.value.code() == grpc.StatusCode.INVALID_ARGUMENT
+        assert e.value.details() == errors.CANT_REFER_SELF
+
+    with references_session(token2) as api:
+        # test the number of references in GetUser and Ping
+        res = api.GetUser(references_pb2.GetUserReq(user=user2.username))
+        assert res.num_references == 3
+
+        res = api.Ping(references_pb2.PingReq())
+        assert res.user.num_references == 3
