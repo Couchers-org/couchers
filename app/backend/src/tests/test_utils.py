efed040dd30d8e375cb4234bce79255003b4e40d
OmerBaddour <omer.m.baddour@gmail.com>
2021-03-21 15:02:02 -0400

    actually tests to_timezone()

--- app/backend/src/tests/test_utils.py
+++ app/backend/src/tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
-from timezonefinder import TimezoneFinder
 
+from couchers.utils import to_timezone
 from tests.test_fixtures import testconfig
 
 
@@ -9,11 +9,11 @@ def _(testconfig):
     pass
 
 
-def test_to_timezone_lng_lat():
-    tf = TimezoneFinder()
+def test_to_timezone():
+    # test valid lat, lng
     lat, lng = 52.5061, 13.358
-    assert tf.timezone_at(lng=lng, lat=lat) == "Europe/Berlin"
+    assert to_timezone(lat=lat, lng=lng) == "Europe/Berlin"
 
-    with pytest.raises(ValueError):
-        lat, lng = -91, -181
-        tf.timezone_at(lng=lng, lat=lat)
+    # test invalid lat, lng
+    lat, lng = -91, -181
+    assert to_timezone(lat=lat, lng=lng) == None
