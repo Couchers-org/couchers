952dad1502d18b19e022f685e3f1bcd5392c2a96
Yannic Spreen-Ledebur <yannic.spreen-ledebur@plan-d.com>
2021-03-23 16:50:53 +0100

    Shortened redirect syntax

--- app/frontend/src/features/communities/redirect.ts
+++ app/frontend/src/features/communities/redirect.ts
@@ -1,10 +1,7 @@
 import { Page, PageType } from "pb/pages_pb";
 import { routeToGuide, routeToPlace } from "routes";
 
-export const pageURL = (page: Page.AsObject) => {
-  if (page.type === PageType.PAGE_TYPE_GUIDE) {
-    return routeToGuide(page.pageId, page.slug);
-  } else {
-    return routeToPlace(page.pageId, page.slug);
-  }
-};
+export const pageURL = (page: Page.AsObject) => 
+  page.type === PageType.PAGE_TYPE_GUIDE
+  ? routeToGuide(page.pageId, page.slug)
+  : routeToPlace(page.pageId, page.slug);
