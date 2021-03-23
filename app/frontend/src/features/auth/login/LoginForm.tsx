f34c212853278dd135b8332091ba314fe039fc91
Vincent-Projects <crysyroodpro@outlook.com>
2021-03-23 01:08:35 +0100

    yarn format run

--- app/frontend/src/features/auth/login/LoginForm.tsx
+++ app/frontend/src/features/auth/login/LoginForm.tsx
@@ -105,7 +105,7 @@ export default function UsernameForm() {
               fullWidth
               id="password"
               name="password"
-              inputRef={inputElement => {
+              inputRef={(inputElement) => {
                 if (inputElement) {
                   inputElement.focus();
                 }
