a4d9833a8fb61205619914daca9b986691d5d788
ItsiW <itsi148@gmail.com>
2021-03-30 00:09:33 +1100

    add blog to footer

--- site/layouts/default.vue
+++ site/layouts/default.vue
@@ -111,7 +111,10 @@
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          <strong>Couchers.org</strong> by <a href="/team">the <b>Couchers.org team</b></a>.
+          Read <a href="/team">the Blog</a>.
+        </p>
+        <p>
+          <b>Couchers.org</b> by <a href="/team">the <b>Couchers.org team</b></a>.
         </p>
         <p>It's like Couchsurfing&#8482;, but better.</p>
       </div>
