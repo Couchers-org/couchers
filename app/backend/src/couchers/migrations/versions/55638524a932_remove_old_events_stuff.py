904de1b00e0fb318f45587b8d1926d8760f07c2d
Aapeli <git@aapelivuorinen.com>
2021-04-04 16:58:32 -0400

    Fix migration

--- app/backend/src/couchers/migrations/versions/55638524a932_remove_old_events_stuff.py
+++ app/backend/src/couchers/migrations/versions/55638524a932_remove_old_events_stuff.py
@@ -1,7 +1,7 @@
 """Remove old events stuff
 
 Revision ID: 55638524a932
-Revises: 17362f602f12
+Revises: 27a2782784d0
 Create Date: 2021-03-29 13:00:58.002000
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "55638524a932"
-down_revision = "17362f602f12"
+down_revision = "27a2782784d0"
 branch_labels = None
 depends_on = None
 
