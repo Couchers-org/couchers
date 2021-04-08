40c417e8eac8260db6dc4168e0d81d337b485a7d
Aapeli <git@aapelivuorinen.com>
2021-04-08 19:17:37 -0400

    Attempt to fix logging format

--- app/backend/src/app.py
+++ app/backend/src/app.py
@@ -47,7 +47,7 @@ from pb import (
 
 config.check_config()
 
-logging.basicConfig(format="%(asctime)s: %(name)d: %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(asctime)s: %(name)s: %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logging.getLogger("couchers.jobs.worker").setLevel(logging.INFO)
