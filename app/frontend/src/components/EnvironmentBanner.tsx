fea241ca255a18c11587b6bfc8ad88ce2e61ede8
lucaslcode <lucaslevin@gmail.com>
2021-04-17 18:50:43 +0100

    Make environment banner hideable and bottom-right

--- app/frontend/src/components/EnvironmentBanner.tsx
+++ app/frontend/src/components/EnvironmentBanner.tsx
@@ -1,23 +1,21 @@
-import { makeStyles, useMediaQuery, useTheme } from "@material-ui/core";
-import React from "react";
+import { Chip, makeStyles, useMediaQuery, useTheme } from "@material-ui/core";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
+  "@keyframes attention": {
+    from: { opacity: 0.5 },
+    to: { opactiy: 0.9 },
+  },
   banner: {
+    animation: "2s infinite alternate $attention",
     position: "fixed",
-    top: 2,
-    left: 2,
+    bottom: theme.spacing(1),
+    right: theme.spacing(1),
     zIndex: 5000,
-    paddingTop: 2,
-    paddingBottom: 2,
-    paddingLeft: 5,
-    paddingRight: 5,
-    opacity: 0.8,
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.common.white,
-    pointerEvents: "none",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
+    "&.MuiChip-root": {
+      backgroundColor: theme.palette.error.main,
+      color: theme.palette.common.white,
+    },
   },
 }));
 
@@ -25,19 +23,25 @@ export function EnvironmentBanner() {
   const theme = useTheme();
   const classes = useStyles();
   const isBelowSm = useMediaQuery(theme.breakpoints.down("sm"));
+  const [isHidden, setIsHidden] = useState(false);
 
-  if (process.env.REACT_APP_COUCHERS_ENV === "prod") {
+  if (isHidden || process.env.REACT_APP_COUCHERS_ENV === "prod") {
     return null;
   } else {
     return (
-      <div className={classes.banner}>
-        This is a{" "}
-        {process.env.REACT_APP_COUCHERS_ENV === "preview"
-          ? "preview"
-          : "development"}{" "}
-        build of the app.
-        {!isBelowSm && " It uses a separate database to the production app."}
-      </div>
+      <Chip
+        className={classes.banner}
+        label={`This is a ${
+          process.env.REACT_APP_COUCHERS_ENV === "preview"
+            ? "preview"
+            : "development"
+        } build of the app.${
+          !isBelowSm
+            ? " It uses a separate database to the production app."
+            : ""
+        }`}
+        onDelete={() => setIsHidden(true)}
+      />
     );
   }
 }
