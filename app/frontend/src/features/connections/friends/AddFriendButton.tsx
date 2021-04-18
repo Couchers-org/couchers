008d3c0eefc60be590a9a24decdd16f0b23ceefc
ineswq <ines.warleta@gmail.com>
2021-04-18 15:44:42 +0200

    Optimistically update query cache

--- app/frontend/src/features/connections/friends/AddFriendButton.tsx
+++ app/frontend/src/features/connections/friends/AddFriendButton.tsx
@@ -2,6 +2,7 @@ import Button from "components/Button";
 import { PersonAddIcon } from "components/Icons";
 import { ADD_FRIEND } from "features/connections/constants";
 import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { User } from "pb/api_pb";
 import React from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { service } from "service";
@@ -24,14 +25,31 @@ export default function AddFriendButton({
     Error,
     AddFriendButtonProps
   >(({ userId }) => service.api.sendFriendRequest(userId), {
-    onError: (error, { setMutationError }) => {
-      setMutationError(error.message);
-    },
-    onMutate: ({ setMutationError }) => {
+    onMutate: async ({ setMutationError }) => {
       setMutationError("");
+
+      await queryClient.cancelQueries(["user", userId]);
+
+      const cachedUser = queryClient.getQueryData<User.AsObject>([ "user", userId, ]);
+
+      if (cachedUser) {
+        queryClient.setQueryData<User.AsObject>(
+          ["user", userId], { 
+            ...cachedUser, 
+            friends: User.FriendshipStatus.PENDING, 
+          }
+        );
+      }
+      return cachedUser;
+    },
+    onError: (error, { setMutationError }, cachedUser) => {
+      setMutationError(error.message);
+      if (cachedUser) {
+        queryClient.setQueryData(["user", userId], cachedUser);
+      }
     },
     onSuccess: (_, { userId }) => {
-      queryClient.invalidateQueries(["user", userId]);
+      queryClient.invalidateQueries(["user", userId])
     },
   });
 
