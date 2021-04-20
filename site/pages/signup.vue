53ecf8174f98b47ea9f8ad33056e78a1ccd19c37
Aapeli <git@aapelivuorinen.com>
2021-04-20 13:14:42 -0400

    Add manual redirect too

---
+++ site/pages/signup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h2 class="title is-2">Sign up to contribute to Couchers.org</h2>
+          <p class="subtitle is-4">The contributor form has been integrated into the app. You should be redirected soon, otherwise click the button below to go to the form.</p>
+          <p><a class="button is-primary" href = "https://app.couchers.org/contribute">Contribute to Couchers.org</a></p>
+          <script>window.location = "https://app.couchers.org/contribute"</script>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Sign up',
+      meta: [
+        { hid: 'og:title', name: 'og:title', content: 'Sign up' },
+        { hid: 'twitter:title', name: 'twitter:title', content: 'Sign up' },
+      ]
+    }
+  }
+}
+</script>
