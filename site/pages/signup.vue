dd12f5abf4358b382738d2594086009c19a4137a
Aapeli <git@aapelivuorinen.com>
2021-04-20 13:34:55 -0400

    Try to fix redirect

--- site/pages/signup.vue
+++ site/pages/signup.vue
@@ -6,7 +6,6 @@
           <h2 class="title is-2">Sign up to contribute to Couchers.org</h2>
           <p class="subtitle is-4">The contributor form has been integrated into the app. You should be redirected soon, otherwise click the button below to go to the form.</p>
           <p><a class="button is-primary" href = "https://app.couchers.org/contribute">Contribute to Couchers.org</a></p>
-          <script>window.location = "https://app.couchers.org/contribute"</script>
         </div>
       </div>
     </section>
@@ -15,6 +14,9 @@
 
 <script>
 export default {
+  async asyncData({redirect}) {
+    redirect("https://app.couchers.org/contribute")
+  },
   head () {
     return {
       title: 'Sign up',
