06464af625d18442e1f3593446dee82f4f3649c8
thdk <t.dekiere@gmail.com>
2021-04-21 14:16:49 +0200

    fix: error boundary should update when location changes

--- app/frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ app/frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,11 +1,14 @@
 import ErrorFallback from "components/ErrorFallback";
 import { ReactNode } from "react";
 import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
+import { useLocation } from "react-router";
 
 export default function ErrorBoundary({ children }: { children: ReactNode }) {
+  const { pathname } = useLocation();
 
   return (
     <ReactErrorBoundary
+      resetKeys={[pathname || "home"]}
       FallbackComponent={() => <ErrorFallback />}
     >
       {children}
