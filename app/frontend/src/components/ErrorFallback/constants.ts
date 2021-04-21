522d481dd7ae5927fcb5972a80d53e654e2885bb
Thomas Dekiere <thdk@users.noreply.github.com>
2021-04-21 07:02:40 +0200

    Update app/frontend/src/components/ErrorFallback/constants.ts
    Co-authored-by: Lucas Levin <lucas@couchers.org>

--- app/frontend/src/components/ErrorFallback/constants.ts
+++ app/frontend/src/components/ErrorFallback/constants.ts
@@ -1,5 +1,5 @@
-export const ERROR_HEADING = "Ooops... that's an error";
+export const ERROR_HEADING = "Ooops... that's an unknown error";
 export const ERROR_INFO =
-  "Please use the 'Report a problem' button to help us prevent this error in the future.";
+  "Please use the 'Report a problem' button and tell us what you were doing to help us prevent this error in the future.";
 export const GO_TO_HOMEPAGE = "Go to homepage";
 export const REFRESH_PAGE = "Refresh page";
