593038e8e20e99b4506a440954a02e2e3a56f172
ineswq <ines.warleta@gmail.com>
2021-04-26 15:36:38 +0200

    expand testing

--- app/frontend/src/features/communities/leavereference/LeaveReferencePage.test.tsx
+++ app/frontend/src/features/communities/leavereference/LeaveReferencePage.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen, within } from "@testing-library/react";
-import { INVALID_REFERENCE_TYPE } from "features/communities/constants";
+import {
+  INVALID_REFERENCE_TYPE,
+  REFERENCE_TYPE_NOT_AVAILABLE,
+} from "features/communities/constants";
 import { Route } from "react-router-dom";
 import { leaveReferenceBaseRoute } from "routes";
 import { service } from "service";
@@ -18,7 +21,11 @@ const getUserMock = service.user.getUser as MockedService<
   typeof service.user.getUser
 >;
 
-function renderLeaveReferencePage(referenceType: string, userId: string) {
+function renderLeaveReferencePage(
+  referenceType: string,
+  userId: string,
+  hostRequest?: number
+) {
   const { wrapper } = getHookWrapperWithClient({
     initialRouterEntries: [
       `${leaveReferenceBaseRoute}/${referenceType}/${userId}`,
@@ -27,7 +34,7 @@ function renderLeaveReferencePage(referenceType: string, userId: string) {
 
   render(
     <Route path={`${leaveReferenceBaseRoute}/:referenceType/:userId`}>
-      <LeaveReferencePage />
+      <LeaveReferencePage hostRequest={hostRequest} />
     </Route>,
     { wrapper }
   );
@@ -52,14 +59,13 @@ describe("LeaveReferencePage", () => {
     });
 
     it("does not show reviewee information", () => {
-      expect(
-        screen.findByTestId(REVIEWEE_CARD_TEST_ID)
-      ).not.toBeInTheDocument();
+      const card = screen.queryByTestId(REVIEWEE_CARD_TEST_ID);
+      expect(card).not.toBeInTheDocument();
     });
 
     it("does not show the form", () => {
       expect(
-        screen.findByRole("heading", {
+        screen.queryByRole("heading", {
           name: "You met with Friendly Cow",
         })
       ).not.toBeInTheDocument();
@@ -67,8 +73,73 @@ describe("LeaveReferencePage", () => {
   });
 
   describe("When the reference type is friend", () => {
+    describe("and the users are friends", () => {
+      beforeEach(() => {
+        renderLeaveReferencePage("friend", "5");
+      });
+
+      it("verifies that the review type is available", async () => {
+        expect(getAvailableReferencesMock).toHaveBeenCalledTimes(1);
+        expect(getAvailableReferencesMock).toHaveReturned();
+      });
+
+      it("does not return an error", () => {
+        expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+      });
+
+      it("displays reviewee information", async () => {
+        expect(
+          await screen.findByTestId(REVIEWEE_CARD_TEST_ID)
+        ).toBeInTheDocument();
+      });
+
+      it("displays the form", async () => {
+        expect(
+          await screen.findByRole("heading", {
+            name: "You met with Friendly Cow",
+          })
+        ).toBeInTheDocument();
+      });
+    });
+
+    describe("and the users aren't friends", () => {
+      beforeEach(() => {
+        renderLeaveReferencePage("friend", "1");
+      });
+
+      it("verifies the review type", async () => {
+        expect(getAvailableReferencesMock).toHaveBeenCalledTimes(1);
+        expect(getAvailableReferencesMock).toHaveReturned();
+      });
+
+      it("Returns an error", async () => {
+        const errorAlert = await screen.findByRole("alert");
+        expect(
+          within(errorAlert).getByText(REFERENCE_TYPE_NOT_AVAILABLE)
+        ).toBeVisible();
+      });
+
+      it("does not show reviewee information", () => {
+        expect(
+          screen.findByTestId(REVIEWEE_CARD_TEST_ID)
+        ).not.toBeInTheDocument();
+      });
+
+      it("does not show the form", () => {
+        expect(
+          screen.queryByRole("heading", {
+            name: "You met with Funny Cat current User",
+          })
+        ).not.toBeInTheDocument();
+      });
+    });
+  });
+});
+
+describe("When the reference type is hosted/surfed", () => {
+  describe("And a review is available", () => {
     beforeEach(() => {
-      renderLeaveReferencePage("friend", "5");
+      renderLeaveReferencePage("hosted", "5", 1);
     });
 
     it("verifies that the review type is available", async () => {
@@ -94,4 +165,6 @@ describe("LeaveReferencePage", () => {
       ).toBeInTheDocument();
     });
   });
+
+  describe("and a review is unavailable", () => {});
 });
