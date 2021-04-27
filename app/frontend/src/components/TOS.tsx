04d6d35277e10b46fb88ffb4ef521aebe4999677
ItsiW <itsi148@gmail.com>
2021-04-27 23:20:08 +1000

    Update Terms of Service

--- app/frontend/src/components/TOS.tsx
+++ app/frontend/src/components/TOS.tsx
@@ -9,26 +9,47 @@ export default function TOS() {
     <>
       <PageTitle>Terms of Service</PageTitle>
       <TextBody>
-        This is a test TOS. I agree to follow the&nbsp;
-        <Link href="https://community.couchers.org/faq">
-          Couchers guidelines
-        </Link>
-        . I understand this is a preview of couchers.org and my data may be
-        erased. Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+        You must agree to these Terms to access the Couchers.org services and create an account.
         <br />
         <br />
-        Lorem Ipsum has been the industry's standard dummy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
-        unchanged.
+        <b>Note: we'll update the Terms of Service as we approach the full launch and clarify the reasons and ways in which we may terminate or ban accounts as we develop these processes and solidify our moderation process.</b>
         <br />
         <br />
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
+        <b>1. Foundation</b>
+        <br />
+        Access to the Couchers.org platform is provided by the Couchers.org Foundation (Cross-Cultural Exchange and Communties Foundation Limited), a non-profit organization formed in Sydney, Australia.
+        <br />
+        <br />
+        <b>2. Eligibility</b>
+        <br />
+        You must be at least 18 years old to access the Couchers.org services.
+        <br />
+        <br />
+        <b>3. Account information</b>
+        <br />
+        You are responsible for any activity done using your account, as well as securing your account from inappropriate access. For example, you must use a secure password not used on other products or services, and not you cannot leave your account logged in on shared computers. If for whatever purpose you give access to your account to others you are liable for any such activity.
+        <br />
+        <br />
+        You must provide truthful and current information on your account. Your display name does not need to be your legal name and your display location does not need to be your true location. You may hide or obfuscate information on your profile for your own privacy or security, but you may not add information to your profile that would reasonably deceive other users. Information used in verification must be truthful and accurate, this includes your birth date, your gender identity, your legal name, and your verified address.
+        <br />
+        <br />
+        <b>4. Liability and your interactions with others</b>
+        <br />
+        Couchers.org provides a platform for users to contact one another and organise activities together. You agree to not hold us liable or responsible for user-created content or for interactions between users, including online and in person. You are solely liable and responsible for your actions on the platform, including any interactions with other users and any content that you create.
+        <br />
+        <br />
+        Although we are not legally responsible for user-created content, we actively moderate the platform to remove any content that is harmful, deceitful, or otherwise does not adhere to our rules. If you believe some content does not adhere to our rules, please report it to us via support@couchers.org.
+        <br />
+        <br />
+        When posting any image, you agree to obtain consent from any individual pictured in the image.
+        <br />
+        <br />
+        We are not liable if at any time, with or without prior notice, we modify, suspend or discontinue our services.
+        <br />
+        <br />
+        <b>5. Termination</b>
+        <br />
+        If you are in breach of these terms or any other policies in place, we may at our discretion suspend or terminate your account.
       </TextBody>
     </>
   );
