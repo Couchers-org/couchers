d8709f736ed7b28bccd699378914c2c8c36c9680
Darren Vong <darrenvong@users.noreply.github.com>
2021-05-02 03:19:02 +0100

    feat: move thread data fetching into RepliesTree

--- app/frontend/src/features/communities/discussions/DiscussionPage.tsx
+++ app/frontend/src/features/communities/discussions/DiscussionPage.tsx
@@ -1,27 +1,51 @@
 import { Typography } from "@material-ui/core";
 import Alert from "components/Alert";
+import Avatar from "components/Avatar";
 import CircularProgress from "components/CircularProgress";
+import Divider from "components/Divider";
+import HeaderButton from "components/HeaderButton";
+import { BackIcon } from "components/Icons";
 import Markdown from "components/Markdown";
 import PageTitle from "components/PageTitle";
-import useUsers from "features/userQueries/useUsers";
+import { useUser } from "features/userQueries/useUsers";
 import { Error as GrpcError } from "grpc-web";
 import { Discussion } from "pb/discussions_pb";
 import { discussionKey } from "queryKeys";
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { service } from "service";
-import hasAtLeastOnePage from "utils/hasAtLeastOnePage";
+import { dateFormatter, timestamp2Date } from "utils/date";
 import makeStyles from "utils/makeStyles";
 
-import { useCommunity, useThread } from "../hooks";
+import { ADDED_BY, UNKNOWN_USER } from "../constants";
 import RepliesTree from "./RepliesTree";
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    paddingBottom: 0,
+  header: {
+    alignItems: "center",
+    display: "flex",
+  },
+  discussionTitle: {
+    marginInlineStart: theme.spacing(2),
   },
   discussionContent: {
     margin: 0,
+  },
+  creatorContainer: {
+    "& > * + *": {
+      marginInlineStart: theme.spacing(2),
+    },
+    alignItems: "center",
+    display: "flex",
+    marginBlockEnd: theme.spacing(2),
+  },
+  creatorDetailsContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  avatar: {
+    height: "3rem",
+    width: "3rem",
   },
 }));
 
@@ -31,6 +55,7 @@ export default function DiscussionPage() {
     discussionId: string;
     discussionSlug?: string;
   }>();
+  const history = useHistory();
 
   const { data: discussion, error, isLoading: isDiscussionLoading } = useQuery<
     Discussion.AsObject,
@@ -39,46 +64,46 @@ export default function DiscussionPage() {
     queryKey: discussionKey(+discussionId),
     queryFn: () => service.discussions.getDiscussion(+discussionId),
   });
-  const { data: community, isLoading: isCommunityLoading } = useCommunity(
-    discussion?.ownerCommunityId ?? 0,
-    {
-      enabled: !!discussion,
-    }
-  );
-  const {
-    data: discussionThread,
-    isLoading: isThreadLoading,
-  } = useThread(discussion?.threadId ?? 0, { enabled: !!discussion });
 
-  const userIds = hasAtLeastOnePage(discussionThread, "repliesList")
-    ? discussionThread.pages.flatMap((page) =>
-        page.repliesList.map((reply) => reply.authorUserId)
-      )
-    : [];
-  const { data: users, isLoading: isUsersLoading } = useUsers(userIds);
+  const { data: discussionCreator } = useUser(discussion?.creatorUserId);
 
   return (
     <>
       {error && <Alert severity="error">{error.message}</Alert>}
-      {isDiscussionLoading ||
-      isCommunityLoading ||
-      isThreadLoading ||
-      isUsersLoading ? (
+      {isDiscussionLoading ? (
         <CircularProgress />
       ) : (
-        discussion &&
-        discussionThread &&
-        users && (
+        discussion && (
           <>
-            <PageTitle className={classes.title}>{discussion.title}</PageTitle>
+            <div className={classes.header}>
+              <HeaderButton onClick={() => history.goBack()} aria-label="Back">
+                <BackIcon />
+              </HeaderButton>
+              <PageTitle className={classes.discussionTitle}>
+                {discussion.title}
+              </PageTitle>
+            </div>
+            <Divider />
             <Markdown source={discussion.content} />
-            <Typography variant="body1">
-              By {users.get(discussion.creatorUserId)?.name} in{" "}
-              {community
-                ? `${community.name} community`
-                : `${discussion.ownerGroupId} group`}
-            </Typography>
-            <RepliesTree discussionThread={discussionThread} users={users} />
+            <Typography variant="body1">{ADDED_BY}</Typography>
+            <div className={classes.creatorContainer}>
+              <Avatar
+                user={discussionCreator}
+                className={classes.avatar}
+                isProfileLink={false}
+              />
+              <div className={classes.creatorDetailsContainer}>
+                <Typography variant="body1">
+                  {discussionCreator?.name ?? UNKNOWN_USER}
+                </Typography>
+                <Typography variant="body2">
+                  Created at{" "}
+                  {dateFormatter.format(timestamp2Date(discussion.created!))}
+                </Typography>
+              </div>
+            </div>
+            <Divider />
+            <RepliesTree discussion={discussion} />
           </>
         )
       )}
