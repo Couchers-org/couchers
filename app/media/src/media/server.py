da22519ae93a794fcc7848096184d20053157b05
Aapeli <git@aapelivuorinen.com>
2021-05-12 17:26:55 -0400

    Add cache headers back in after flask 2

--- app/media/src/media/server.py
+++ app/media/src/media/server.py
@@ -133,7 +133,7 @@ def create_app(
         if not os.path.isfile(path):
             abort(404, "Not found")
 
-        return send_file(path, mimetype="image/jpeg", conditional=True)
+        return send_file(path, mimetype="image/jpeg", conditional=True, max_age=43200)
 
     @app.route("/img/thumbnail/<key>.jpg")
     def thumbnail(key):
@@ -162,7 +162,7 @@ def create_app(
             img = img.resize(thumbnail_size / size)
             img.jpegsave(thumbnail_path, strip=True, interlace=True, Q=75)
 
-        return send_file(thumbnail_path, mimetype="image/jpeg", conditional=True)
+        return send_file(thumbnail_path, mimetype="image/jpeg", conditional=True, max_age=43200)
 
     return app
 
