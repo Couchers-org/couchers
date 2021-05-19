6d22fb06e7f664f0b99de73257bbffa0fb65b2a5
thdk <t.dekiere@gmail.com>
2021-05-19 22:41:58 +0200

    feat: add select component

---
+++ app/frontend/src/components/Select.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, Story } from "@storybook/react";
+
+import Select from "./Select";
+
+export default {
+  component: Select,
+  title: "Components/Simple/Select",
+} as Meta;
+
+const Template: Story<{ options: string[] }> = ({ options }) => (
+  <Select>
+    {options.map((o) => (
+      <option value={o}>{o}</option>
+    ))}
+  </Select>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  options: ["foo bar", "lorem", "ipsum"],
+};
---
+++ app/frontend/src/components/Select.tsx
@@ -0,0 +1,53 @@
+import {
+  FormControl,
+  InputLabel,
+  Select as MuiSelect,
+  SelectProps,
+} from "@material-ui/core";
+import classnames from "classnames";
+import React from "react";
+import makeStyles from "utils/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    "& .MuiOutlinedInput-root": {
+      borderRadius: theme.shape.borderRadius * 3,
+    },
+    "& .MuiInputBase-input": {
+      height: "auto",
+    },
+    display: "block",
+  },
+}));
+
+export default function Select({
+  id,
+  children,
+  className,
+  label,
+  variant = "outlined",
+  ...otherProps
+}: SelectProps) {
+  const classes = useStyles();
+  return (
+    <FormControl
+      variant={variant}
+      id={id}
+      className={classnames(className, classes.formControl)}
+      margin="normal"
+    >
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <MuiSelect
+        native
+        label={label}
+        {...otherProps}
+        inputProps={{
+          name: id,
+          id,
+        }}
+      >
+        {children}
+      </MuiSelect>
+    </FormControl>
+  );
+}
