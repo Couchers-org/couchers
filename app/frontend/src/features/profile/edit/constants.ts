dabf24e735fc01544f5766e01fdbf065440186e0
Darren Vong <darrenvong@users.noreply.github.com>
2021-05-23 23:46:01 +0100

    feat: add default headings for about me and hobbies

---
+++ app/frontend/src/features/profile/edit/constants.ts
@@ -0,0 +1,17 @@
+export const DEFAULT_ABOUT_ME_HEADINGS = `# Current mission
+
+# Why I use Couchers
+
+# My favourite travel story
+
+`;
+
+export const DEFAULT_HOBBIES_HEADINGS = `# Art
+
+# Books
+
+# Movies
+
+# Music
+
+`;
