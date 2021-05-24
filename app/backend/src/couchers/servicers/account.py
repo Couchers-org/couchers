2c40dd20b1988a80e9d641ae96a03a91be49c0ea
Christian Häggström <kalvdans@gmail.com>
2021-05-25 00:51:35 +0200

    Make sure verified phone numbers are unique

--- app/backend/src/couchers/servicers/account.py
+++ app/backend/src/couchers/servicers/account.py
@@ -203,6 +203,9 @@ class Account(account_pb2_grpc.AccountServicer):
                 session.commit()
                 context.abort(grpc.StatusCode.NOT_FOUND, errors.WRONG_SMS_CODE)
 
+            # Delete verifications from everyone else that has this number
+            session.query(User).filter(User.phone == user.phone).update(dict(phone_verification_verified=None))
+
             user.phone_verification_token = None
             user.phone_verification_sent = None
             user.phone_verification_verified = now()
