f4af750fc49c886699548f21c10ef70efef7f5c6
Aapeli <git@aapelivuorinen.com>
2021-05-27 13:58:50 -0400

    Fix migrations

--- app/backend/src/couchers/migrations/versions/3b8d963e0b7d_regions_and_languages.py
+++ app/backend/src/couchers/migrations/versions/3b8d963e0b7d_regions_and_languages.py
@@ -1,7 +1,7 @@
 """Regions and languages
 
 Revision ID: 3b8d963e0b7d
-Revises: 763e0bd674a5
+Revises: 584f70c6a0d4
 Create Date: 2021-05-10 16:22:59.580595
 
 """
@@ -14,7 +14,7 @@ from couchers.resources import copy_resources_to_database
 
 # revision identifiers, used by Alembic.
 revision = "3b8d963e0b7d"
-down_revision = "763e0bd674a5"
+down_revision = "584f70c6a0d4"
 branch_labels = None
 depends_on = None
 
