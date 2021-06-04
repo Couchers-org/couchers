22c5e726f1871a36f495ae38c806c626ee09a252
Lennart van Laake <lennartvanlaake@gmail.com>
2021-06-04 17:01:20 +0200

    remove curly braces

--- app/prometheus/entrypoint.sh
+++ app/prometheus/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /bin/sed -i 's/{GRAFANA_CLOUD_TOKEN}/'$GRAFANA_CLOUD_TOKEN'/' /etc/prometheus/prometheus.yml
-/bin/sed -i 's/{PROMETHEUS_ENVIRONMENT}/'${PROMETHEUS_ENVIRONMENT}'/' /etc/prometheus/prometheus.yml
+/bin/sed -i 's/{PROMETHEUS_ENVIRONMENT}/'$PROMETHEUS_ENVIRONMENT'/' /etc/prometheus/prometheus.yml
 /bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
              --storage.tsdb.path=/prometheus \
              --web.console.libraries=/usr/share/prometheus/console_libraries \
