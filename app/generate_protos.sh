6551ec50093d3c681708b5b3594df8675bc1aef7
Christian Häggström <kalvdans@gmail.com>
2021-06-09 20:06:05 +0200

    Use relative imports in protocol definitions
    Fixup the broken generated code in a postprocessing step.

--- app/generate_protos.sh
+++ app/generate_protos.sh
@@ -2,17 +2,17 @@
 set -e
 
 # generate API protos and grpc stuff
-find proto -name '*.proto' | protoc -I. \
+find proto -name '*.proto' | protoc -I proto \
   --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
   --include_imports --include_source_info \
   \
   --descriptor_set_out proxy/protos.pb \
   \
-  --python_out=backend/src \
-  --grpc_python_out=backend/src \
+  --python_out=backend/src/proto \
+  --grpc_python_out=backend/src/proto \
   \
-  --python_out=media/src \
-  --grpc_python_out=media/src \
+  --python_out=media/src/proto \
+  --grpc_python_out=media/src/proto \
   \
   --js_out="import_style=commonjs,binary:frontend/src" \
   --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:frontend/src" \
@@ -23,5 +23,9 @@ find proto -name '*.proto' | protoc -I. \
 cd backend && find proto -name '*.proto' | protoc \
   --python_out=src \
   $(xargs)
+
+# fixup python3 relative imports with oneliner from
+# https://github.com/protocolbuffers/protobuf/issues/1491#issuecomment-690618628
+sed -i -E 's/^import.*_pb2/from . \0/' backend/src/proto/*.py media/src/proto/*.py
 
 echo "OK"
