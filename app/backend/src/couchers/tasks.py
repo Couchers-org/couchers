9d06ee3a5312bdea4966a6644c4527ee73cda5b5
austinburnett <aburn1011@gmail.com>
2021-06-18 21:12:04 -0400

    Added user url to urls.py and fixed friend_request_link in tasks.py

--- app/backend/src/couchers/tasks.py
+++ app/backend/src/couchers/tasks.py
@@ -141,7 +141,8 @@ def send_friend_request_email(friend_relationship):
     )
 
 def send_friend_request_accepted_email(friend_relationship):
-    friend_requests_link = urls.profile_link(friend_relationship.to_user.username)
+    friend_requests_link = urls.user_link(friend_relationship.to_user.username)
+    
 
     logger.info(f"Sending friend request acceptance email to {friend_relationship.from_user=}:")
     logger.info(f"Email for {friend_relationship.from_user.username=} sent to {friend_relationship.from_user.email=}")
