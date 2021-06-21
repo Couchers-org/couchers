873b030f78803f26ca7229a99ad7ff6e03ccfe98
austinburnett <aburn1011@gmail.com>
2021-06-21 00:42:12 -0400

    Reformatted using isort and black

--- app/backend/src/couchers/urls.py
+++ app/backend/src/couchers/urls.py
@@ -13,8 +13,10 @@ def app_link():
 def profile_link():
     return f"{config['BASE_URL']}/profile"
 
+
 def user_link(user_name):
     return f"{config['BASE_URL']}/user/{user_name}"
+
 
 def edit_profile_link():
     return f"{config['BASE_URL']}/profile/edit"
