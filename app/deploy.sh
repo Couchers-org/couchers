13159edeac8d3da7c893123619fab967cd947d31
Aapeli <git@aapelivuorinen.com>
2021-06-24 16:46:49 -0400

    Sync S3 with delete

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export AWS_PROFILE=couchers
+AWS_PROFILE=couchers
 
 AWS_S3_BUCKET_NAME=app.couchers.org
 AWS_CF_DISTRIBUTION_ID=E2PHSPAI260AGQ
@@ -12,7 +12,7 @@ pushd data/$folder
 curl -Lo artifacts.zip "https://gitlab.com/couchers/couchers/-/jobs/artifacts/develop/download?job=build:frontend"
 unzip artifacts.zip
 echo "Syncing S3"
-aws s3 sync artifacts/frontend/ s3://$AWS_S3_BUCKET_NAME --acl public-read
+aws s3 sync artifacts/frontend/ s3://$AWS_S3_BUCKET_NAME --delete --acl public-read
 
 echo "Creating invalidation"
 aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths "/*"
