776b99f149f3b80ad34ddcac01cfb45251162310
ItsiW <itsi148@gmail.com>
2021-07-13 16:42:43 +1000

    Add Poovan to team page

--- site/pages/team.vue
+++ site/pages/team.vue
@@ -247,6 +247,25 @@
                     <p class="title is-4">Nolo</p>
                     <p class="subtitle is-6 role">Community Forum</p>
                     <p><span class="icon has-text-info"><i class="mdi mdi-map-marker"></i></span>Las Palmas, Spain</p>
+                    <p><span class="icon has-text-info"><i class="mdi mdi-linkedin"></i></span><a href="https://www.linkedin.com/in/goldpoovan/">goldpoovan</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column is-one-third">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-96x96">
+                      <img class="is-rounded" src="/img/head-poovan.jpg" alt="Headshot of Poovan">
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">Poovan Devasagayam</p>
+                    <p class="subtitle is-6 role">Digital Marketer</p>
+                    <p><span class="icon has-text-info"><i class="mdi mdi-map-marker"></i></span>Paris, France</p>
                   </div>
                 </div>
               </div>
