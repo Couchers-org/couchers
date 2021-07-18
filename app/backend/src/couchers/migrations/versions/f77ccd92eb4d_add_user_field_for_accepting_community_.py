bc62c1d0d26e76381b7e35bb0a44963e0d0c9f1c
Aapeli <git@aapelivuorinen.com>
2021-07-18 11:36:15 +0300

    Fix migrations

--- app/backend/src/couchers/migrations/versions/f77ccd92eb4d_add_user_field_for_accepting_community_.py
+++ app/backend/src/couchers/migrations/versions/f77ccd92eb4d_add_user_field_for_accepting_community_.py
@@ -1,7 +1,7 @@
 """Add user field for accepting community guidelines
 
 Revision ID: f77ccd92eb4d
-Revises: e6d03b494119
+Revises: a19fc23547ce
 Create Date: 2021-07-15 23:58:27.233804
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "f77ccd92eb4d"
-down_revision = "e6d03b494119"
+down_revision = "a19fc23547ce"
 branch_labels = None
 depends_on = None
 
