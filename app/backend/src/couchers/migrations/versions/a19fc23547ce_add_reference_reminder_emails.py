b50c19d1e46f571c569e9dad697e566101889b95
Aapeli <git@aapelivuorinen.com>
2021-07-18 11:29:03 +0300

    Fix migrations

--- app/backend/src/couchers/migrations/versions/a19fc23547ce_add_reference_reminder_emails.py
+++ app/backend/src/couchers/migrations/versions/a19fc23547ce_add_reference_reminder_emails.py
@@ -1,7 +1,7 @@
 """Add reference reminder emails
 
 Revision ID: a19fc23547ce
-Revises: 62fcd41e4dcd
+Revises: e6d03b494119
 Create Date: 2021-07-13 01:18:08.138793
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "a19fc23547ce"
-down_revision = "62fcd41e4dcd"
+down_revision = "e6d03b494119"
 branch_labels = None
 depends_on = None
 
