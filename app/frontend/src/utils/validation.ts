94d57b75abb153c79aaca0dd0a55ba206b03ea7f
Darren Vong <darrenvong@users.noreply.github.com>
2021-07-26 23:34:25 +0100

    fix: make email validation case insensitive

--- app/frontend/src/utils/validation.ts
+++ app/frontend/src/utils/validation.ts
@@ -2,7 +2,7 @@
 export const nameValidationPattern = /\S+/;
 export const usernameValidationPattern = /^[a-z][0-9a-z_]*[a-z0-9]$/i;
 export const emailValidationPattern =
-  /^[0-9a-z][0-9a-z\-_+.]*@([0-9a-z-]+\.)*[0-9a-z-]+\.[a-z]{2,}$/;
+  /^[0-9a-z][0-9a-z\-_+.]*@([0-9a-z-]+\.)*[0-9a-z-]+\.[a-z]{2,}$/i;
 
 export function validatePastDate(stringDate: string) {
   const date = new Date(stringDate);
