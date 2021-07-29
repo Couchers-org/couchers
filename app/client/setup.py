c52256102bb305b4202a20aeb5d6d8cc8759c3ca
Aapeli <git@aapelivuorinen.com>
2021-07-29 22:54:21 +0300

    Bump and release pypi package

--- app/client/setup.py
+++ app/client/setup.py
@@ -12,7 +12,7 @@ Usage of the Couchers.org service provided by the Couchers.org Foundation is gov
 
 setuptools.setup(
     name="couchers",
-    version="0.0.19",
+    version="0.0.21",
     author="Couchers.org Foundation and Contributors",
     author_email="support@couchers.org",
     description="Couchers.org: The next-generation couch surfing platform. Free forever. Community‑led. Non‑profit. Modern.",
