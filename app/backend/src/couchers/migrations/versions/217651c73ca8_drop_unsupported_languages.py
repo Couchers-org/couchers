31392bf39452ac51148384304af4ac5e2bb083eb
ShmuelTreiger <shmueltreiger@gmail.com>
2021-08-02 20:04:39 -0700

    Fixing migration

--- app/backend/src/couchers/migrations/versions/217651c73ca8_drop_unsupported_languages.py
+++ app/backend/src/couchers/migrations/versions/217651c73ca8_drop_unsupported_languages.py
@@ -13,7 +13,7 @@ from couchers.resources import copy_resources_to_database
 
 # revision identifiers, used by Alembic.
 revision = "217651c73ca8"
-down_revision = "e938e80b67a4"
+down_revision = "f2abd32dee6e"
 branch_labels = None
 depends_on = None
 
@@ -21,6 +21,11 @@ depends_on = None
 def upgrade():
     session = Session(bind=op.get_bind())
     copy_resources_to_database(session)
+    op.execute("UPDATE language_abilities SET language_code = 'arb' WHERE language_code = 'ara';")
+    op.execute("UPDATE language_abilities SET language_code = 'cmn' WHERE language_code = 'zho';")
+    op.execute(
+        "DELETE FROM language_abilities WHERE language_code in ('ara', 'arg', 'ave', 'bis', 'bre', 'cha', 'cor', 'cos', 'cre', 'div', 'dzo', 'fao', 'fij', 'gla', 'gle', 'glv', 'her', 'hmo', 'ido', 'iku', 'ipk', 'kua', 'mah', 'mri', 'nau', 'nav', 'oci', 'oji', 'pli', 'roh', 'sag', 'san', 'sme', 'tah', 'ton', 'vol', 'zho');"
+    )
     session.commit()
 
 
