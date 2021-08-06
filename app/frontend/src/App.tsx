7c1edf26029b31745b33c708cca2782ea9f4c675
Lucas levin <lucaslevin@gmail.com>
2021-08-06 21:09:17 +0100

    add react-snap

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
 
-import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import {
+  createGenerateClassName,
+  CssBaseline,
+  StylesProvider,
+  ThemeProvider,
+} from "@material-ui/core";
 import { EnvironmentBanner } from "components/EnvironmentBanner";
 import ErrorBoundary from "components/ErrorBoundary";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,20 +15,27 @@ import AuthProvider from "./features/auth/AuthProvider";
 import { ReactQueryClientProvider } from "./reactQueryClient";
 import { theme } from "./theme";
 
+//for react-snap https://github.com/stereobooster/react-snap/issues/99
+const generateClassName = createGenerateClassName({
+  productionPrefix: navigator.userAgent === "ReactSnap" ? "snap" : "jss",
+});
+
 function App() {
   return (
     <Router>
-      <ThemeProvider theme={theme}>
-        <ErrorBoundary isFatal>
-          <ReactQueryClientProvider>
-            <AuthProvider>
-              <CssBaseline />
-              <EnvironmentBanner />
-              <AppRoutes />
-            </AuthProvider>
-          </ReactQueryClientProvider>
-        </ErrorBoundary>
-      </ThemeProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <ThemeProvider theme={theme}>
+          <ErrorBoundary isFatal>
+            <ReactQueryClientProvider>
+              <AuthProvider>
+                <CssBaseline />
+                <EnvironmentBanner />
+                <AppRoutes />
+              </AuthProvider>
+            </ReactQueryClientProvider>
+          </ErrorBoundary>
+        </ThemeProvider>
+      </StylesProvider>
     </Router>
   );
 }
