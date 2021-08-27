1582eae7115f51fde5a30ccc379ee6a883183add
stifler6005 <mastergoyal2001@gmail.com>
2021-08-27 13:18:48 +0530

    creatred issue1837

--- app/backend/src/couchers/servicers/auth.py
+++ app/backend/src/couchers/servicers/auth.py
@@ -157,11 +157,6 @@ class Auth(auth_pb2_grpc.AuthServicer):
                     if existing_user:
                         context.abort(grpc.StatusCode.FAILED_PRECONDITION, errors.SIGNUP_FLOW_EMAIL_TAKEN)
 
-                    existing_user_name = session.execute(
-                        select(User).where(User.username == request.basic.username)
-                    ).scalar_one_or_none()
-                    if existing_user_name:
-                        context.abort(grpc.StatusCode.FAILED_PRECONDITION, errors.SIGNUP_FLOW_USERNAME_TAKEN)
 
                     existing_flow = session.execute(
                         select(SignupFlow).where(SignupFlow.email == request.basic.email)
