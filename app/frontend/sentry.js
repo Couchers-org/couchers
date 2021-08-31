0924d75f3b5a245476002438aa021fcfefe9af7f
Darren Vong <darrenvong@users.noreply.github.com>
2021-08-31 02:16:22 +0100

    Add command for adding source map to Sentry

---
+++ app/frontend/sentry.js
@@ -0,0 +1,30 @@
+const SentryCli = require("@sentry/cli");
+
+async function createReleaseAndUpload() {
+  const release = process.env.REACT_APP_VERSION;
+  if (!release) {
+    console.warn("REACT_APP_VERSION is not set");
+    return;
+  }
+
+  /**
+   * @type import("@sentry/cli").default
+   */
+  const cli = new SentryCli();
+
+  try {
+    console.log(`Creating sentry release ${release}`);
+    await cli.releases.new(release);
+    console.log("Uploading source maps");
+    await cli.releases.uploadSourceMaps(release, {
+      include: ["build/static/js"],
+      urlPrefix: "~/static/js",
+      rewrite: false,
+    });
+    console.log("Finalizing release");
+    await cli.releases.finalize(release);
+  } catch (e) {
+    console.error("Source maps uploading failed:", e);
+  }
+}
+createReleaseAndUpload();
