068d3b992420d0facccd1d7d0c08450458f2f0c2
Lucas levin <lucaslevin@gmail.com>
2021-09-14 23:25:52 +0100

    change jss workaround to tagging react-snap jss then removing on hydrate

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,11 +1,6 @@
 import "./App.css";
 
-import {
-  createGenerateClassName,
-  CssBaseline,
-  StylesProvider,
-  ThemeProvider,
-} from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { EnvironmentBanner } from "components/EnvironmentBanner";
 import ErrorBoundary from "components/ErrorBoundary";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -15,27 +10,20 @@ import AuthProvider from "./features/auth/AuthProvider";
 import { ReactQueryClientProvider } from "./reactQueryClient";
 import { theme } from "./theme";
 
-//for react-snap https://github.com/stereobooster/react-snap/issues/99
-const generateClassName = createGenerateClassName({
-  productionPrefix: navigator.userAgent === "ReactSnap" ? "snap" : "jss",
-});
-
 function App() {
   return (
     <Router>
-      <StylesProvider generateClassName={generateClassName} injectFirst={true}>
-        <ThemeProvider theme={theme}>
-          <ErrorBoundary isFatal>
-            <ReactQueryClientProvider>
-              <AuthProvider>
-                <CssBaseline />
-                <EnvironmentBanner />
-                <AppRoutes />
-              </AuthProvider>
-            </ReactQueryClientProvider>
-          </ErrorBoundary>
-        </ThemeProvider>
-      </StylesProvider>
+      <ThemeProvider theme={theme}>
+        <ErrorBoundary isFatal>
+          <ReactQueryClientProvider>
+            <AuthProvider>
+              <CssBaseline />
+              <EnvironmentBanner />
+              <AppRoutes />
+            </AuthProvider>
+          </ReactQueryClientProvider>
+        </ErrorBoundary>
+      </ThemeProvider>
     </Router>
   );
 }
