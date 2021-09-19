f0db27361f9b256ab9f59a6d5ffbe3e28c6e1469
Lucas levin <lucaslevin@gmail.com>
2021-09-19 21:32:48 +0100

    force app rerender on hydrate to try to fix jss classname mismatching

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { EnvironmentBanner } from "components/EnvironmentBanner";
 import ErrorBoundary from "components/ErrorBoundary";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import AppRoutes from "./AppRoutes";
@@ -11,6 +12,10 @@ import { ReactQueryClientProvider } from "./reactQueryClient";
 import { theme } from "./theme";
 
 function App() {
+  //workaround to force re-render on hydration to avoid mismatching jss classNames
+  const [, setRender] = useState(0);
+  useEffect(() => setRender(1), []);
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
