4794a420546bf37a73717eaa419a234922857359
William Bruntrager <w.bruntrager@gmail.com>
2021-10-02 08:45:25 +0000

    fix: change (oversized) homepage alert links to buttons

--- src/features/dashboard/DashboardBanners.tsx
+++ src/features/dashboard/DashboardBanners.tsx
@@ -1,12 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { Button as MuiButton, Typography } from "@material-ui/core";
 import { Alert as MuiAlert } from "@material-ui/lab/";
 import Alert from "components/Alert";
-import StyledLink from "components/StyledLink";
 import { Error as GrpcError } from "grpc-web";
 import { GetAccountInfoRes } from "proto/account_pb";
 import { accountInfoQueryKey } from "queryKeys";
 import React from "react";
 import { useQuery } from "react-query";
+import { Link as RouterLink } from "react-router-dom";
 import { routeToEditProfile, settingsRoute } from "routes";
 import { service } from "service";
 import makeStyles from "utils/makeStyles";
@@ -16,7 +16,6 @@ import {
   DONT_YOU_HATE,
   FILL_IN_WHO_I_AM,
   PASSWORD_TEXT_1,
-  PASSWORD_TEXT_2,
   PASSWORD_TEXT_LINK,
   PLEASE_COMPLETE_PROFILE,
   UPLOAD_PHOTO,
@@ -25,6 +24,9 @@ import {
 const useStyles = makeStyles((theme) => ({
   alert: {
     marginBottom: theme.spacing(2),
+  },
+  settingsButton: {
+    marginRight: theme.spacing(1),
   },
 }));
 
@@ -51,9 +53,15 @@ export default function DashboardBanners() {
                 {UPLOAD_PHOTO}
               </Typography>
               <Typography variant="inherit" paragraph>
-                <StyledLink to={routeToEditProfile()}>
+                <MuiButton
+                  variant="contained"
+                  color="default"
+                  component={RouterLink}
+                  to={routeToEditProfile()}
+                  role="link"
+                >
                   {CLICK_HERE_TO_EDIT}
-                </StyledLink>
+                </MuiButton>
               </Typography>
               <Typography variant="inherit">{DONT_YOU_HATE}</Typography>
             </MuiAlert>
@@ -61,9 +69,17 @@ export default function DashboardBanners() {
           {!data.hasPassword && (
             <MuiAlert className={classes.alert} severity="info">
               <Typography variant="inherit">
-                {PASSWORD_TEXT_1}{" "}
-                <StyledLink to={settingsRoute}>{PASSWORD_TEXT_LINK}</StyledLink>{" "}
-                {PASSWORD_TEXT_2}
+                <MuiButton
+                  className={classes.settingsButton}
+                  variant="contained"
+                  color="default"
+                  component={RouterLink}
+                  to={settingsRoute}
+                  role="link"
+                >
+                  {PASSWORD_TEXT_LINK}
+                </MuiButton>
+                {PASSWORD_TEXT_1}
               </Typography>
             </MuiAlert>
           )}
