e3552b1f5d49ca8b9ff5be671bfd40c9abe21834
William Bruntrager <w.bruntrager@gmail.com>
2021-10-02 08:45:25 +0000

    fix: change (oversized) homepage alert links to buttons

--- app/web/src/features/dashboard/constants.ts
+++ app/web/src/features/dashboard/constants.ts
@@ -4,14 +4,13 @@ export const FILL_IN_WHO_I_AM =
   '1. Fill in your "Who I am" section with a few sentences about yourself';
 export const UPLOAD_PHOTO =
   "2. Upload a photo (by clicking on the avatar on the edit page)";
-export const CLICK_HERE_TO_EDIT = "Click here to edit your profile";
+export const CLICK_HERE_TO_EDIT = "Edit your profile";
 export const DONT_YOU_HATE =
   "Don't you hate how other platforms have empty \"ghost\" profiles with no information. Please don't ghost us! 👻👻👻";
 
-export const PASSWORD_TEXT_1 = "You can set a password on";
-export const PASSWORD_TEXT_LINK = "the account settings page";
-export const PASSWORD_TEXT_2 =
-  "so you don't need check your email each time you log in.";
+export const PASSWORD_TEXT_1 =
+  "Tip: You can set a password so you don't need check your email each time you log in";
+export const PASSWORD_TEXT_LINK = "Account Settings";
 
 export const YOUR_COMMUNITIES_HEADING = "Your communities";
 export const ALL_COMMUNITIES_HEADING = "All communities";
