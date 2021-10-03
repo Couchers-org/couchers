91b82b4e269f6026efdde9f315d033f4eb534c2e
goskan93 <natalia.goska@gmail.com>
2021-10-03 11:43:09 +0200

    add bottom padding to container

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -21,6 +21,7 @@ export const useAppRouteStyles = makeStyles((theme) => ({
   standardContainer: {
     paddingLeft: theme.spacing(2),
     paddingRight: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
   },
   fullWidthContainer: {
     margin: "0 auto",
