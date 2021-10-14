d7daba7230d4bf0cefce0ea7cf51a4e533ece842
Darren Vong <darrenvong@users.noreply.github.com>
2021-10-14 02:00:27 +0100

    refactor: translation keys on donation page to be more semantic

--- src/features/donations/constants.ts
+++ src/features/donations/constants.ts
@@ -1,16 +1,7 @@
-export const DONATIONSBOX_ALERT_SUCCESS =
-  "Thank you for donating to Couchers.org, we appreciate your support! You will receive an email shortly.";
-export const DONATIONSBOX_ALERT_WARNING = "The payment was cancelled.";
 export const DONATIONSBOX_RECURRING =
   "A monthly donation helps us build our operations on a stable basis and plan ahead.";
 export const DONATIONSBOX_MONTHLY = "Monthly";
 export const DONATIONSBOX_ONETIME = "One-time";
-export const DONATIONSBOX_REQUIRED = "This field is required";
-export const DONATIONSBOX_NEXT = "Next";
-export const DONATIONSBOX_RECURRING_ARIA = "recurring";
-export const DONATIONSBOX_TITLE = "Donation Amount";
-export const DONATIONSBOX_TEXT =
-  "You will be redirected to Stripe, our payment processor, to complete your donation.";
 export const DONATIONSBOX_CURRENCY = "$";
 export const DONATIONSBOX_VALUES = [
   { amount: 3, currency: "$" },
