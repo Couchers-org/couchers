8c9f3da8a2963a574ed3cd9f178e6f86ca4780de
foormea <foormea@gmail.com>
2021-10-16 23:47:22 +0200

    #1996: clarify Yue = Cantonese: json edit + alembic migration

---
+++ app/backend/src/couchers/migrations/versions/1c809d111871_1996_language_list_yue.py
@@ -0,0 +1,30 @@
+"""1996_language_list_yue
+
+Revision ID: 1c809d111871
+Revises: 1e9b694d08c0
+Create Date: 2021-10-16 23:31:43.298208
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.orm.session import Session
+
+from couchers.resources import copy_resources_to_database
+
+# revision identifiers, used by Alembic.
+revision = "1c809d111871"
+down_revision = "1e9b694d08c0"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    session = Session(bind=op.get_bind())
+    copy_resources_to_database(session)
+    op.execute(
+        "UPDATE languages SET name = 'Chinese (Cantonese/Yue)' WHERE code = 'yue';")
+    session.commit()
+
+
+def downgrade():
+    pass
