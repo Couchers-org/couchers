950c1269cb42287663dcaf58d82da6c99b84a661
Lucas levin <lucaslevin@gmail.com>
2021-10-18 11:05:06 +0800

    add MarkAllReadButton tests

--- app/web/src/features/messages/requests/MarkAllReadButton.tsx
+++ app/web/src/features/messages/requests/MarkAllReadButton.tsx
@@ -1,7 +1,9 @@
 import { Tooltip } from "@material-ui/core";
 import IconButton from "components/IconButton";
 import { DoneAllIcon } from "components/Icons";
+import Snackbar from "components/Snackbar";
 import { MARK_ALL_READ } from "features/messages/constants";
+import { Error as GrpcError } from "grpc-web";
 import { useMutation } from "react-query";
 import { service } from "service";
 import getAllPages from "utils/getAllPages";
@@ -11,7 +13,7 @@ export default function MarkAllReadButton({
 }: {
   type: "chats" | "hosting" | "surfing";
 }) {
-  const markAll = useMutation(async () => {
+  const markAll = useMutation<void, GrpcError>(async () => {
     if (type === "chats") {
       const data = await getAllPages({
         serviceFunction: service.conversations.listGroupChats,
@@ -21,7 +23,8 @@ export default function MarkAllReadButton({
       });
       await Promise.all(
         data.map<void>((chat) =>
-          chat.latestMessage
+          chat.latestMessage &&
+          chat.lastSeenMessageId < chat.latestMessage.messageId
             ? service.conversations.markLastSeenGroupChat(
                 chat.groupChatId,
                 chat.latestMessage.messageId
@@ -41,7 +44,8 @@ export default function MarkAllReadButton({
       });
       await Promise.all(
         data.map<void>((request) =>
-          request.latestMessage
+          request.latestMessage &&
+          request.lastSeenMessageId < request.latestMessage.messageId
             ? service.requests.markLastRequestSeen(
                 request.hostRequestId,
                 request.latestMessage.messageId
@@ -53,14 +57,19 @@ export default function MarkAllReadButton({
   });
 
   return (
-    <Tooltip title={MARK_ALL_READ}>
-      <IconButton
-        aria-label={MARK_ALL_READ}
-        loading={markAll.isLoading}
-        onClick={() => markAll.mutate()}
-      >
-        <DoneAllIcon />
-      </IconButton>
-    </Tooltip>
+    <>
+      {markAll.error && (
+        <Snackbar severity="error">{markAll.error.message}</Snackbar>
+      )}
+      <Tooltip title={MARK_ALL_READ}>
+        <IconButton
+          aria-label={MARK_ALL_READ}
+          loading={markAll.isLoading}
+          onClick={() => markAll.mutate()}
+        >
+          <DoneAllIcon />
+        </IconButton>
+      </Tooltip>
+    </>
   );
 }
