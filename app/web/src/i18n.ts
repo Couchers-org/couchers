35621b1b7274c0e87c09e588df47880d55a9e4bf
Darren Vong <darrenvong@users.noreply.github.com>
2021-11-21 23:39:56 +0000

    chore: add language detector to i18n config

--- app/web/src/i18n.ts
+++ app/web/src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .use(
     resourcesToBackend((language, namespace, callback) => {
       if (namespace !== "global") {
