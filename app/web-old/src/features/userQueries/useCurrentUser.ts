d52452f70558ff4d5ecb9e09fc0b016a235a8caa
Lucas levin <lucaslevin@gmail.com>
2021-12-01 23:31:44 +0800

    move rest of files except storybook stuff

--- app/web-old/src/features/userQueries/useCurrentUser.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthContext } from "features/auth/AuthProvider";
-import { useUser } from "features/userQueries/useUsers";
-
-export default function useCurrentUser() {
-  const authState = useAuthContext().authState;
-  if (!authState.userId)
-    throw new Error("No user id available to get current user.");
-  return useUser(authState.userId);
-}
