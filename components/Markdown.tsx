69d43b1ca77f6a50009d32c7c897facb27d95009
Lucas levin <lucaslevin@gmail.com>
2021-12-16 22:03:52 +0800

    fix markdown components

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -6,4 +6,19 @@ const Markdown = dynamic(() => import("components/MarkdownNoSSR"), {
 
 export default Markdown;
 
-export { increaseMarkdownHeaderLevel } from "./MarkdownNoSSR";
+export function increaseMarkdownHeaderLevel(
+  source: string,
+  topHeaderLevel: number
+) {
+  let convertedSource = source;
+  for (let i = 6; i >= 1; i--) {
+    //loop through each header level, and add extra # as necessary
+    //also put a ~ in front so we know that line is done
+    convertedSource = convertedSource.replace(
+      new RegExp(`^${"#".repeat(i)}`, "gm"),
+      `~${"#".repeat(Math.min(i + topHeaderLevel - 1, 6))}`
+    );
+  }
+  //take out the ~ markers (line start only)
+  return convertedSource.replace(/^~#/gm, "#");
+}
--- components/MarkdownNoSSR.tsx
+++ components/MarkdownNoSSR.tsx
@@ -2,6 +2,7 @@ import "@toast-ui/editor/dist/toastui-editor-viewer.css";
 
 import ToastUIEditorViewer from "@toast-ui/editor/dist/toastui-editor-viewer";
 import classNames from "classnames";
+import { increaseMarkdownHeaderLevel } from "components/Markdown";
 import { useEffect, useRef } from "react";
 import { escapeRegExp } from "utils/escapeRegExp";
 import makeStyles from "utils/makeStyles";
@@ -81,21 +82,4 @@ export default function Markdown({
   }, [source, topHeaderLevel, allowImages]);
 
   return <div className={classNames(className, classes.root)} ref={rootEl} />;
-}
-
-export function increaseMarkdownHeaderLevel(
-  source: string,
-  topHeaderLevel: number
-) {
-  let convertedSource = source;
-  for (let i = 6; i >= 1; i--) {
-    //loop through each header level, and add extra # as necessary
-    //also put a ~ in front so we know that line is done
-    convertedSource = convertedSource.replace(
-      new RegExp(`^${"#".repeat(i)}`, "gm"),
-      `~${"#".repeat(Math.min(i + topHeaderLevel - 1, 6))}`
-    );
-  }
-  //take out the ~ markers (line start only)
-  return convertedSource.replace(/^~#/gm, "#");
 }
