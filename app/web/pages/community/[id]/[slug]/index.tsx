eb7092b5c25ac49a0a47b768ec94edbf88fb031e
Lucas levin <lucaslevin@gmail.com>
2021-12-29 23:57:14 +0800

    change communities optional catch-all to catch-all
    it wasn't working when deployed to vercel (works
    locally)

--- app/web/pages/community/[id]/[slug]/index.tsx
+++ app/web/pages/community/[id]/[slug]/index.tsx
@@ -1,17 +1,11 @@
 import { appGetLayout } from "components/AppRoute";
 import CommunityPageComponent from "features/communities/CommunityPage";
 import NotFoundPage from "features/NotFoundPage";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import nextI18nextConfig from "next-i18next.config";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { communityTabs } from "routes";
 import stringOrFirstString from "utils/stringOrFirstString";
-
-export const getStaticPaths: GetStaticPaths = () => ({
-  paths: [""],
-  fallback: "blocking",
-});
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
@@ -23,26 +17,19 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   },
 });
 
+// Using optional catch all route [[...params]] doesn't work here when deployed only
+// - maybe because of the other dynamic parts of the path...
 export default function CommunityPage() {
   const router = useRouter();
 
   const parsedId = Number.parseInt(stringOrFirstString(router.query.id) ?? "");
   if (isNaN(parsedId)) return <NotFoundPage />;
-  //first element of params is the tab
-  const tab = stringOrFirstString(router.query.params);
-  let parsedTab = undefined;
-  if (tab) {
-    //if the tab isn't blank and isn't valid, 404
-    parsedTab = communityTabs.find((valid) => tab === valid);
-    if (!parsedTab) return <NotFoundPage />;
-  }
-  const edit = router.query.params?.[1] === "edit";
 
   return (
     <CommunityPageComponent
       communityId={parsedId}
-      tab={parsedTab}
-      edit={edit}
+      tab={undefined}
+      edit={undefined}
     />
   );
 }
