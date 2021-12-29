5f71fc6bc0a1597fbe64f5bb667facde92af73c6
Lucas levin <lucaslevin@gmail.com>
2021-12-30 00:00:22 +0800

    add missing getStaticPaths

--- app/web/pages/community/[id]/[slug]/index.tsx
+++ app/web/pages/community/[id]/[slug]/index.tsx
@@ -1,11 +1,16 @@
 import { appGetLayout } from "components/AppRoute";
 import CommunityPageComponent from "features/communities/CommunityPage";
 import NotFoundPage from "features/NotFoundPage";
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import nextI18nextConfig from "next-i18next.config";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import stringOrFirstString from "utils/stringOrFirstString";
+
+export const getStaticPaths: GetStaticPaths = () => ({
+  paths: [],
+  fallback: "blocking",
+});
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
