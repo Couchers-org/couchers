25c8a19120c4e0b153df260817c87e4e294a8de0
Aapeli <git@aapelivuorinen.com>
2022-01-08 14:21:44 +0200

    Hotfix: fix dl-protos.sh

--- app/web/dl-protos.sh
+++ app/web/dl-protos.sh
@@ -1,3 +1,3 @@
 curl -L https://gitlab.com/couchers/couchers/-/jobs/artifacts/develop/download\?job\=protos -o /tmp/protos.zip
-unzip /tmp/protos.zip app/web/src/proto/* -d proto
-mv proto/app/web/src/proto/* proto
+unzip /tmp/protos.zip app/web/proto/* -d proto
+mv proto/app/web/proto/* proto
