6464d918bfecaf8905b26f7bbd88c2bd8bc4b53c
Aapeli <git@aapelivuorinen.com>
2022-01-09 21:33:32 +0200

    Review comments

--- features/auth/notifications/NotificationSettings.tsx
+++ features/auth/notifications/NotificationSettings.tsx
@@ -4,16 +4,23 @@ import Button from "components/Button";
 import CircularProgress from "components/CircularProgress";
 import { notificationSettingsQueryKey } from "features/queryKeys";
 import { Error as GrpcError } from "grpc-web";
-import { GetNotificationSettingsRes } from "proto/notifications_pb";
+import {
+  GetNotificationSettingsRes,
+  SetNotificationSettingsRes,
+} from "proto/notifications_pb";
 import { useTranslation } from "react-i18next";
-import { useQuery, useQueryClient } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { service } from "service";
 
-type NotificationSettingsProps = {
-  className?: string;
-};
+interface NotificationSettingFormData {
+  newNotificationsEnabled: boolean;
+}
 
-export default function NotificationSettings(props: NotificationSettingsProps) {
+export default function NotificationSettings({
+  className,
+}: {
+  className: string;
+}) {
   const { t } = useTranslation("auth");
 
   const queryClient = useQueryClient();
@@ -26,32 +33,54 @@ export default function NotificationSettings(props: NotificationSettingsProps) {
     service.notifications.getNotificationSettings
   );
 
+  const mutation = useMutation<
+    SetNotificationSettingsRes.AsObject,
+    GrpcError,
+    NotificationSettingFormData
+  >(
+    ({ newNotificationsEnabled }) =>
+      service.notifications.setNotificationSettings(newNotificationsEnabled),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(notificationSettingsQueryKey);
+      },
+    }
+  );
+
   const toggleNewNotifications = async () => {
-    await service.notifications.setNotificationSettings(
-      !data?.newNotificationsEnabled
-    );
-    queryClient.invalidateQueries(notificationSettingsQueryKey);
+    mutation.mutate({
+      newNotificationsEnabled: !data?.newNotificationsEnabled,
+    });
   };
 
-  const { className } = props;
   return (
     <div className={className}>
       <Typography variant="h2">{t("notification_settings.title")}</Typography>
-      {isLoading ? (
+      {(isLoading || !data) ? (
         <CircularProgress />
       ) : error ? (
         <Alert severity="error">{error.message}</Alert>
       ) : (
-        <Typography variant="body1">
-          The new notification system is currently{" "}
-          <b>{data.newNotificationsEnabled ? "enabled" : "disabled"}</b> for
-          your account.
-          <br />
-          <Button onClick={() => toggleNewNotifications()}>
-            {data.newNotificationsEnabled ? "Disable" : "Enable"} new
-            notification system.
-          </Button>
-        </Typography>
+        <>
+          <Typography variant="body1">
+            The new notification system is currently{" "}
+            <b>{data.newNotificationsEnabled ? "enabled" : "disabled"}</b> for
+            your account.
+          </Typography>
+          {mutation.error ? (
+            <Alert severity="error">{mutation.error.message}</Alert>
+          ) : (
+            <Typography variant="body1">
+              <Button
+                onClick={() => toggleNewNotifications()}
+                loading={mutation.isLoading}
+              >
+                {data.newNotificationsEnabled ? "Disable" : "Enable"} new
+                notification system.
+              </Button>
+            </Typography>
+          )}
+        </>
       )}
     </div>
   );
