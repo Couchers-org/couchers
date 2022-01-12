00007c84d1c851388124b28066d758576f8fa19e
Ricardo Keigo Andrade <rickean@gmail.com>
2022-01-12 11:30:18 -0300

    fix: resources custom type must be a const

--- i18n/resources.ts
+++ i18n/resources.ts
@@ -8,18 +8,16 @@ import profile from "features/profile/locales/en.json";
 import search from "features/search/locales/en.json";
 import global from "resources/locales/en.json";
 
-import { NAMESPACES } from "./namespaces";
-
 const resources = {
-  [NAMESPACES.AUTH]: auth,
-  [NAMESPACES.COMMUNITIES]: communities,
-  [NAMESPACES.CONNECTIONS]: connections,
-  [NAMESPACES.DASHBOARD]: dashboard,
-  [NAMESPACES.DONATIONS]: donations,
-  [NAMESPACES.MESSAGES]: messages,
-  [NAMESPACES.PROFILE]: profile,
-  [NAMESPACES.SEARCH]: search,
-  [NAMESPACES.GLOBAL]: global,
+  auth,
+  communities,
+  connections,
+  dashboard,
+  donations,
+  messages,
+  profile,
+  search,
+  global,
 } as const;
 
 export default resources;
