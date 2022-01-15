76a6fb455c452bbee7d219514d7f31e305fda27a
Aapeli <git@aapelivuorinen.com>
2022-01-15 14:59:08 +0200

    Work on styling

--- features/markdown/MarkdownPage.tsx
+++ features/markdown/MarkdownPage.tsx
@@ -1,8 +1,14 @@
-import { Breadcrumbs, Container, Link, makeStyles, Typography } from "@material-ui/core";
+import {
+  Breadcrumbs,
+  Container,
+  Link,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import HtmlMeta from "components/HtmlMeta";
-import markdown from 'markdown-it'
+import markdown from "markdown-it";
 
-const mkd = new markdown()
+const mkd = new markdown();
 
 export interface MarkdownPageFrontmatter {
   title: string;
@@ -21,61 +27,131 @@ export interface MarkdownPageProps {
   content: string;
 }
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    maxWidth: "769px",
+    marginTop: theme.spacing(3),
+    fontSize: theme.typography.fontSize,
+    fontFamily: theme.typography.fontFamily,
+    "& h1, & h2, & h3, & h4, & h5, & h6, & p": {
+      borderBottom: "none",
+      paddingBottom: 0,
+      marginBottom: 0,
+      marginTop: theme.spacing(2),
+      overflowWrap: "break-word",
+      whiteSpace: "pre-wrap",
+    },
+    "& h1": { ...theme.typography.h1, fontSize: "2.5rem", lineHeight: "1.125" },
+    "& h2": { ...theme.typography.h2, fontSize: "1.75em !important" },
+    "& h3": { ...theme.typography.h3, fontSize: "1.5em !important" },
+    "& h4": theme.typography.h4,
+    "& h5": theme.typography.h5,
+    "& h6": theme.typography.h6,
+    "& p": theme.typography.body1,
+    "& ol": theme.typography.body1,
+    "& ul": theme.typography.body1,
+    "& blockquote": theme.typography.body1,
+    "& a": {
+      color: "#3273dc",
+    },
+    "& img": {
+      width: "100%",
+      maxWidth: "400px",
+      height: "auto",
+    },
+  },
+  crumbs: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+  title: {
+    fontSize: "2.5rem !important",
+    lineHeight: "1.125 !important",
+  },
+}));
 
-function CreateBreadcrumbs({slug, frontmatter}: {slug: Array<string>; frontmatter: MarkdownPageFrontmatter}) {
-  const crumbs = [{key:"root",value:"Couchers.org",path:"/"}]
+function CreateBreadcrumbs({
+  slug,
+  frontmatter,
+}: {
+  slug: Array<string>;
+  frontmatter: MarkdownPageFrontmatter;
+}) {
+  const crumbs = [{ key: "root", value: "Couchers.org", path: "/" }];
   if (slug.length > 2 && slug[0] == "blog") {
     // this is fragile, but basically hides the date from the blog crumbs
     crumbs.push({
       key: "blog",
       value: "Blog",
-      path: "/blog/"
-    })
+      path: "/blog/",
+    });
     crumbs.push({
       key: slug[-1],
       value: frontmatter.title,
-      path: "/" + slug.join("/") + "/"
-    })
+      path: "/" + slug.join("/") + "/",
+    });
   } else {
     for (let i = 0; i < slug.length; i++) {
-      const item = slug[i]
+      const item = slug[i];
       crumbs.push({
         key: item,
-        value: i == slug.length - 1 ? (frontmatter.crumb ? frontmatter.crumb : frontmatter.title) : item.substring(0, 1).toUpperCase() + item.substring(1, item.length),
-        path: (i == 0 ? "/" : crumbs[i-1].path) + item + "/"
-      })
+        value:
+          i == slug.length - 1
+            ? frontmatter.crumb
+              ? frontmatter.crumb
+              : frontmatter.title
+            : item.substring(0, 1).toUpperCase() +
+              item.substring(1, item.length),
+        path: (i == 0 ? "/" : crumbs[i - 1].path) + item + "/",
+      });
     }
   }
-  return crumbs
+  return crumbs;
 }
 
-export default function MarkdownPage({slug, frontmatter, content}: MarkdownPageProps) {
+export default function MarkdownPage({
+  slug,
+  frontmatter,
+  content,
+}: MarkdownPageProps) {
   const classes = useStyles();
 
-  const subtitle = !!frontmatter.subtitle ? mkd.renderInline(frontmatter.subtitle) : null
-  const bustitle = !!frontmatter.bustitle ? mkd.renderInline(frontmatter.bustitle) : null
+  const subtitle = !!frontmatter.subtitle
+    ? mkd.renderInline(frontmatter.subtitle)
+    : null;
+  const bustitle = !!frontmatter.bustitle
+    ? mkd.renderInline(frontmatter.bustitle)
+    : null;
 
-  const crumbs = CreateBreadcrumbs({slug, frontmatter})
+  const crumbs = CreateBreadcrumbs({ slug, frontmatter });
 
   return (
     <>
       <HtmlMeta title={frontmatter.title} />
-      <Container maxWidth="sm">
-      <Breadcrumbs aria-label="breadcrumb">
-        {crumbs.map(crumb =>
-        <Link key={crumb.key} underline="hover" color="inherit" href={crumb.path}>
-          {crumb.value}
-        </Link>
-        )}
-      </Breadcrumbs>
-        <Typography component="h1">{frontmatter.title}</Typography>
-      {subtitle && 
-      <div dangerouslySetInnerHTML={{ __html: subtitle }}></div>}
-      <div dangerouslySetInnerHTML={{ __html: content }}></div>
-      {bustitle && 
-      <div dangerouslySetInnerHTML={{ __html: bustitle }}></div>}
-      </Container>
+      <div className={classes.root}>
+        <Typography gutterBottom>
+          <Breadcrumbs aria-label="breadcrumb" className={classes.crumbs}>
+            {crumbs.map((crumb) => (
+              <Link
+                key={crumb.key}
+                underline="hover"
+                color="inherit"
+                href={crumb.path}
+              >
+                {crumb.value}
+              </Link>
+            ))}
+          </Breadcrumbs>
+        </Typography>
+        <Typography component="h1" variant="h1" className={classes.title}>
+          {frontmatter.title}
+        </Typography>
+        {subtitle && <div dangerouslySetInnerHTML={{ __html: subtitle }}></div>}
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        {bustitle && <div dangerouslySetInnerHTML={{ __html: bustitle }}></div>}
+      </div>
     </>
   );
 }
