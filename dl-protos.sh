a4111369ce2c027fab7ce8175e3af7c0d9e6ed61
Aapeli <git@aapelivuorinen.com>
2022-01-16 01:13:21 +0200

    Tweak

--- dl-protos.sh
+++ dl-protos.sh
@@ -3,6 +3,7 @@ pushd /tmp/deps
 curl -L https://github.com/protocolbuffers/protobuf/releases/download/v3.19.3/protoc-3.19.3-linux-x86_64.zip -o protoc.zip
 unzip protoc.zip
 curl -L https://github.com/grpc/grpc-web/releases/download/1.3.0/protoc-gen-grpc-web-1.3.0-linux-x86_64 -o protoc-gen-grpc-web
+chmod +x protoc-gen-grpc-web
 popd
 
 cd ..
