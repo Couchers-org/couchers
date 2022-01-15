3e861aa7b65317e2c6030f2fe4a1c3571d6db303
Lucas levin <lucaslevin@gmail.com>
2022-01-15 22:29:25 +1100

    [wip] change globals

--- appConstants.ts
+++ appConstants.ts
@@ -25,6 +25,8 @@ export const PROFILE = "Profile";
 
 export const eventImagePlaceholderUrl = "/img/eventImagePlaceholder.svg";
 
+export const supportEmail = "support@couchers.org";
+
 export const userLocationMaxRadius = 2000;
 export const userLocationMinRadius = 50;
 export const userLocationDefault = new LngLat(-0.1, 51.5);
