7a33ce39b9c4982548d2f5ed610f16b97955fad0
Dieter Pankratz <dieter.pankratz@mcmakler.de>
2022-01-22 00:28:14 +0100

    replace constants with keys in home section

--- app/web/features/profile/view/Home.tsx
+++ app/web/features/profile/view/Home.tsx
@@ -2,37 +2,14 @@ import { Typography } from "@material-ui/core";
 import Divider from "components/Divider";
 import LabelAndText from "components/LabelAndText";
 import Markdown from "components/Markdown";
-import {
-  ABOUT_HOME,
-  ACCEPT_CAMPING,
-  ACCEPT_DRINKING,
-  ACCEPT_KIDS,
-  ACCEPT_PETS,
-  ACCEPT_SMOKING,
-  ADDITIONAL,
-  HAS_HOUSEMATES,
-  HOST_DRINKING,
-  HOST_KIDS,
-  HOST_PETS,
-  HOST_SMOKING,
-  HOSTING_PREFERENCES,
-  HOUSE_RULES,
-  LAST_MINUTE,
-  LOCAL_AREA,
-  MAX_GUESTS,
-  MY_HOME,
-  PARKING,
-  PARKING_DETAILS,
-  SLEEPING_ARRANGEMENT,
-  SPACE,
-  WHEELCHAIR,
-} from "features/constants";
 import booleanConversion, {
   parkingDetailsLabels,
   sleepingArrangementLabels,
   smokingLocationLabels,
   UNSURE,
 } from "features/profile/constants";
+import { useTranslation } from "i18n";
+import { GLOBAL } from "i18n/namespaces";
 import { User } from "proto/api_pb";
 import makeStyles from "utils/makeStyles";
 
@@ -59,61 +36,62 @@ interface HomeProps {
 
 export default function Home({ user }: HomeProps) {
   const classes = useStyles();
+  const { t } = useTranslation([GLOBAL]);
 
   return (
     <>
       <div className={classes.root}>
         <div className={classes.info}>
-          <Typography variant="h1">{HOSTING_PREFERENCES}</Typography>
+          <Typography variant="h1">{t("global:hosting_preferences")}</Typography>
           <LabelAndText
-            label={LAST_MINUTE}
+            label={t("global:last_minute")}
             text={booleanConversion(user.lastMinute?.value)}
           />
           <LabelAndText
-            label={WHEELCHAIR}
+            label={t("global:wheelchair")}
             text={booleanConversion(user.wheelchairAccessible?.value)}
           />
           <LabelAndText
-            label={ACCEPT_CAMPING}
+            label={t("global:accept_camping")}
             text={booleanConversion(user.campingOk?.value)}
           />
           <LabelAndText
-            label={MAX_GUESTS}
+            label={t("global:max_guests")}
             text={`${user.maxGuests?.value || UNSURE}`}
           />
           <LabelAndText
-            label={ACCEPT_KIDS}
+            label={t("global:accept_kids")}
             text={booleanConversion(user.acceptsKids?.value)}
           />
           <LabelAndText
-            label={ACCEPT_PETS}
+            label={t("global:accept_pets")}
             text={booleanConversion(user.acceptsPets?.value)}
           />
           <LabelAndText
-            label={ACCEPT_DRINKING}
+            label={t("global:accept_drinking")}
             text={booleanConversion(user.drinkingAllowed?.value)}
           />
           <LabelAndText
-            label={ACCEPT_SMOKING}
+            label={t("global:accept_smoking")}
             text={`${smokingLocationLabels[user.smokingAllowed]}`}
           />
         </div>
         <div className={classes.info}>
-          <Typography variant="h1">{MY_HOME}</Typography>
+          <Typography variant="h1">{t("global:my_home")}</Typography>
           <LabelAndText
-            label={SPACE}
+            label={t("global:space")}
             text={`${sleepingArrangementLabels[user.sleepingArrangement]}`}
           />
           <LabelAndText
-            label={PARKING}
+            label={t("global:parking")}
             text={booleanConversion(user.parking?.value)}
           />
           <LabelAndText
-            label={PARKING_DETAILS}
+            label={t("global:parking_details")}
             text={parkingDetailsLabels[user.parkingDetails]}
           />
           <LabelAndText
-            label={HAS_HOUSEMATES}
+            label={t("global:has_housemates")}
             text={`${booleanConversion(user.hasHousemates?.value)}${
               user.housemateDetails?.value
                 ? `, ${user.housemateDetails?.value}`
@@ -121,23 +99,23 @@ export default function Home({ user }: HomeProps) {
             }`}
           />
           <LabelAndText
-            label={HOST_KIDS}
+            label={t("global:host_kids")}
             text={`${booleanConversion(user.hasKids?.value)}${
               user.kidDetails?.value ? `, ${user.kidDetails?.value}` : ""
             }`}
           />
           <LabelAndText
-            label={HOST_PETS}
+            label={t("global:host_pets")}
             text={`${booleanConversion(user.hasPets?.value)}${
               user.petDetails?.value ? `, ${user.petDetails?.value}` : ""
             }`}
           />
           <LabelAndText
-            label={HOST_DRINKING}
+            label={t("global:host_drinking")}
             text={booleanConversion(user.drinksAtHome?.value)}
           />
           <LabelAndText
-            label={HOST_SMOKING}
+            label={t("global:host_smoking")}
             text={booleanConversion(user.smokesAtHome?.value)}
           />
         </div>
@@ -145,35 +123,35 @@ export default function Home({ user }: HomeProps) {
       <Divider className={classes.marginTop3} />
       {user.aboutPlace && (
         <>
-          <Typography variant="h1">{ABOUT_HOME}</Typography>
+          <Typography variant="h1">{t("global:about_home")}</Typography>
           <Markdown source={user.aboutPlace} />
           <Divider className={classes.marginTop3} />
         </>
       )}
       {user.area && (
         <>
-          <Typography variant="h1">{LOCAL_AREA}</Typography>
+          <Typography variant="h1">{t("global:local_area")}</Typography>
           <Markdown source={user.area?.value} />
           <Divider className={classes.marginTop3} />
         </>
       )}
       {user.sleepingDetails && (
         <>
-          <Typography variant="h1">{SLEEPING_ARRANGEMENT}</Typography>
+          <Typography variant="h1">{t("global:sleeping_arrangement")}</Typography>
           <Markdown source={user.sleepingDetails?.value} />
           <Divider className={classes.marginTop3} />
         </>
       )}
       {user.houseRules && (
         <>
-          <Typography variant="h1">{HOUSE_RULES}</Typography>
+          <Typography variant="h1">{t("global:house_rules")}</Typography>
           <Markdown source={user.houseRules?.value} />
           <Divider className={classes.marginTop3} />
         </>
       )}
       {user.otherHostInfo && (
         <>
-          <Typography variant="h1">{ADDITIONAL}</Typography>
+          <Typography variant="h1">{t("global:additional")}</Typography>
           <Markdown source={user.otherHostInfo?.value} />
         </>
       )}
