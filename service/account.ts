5d952b64e675f7929d68fd7e3b83dd8f817f217a
Aapeli <git@aapelivuorinen.com>
2022-02-09 22:25:45 -0500

    Implement frontend for SMS verification

--- service/account.ts
+++ service/account.ts
@@ -3,8 +3,10 @@ import { StringValue } from "google-protobuf/google/protobuf/wrappers_pb";
 import {
   ChangeEmailReq,
   ChangePasswordReq,
+  ChangePhoneReq,
   DeleteAccountReq,
   FillContributorFormReq,
+  VerifyPhoneReq,
 } from "proto/account_pb";
 import {
   CompletePasswordResetReq,
@@ -82,4 +84,21 @@ export function deleteAccount(confirm: boolean, reason?: string) {
     req.setReason(reason);
   }
   return client.account.deleteAccount(req);
+}
+export function changePhone(phone: string) {
+  const req = new ChangePhoneReq();
+  req.setPhone(phone);
+  return client.account.changePhone(req);
+}
+
+export function removePhone() {
+  const req = new ChangePhoneReq();
+  req.setPhone("");
+  return client.account.changePhone(req);
+}
+
+export function verifyPhone(code: string) {
+  const req = new VerifyPhoneReq();
+  req.setToken(code);
+  return client.account.verifyPhone(req);
 }
