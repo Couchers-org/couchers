673709f53c2b895a9873ed9466c60a2c4d4e1fff
Aapeli <git@aapelivuorinen.com>
2022-03-26 13:40:02 -0400

    Clean up

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -46,4 +46,3 @@ export { default as SettingsIcon } from "@material-ui/icons/SettingsOutlined";
 export { default as CommunityLeadersIcon } from "@material-ui/icons/SportsSoccerOutlined";
 export { default as MuteIcon } from "@material-ui/icons/VolumeOffOutlined";
 export { default as WorkIcon } from "@material-ui/icons/WorkOutlined";
-export { default as HourglassIcon } from "@material-ui/icons/HourglassEmpty";
