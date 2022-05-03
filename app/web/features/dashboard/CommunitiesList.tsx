da57fa7290bae654a2eb5b3dee45694f7d7e4d5a
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-05-03 10:03:49 +0200

    Replace manually using next Link and <a> with the component StyledLink

--- app/web/features/dashboard/CommunitiesList.tsx
+++ app/web/features/dashboard/CommunitiesList.tsx
@@ -2,11 +2,11 @@ import { Link as MuiLink, makeStyles, Typography } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 import Alert from "components/Alert";
 import Button from "components/Button";
+import StyledLink from "components/StyledLink";
 import { useListSubCommunities } from "features/communities/hooks";
 import useUserCommunities from "features/userQueries/useUserCommunities";
 import { useTranslation } from "i18n";
 import { DASHBOARD } from "i18n/namespaces";
-import NextLink from "next/link";
 import React from "react";
 import { routeToCommunity } from "routes";
 import hasAtLeastOnePage from "utils/hasAtLeastOnePage";
@@ -50,21 +50,20 @@ export default function CommunitiesList({ all = false }: { all?: boolean }) {
             {communities.data.pages
               .flatMap((page) => page.communitiesList)
               .map((community) => (
-                <NextLink
+                <StyledLink
                   href={routeToCommunity(community.communityId, community.slug)}
                   key={`community-link-${community.communityId}`}
+                  className={classes.communityLink}
                 >
-                  <a className={classes.communityLink}>
-                    <MuiLink variant="h2" component="span">
-                      {community.name}
-                    </MuiLink>
-                    <Typography variant="body1" color="textSecondary">
-                      {t("dashboard:member_count", {
-                        count: community.memberCount,
-                      })}
-                    </Typography>
-                  </a>
-                </NextLink>
+                  <MuiLink variant="h2" component="span">
+                    {community.name}
+                  </MuiLink>
+                  <Typography variant="body1" color="textSecondary">
+                    {t("dashboard:member_count", {
+                      count: community.memberCount,
+                    })}
+                  </Typography>
+                </StyledLink>
               ))}
             {communities.hasNextPage && (
               <Button
