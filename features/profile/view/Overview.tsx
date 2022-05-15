a9c8c27a5d2b53f7c02ed094a77de83279a5dae6
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-05-15 15:37:34 +0200

    Update profile page button styles

--- features/profile/view/Overview.tsx
+++ features/profile/view/Overview.tsx
@@ -42,12 +42,14 @@ function LoggedInUserActions({ tab }: { tab: UserTab }) {
   return (
     <>
       <Link href={routeToEditProfile(getEditTab(tab))} passHref>
-        <Button component="a" color="secondary">
+        <Button component="a" color="primary">
           {EDIT}
         </Button>
       </Link>
       <Link href={connectionsRoute} passHref>
-        <Button component="a">{t("profile:my_connections")}</Button>
+        <Button component="a" variant="outlined">
+          {t("profile:my_connections")}
+        </Button>
       </Link>
     </>
   );
