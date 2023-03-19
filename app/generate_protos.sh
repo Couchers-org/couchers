353492f3dfd88f2e7b201024aa68c240cc4bc1a4
Aapeli <git@aapelivuorinen.com>
2023-03-19 19:45:48 -0400

    Generate js and python to aws bucket

--- app/generate_protos.sh
+++ app/generate_protos.sh
@@ -2,6 +2,9 @@
 set -e
 
 # create the directories if they don't exist
+rm -rf proto/gen
+mkdir -p proto/gen/python
+mkdir -p proto/gen/js
 mkdir -p backend/src/proto/
 mkdir -p media/src/proto/
 mkdir -p web-frontend/proto/
@@ -15,7 +18,10 @@ find proto -name '*.proto' | protoc -I proto \
   --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) \
   --include_imports --include_source_info \
   \
-  --descriptor_set_out proto/descriptors.pb \
+  --descriptor_set_out proto/gen/descriptors.pb \
+  \
+  --python_out=proto/gen/python \
+  --grpc_python_out=proto/gen/python \
   \
   --python_out=backend/src/proto \
   --grpc_python_out=backend/src/proto \
@@ -26,14 +32,17 @@ find proto -name '*.proto' | protoc -I proto \
   --python_out=media/src/proto \
   --grpc_python_out=media/src/proto \
   \
+  --js_out="import_style=commonjs,binary:proto/gen/js" \
+  --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:proto/gen/js" \
+  \
   --js_out="import_style=commonjs,binary:web-frontend/proto" \
   --grpc-web_out="import_style=commonjs+dts,mode=grpcweb:web-frontend/proto" \
   \
   $(xargs)
 
 # protoc only allows passing --descriptor_set_out once...
-cp proto/descriptors.pb proxy/descriptors.pb
-cp proto/descriptors.pb backend/src/proto/descriptors.pb
+cp proto/gen/descriptors.pb proxy/descriptors.pb
+cp proto/gen/descriptors.pb backend/src/proto/descriptors.pb
 
 # create internal backend protos
 (cd backend && find proto -name '*.proto' | protoc -I proto \
@@ -45,5 +54,8 @@ cp proto/descriptors.pb backend/src/proto/descriptors.pb
 sed -i -E 's/^import.*_pb2/from . &/' backend/src/proto/*.py client/src/couchers/proto/*.py media/src/proto/*.py
 sed -i -E 's/^from google.api/from .google.api/' backend/src/proto/*.py client/src/couchers/proto/*.py media/src/proto/*.py
 sed -i -E 's/^from google.api/from ./' backend/src/proto/google/api/*.py client/src/couchers/proto/google/api/*.py media/src/proto/google/api/*.py
+
+(cd proto/gen && tar czf python.tgz python)
+(cd proto/gen && tar czf js.tgz js)
 
 echo "OK"
