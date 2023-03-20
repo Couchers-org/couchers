ffd4ac43a2aade8f6c9acd88fc8fb8eaad1af908
Aapeli <git@aapelivuorinen.com>
2023-03-20 19:47:25 -0400

    Update protos

--- update_protos.sh
+++ update_protos.sh
@@ -1,2 +1,2 @@
-protos_sha=9591722e619d2c95fdc1be2b0a4ee0ec74b91d405060150df888ac91dfef4cf6
+protos_sha=04e29d7d5a9f174a0a690fcd002b67aeada72058d21681615ba95333a1b3136e
 wget -O typescript_protos.tar.gz https://protos--by-sha.preview.couchershq.org/$protos_sha/ts.tar.gz
