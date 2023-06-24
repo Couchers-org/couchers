6d22ac7fd349ac59d6b60db34acc4eecf7078164
Petr <psfinaki@users.noreply.github.com>
2023-06-25 01:10:06 +0200

    Updating copyright (#332)
    Update constants.ts

--- components/Footer/constants.ts
+++ components/Footer/constants.ts
@@ -1,3 +1,3 @@
-export const COPYRIGHT = "© 2022 Couchers, Inc.";
+export const COPYRIGHT = "© 2023 Couchers, Inc.";
 export const NON_PROFIT =
   "Couchers.org is a project of Couchers, Inc. a U.S. 501(c)(3) non-profit, tax-exempt organization.";
