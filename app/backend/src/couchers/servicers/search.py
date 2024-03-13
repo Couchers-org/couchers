da1f43fd737b914a187789fdf05dc4c8b9bac755
Krishnag09 <krishnag0902@gmail.com>
2024-03-12 23:06:51 -0400

    using profile_completed as other filters

--- app/backend/src/couchers/servicers/search.py
+++ app/backend/src/couchers/servicers/search.py
@@ -379,8 +379,8 @@ class Search(search_pb2_grpc.SearchServicer):
                             User.additional_information.ilike(f"%{request.query.value}%"),
                         )
                     )
-            if request.profile_completed:
-                statement = statement.where(User.has_completed_profile == True)
+            # if request.profile_completed:
+            #     statement = statement.where(User.has_completed_profile == True)
 
             if request.HasField("last_active"):
                 raw_dt = to_aware_datetime(request.last_active)
@@ -407,7 +407,8 @@ class Search(search_pb2_grpc.SearchServicer):
                 statement = statement.where(
                     User.parking_details.in_([parkingdetails2sql[det] for det in request.parking_details_filter])
                 )
-
+            if request.HasField("profile_completed"):
+                statement = statement.where(User.profile_completed == request.profile_completed.value)
             if request.HasField("guests"):
                 statement = statement.where(User.max_guests >= request.guests.value)
             if request.HasField("last_minute"):
