4215f42bb76cf1429c78a9ddd6288760baf7fc28
Aapeli <git@aapelivuorinen.com>
2024-04-17 01:04:24 -0400

    Implement basic travel map

--- features/profile/view/About.tsx
+++ features/profile/view/About.tsx
@@ -1,9 +1,10 @@
-import { Typography } from "@material-ui/core";
+import { Typography, useTheme } from "@material-ui/core";
 import Divider from "components/Divider";
 import Markdown from "components/Markdown";
 import { useTranslation } from "i18n";
 import { GLOBAL, PROFILE } from "i18n/namespaces";
 import { User } from "proto/api_pb";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import makeStyles from "utils/makeStyles";
 
 import { useRegions } from "../hooks/useRegions";
@@ -24,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
 export default function About({ user }: AboutProps) {
   const { t } = useTranslation([GLOBAL, PROFILE]);
   const classes = useStyles();
+  const theme = useTheme();
   const { regions } = useRegions();
   return (
     <div className={classes.root}>
@@ -77,6 +79,27 @@ export default function About({ user }: AboutProps) {
           ? user.regionsLivedList.map((country) => regions[country]).join(`, `)
           : t("profile:regions_empty_state")}
       </Typography>
+      <Divider className={classes.marginTop3} />
+      <Typography variant="h1">{t("profile:heading.map_section")}</Typography>
+      <ComposableMap projection="geoEqualEarth">
+        <Geographies geography={"/regions.json"}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              let color = theme.palette.grey[200];
+              if (regions) {
+                if (user.regionsLivedList.includes(geo.id)) {
+                  color = theme.palette.primary.main;
+                } else if (user.regionsVisitedList.includes(geo.id)) {
+                  color = theme.palette.secondary.main;
+                }
+              }
+              return (
+                <Geography key={geo.rsmKey} geography={geo} fill={color} />
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
     </div>
   );
 }
