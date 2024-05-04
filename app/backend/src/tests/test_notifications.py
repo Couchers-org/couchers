84c02972f140a7355f07fd8686182c8e8acdcd74
Aapeli <git@aapelivuorinen.com>
2024-05-04 14:22:11 -0400

    Add test for unsub

--- app/backend/src/tests/test_notifications.py
+++ app/backend/src/tests/test_notifications.py
@@ -1,13 +1,26 @@
+from unittest.mock import patch
+from urllib.parse import parse_qs, urlparse
+
 import grpc
 import pytest
+from google.protobuf import empty_pb2
 
 from couchers import errors, urls
+from couchers.crypto import b64decode
+from couchers.jobs.enqueue import queue_job
 from couchers.jobs.worker import process_job
 from couchers.models import Notification, NotificationDelivery, NotificationDeliveryType, NotificationTopicAction, User
 from couchers.notifications.notify import notify
 from couchers.sql import couchers_select as select
-from proto import notifications_pb2
-from tests.test_fixtures import db, generate_user, notifications_session, session_scope, testconfig  # noqa
+from proto import auth_pb2, notifications_pb2
+from tests.test_fixtures import (  # noqa
+    auth_api_session,
+    db,
+    generate_user,
+    notifications_session,
+    session_scope,
+    testconfig,
+)
 
 
 @pytest.fixture(autouse=True)
@@ -143,3 +156,92 @@ def test_SetNotificationSettings_preferences_not_editable(db):
             )
         assert e.value.code() == grpc.StatusCode.FAILED_PRECONDITION
         assert e.value.details() == errors.CANNOT_EDIT_THAT_NOTIFICATION_PREFERENCE
+
+
+def test_unsubscribe(db):
+    # this is the ugliest test i've written
+
+    user, token = generate_user()
+
+    topic_action = NotificationTopicAction.friend_request__accept
+
+    # first enable email notifs
+    with notifications_session(token) as notifications:
+        notifications.SetNotificationSettings(
+            notifications_pb2.SetNotificationSettingsReq(
+                enable_new_notifications=True,
+                preferences=[
+                    notifications_pb2.SingleNotificationPreference(
+                        topic=topic_action.topic,
+                        action=topic_action.action,
+                        delivery_method=method,
+                        enabled=enabled,
+                    )
+                    for method, enabled in [("email", True), ("digest", False), ("push", False)]
+                ],
+            )
+        )
+
+    notify(
+        user_id=user.id,
+        topic=topic_action.topic,
+        key="1",
+        action=topic_action.action,
+        avatar_key=None,
+        icon="person",
+        title=f"**Dummy** accepted your friend request",
+        link=urls.user_link(username="dummy"),
+    )
+    queue_job("handle_email_notifications", empty_pb2.Empty())
+
+    with patch("couchers.email.queue_email") as mock:
+        while process_job():
+            pass
+
+    assert mock.call_count == 1
+    (_, _, recipient, _, plain, html), _ = mock.call_args
+    assert recipient == user.email
+    # very ugly
+    start = "To unsubscribe from these types of notifications click on this link: <"
+    ix_s = plain.find(start) + len(start)
+    ix_e = plain.find(">", ix_s)
+    # should look something like
+    # http://localhost:3000/unsubscribe?payload=CAEiGAoOZnJpZW5kX3JlcXVlc3QSBmFjY2VwdA==&sig=BQdk024NTATm8zlR0krSXTBhP5U9TlFv7VhJeIHZtUg=
+    topic_action_unsub_url = plain[ix_s:ix_e]
+    url_parts = urlparse(topic_action_unsub_url)
+    params = parse_qs(url_parts.query)
+
+    with auth_api_session() as (auth_api, metadata_interceptor):
+        res = auth_api.Unsubscribe(
+            auth_pb2.UnsubscribeReq(
+                payload=b64decode(params["payload"][0]),
+                sig=b64decode(params["sig"][0]),
+            )
+        )
+
+    with notifications_session(token) as notifications:
+        res = notifications.GetNotificationSettings(notifications_pb2.GetNotificationSettingsReq())
+    assert res.new_notifications_enabled
+    for group in res.groups:
+        for topic in group.topics:
+            for item in topic.items:
+                if topic == topic_action.topic and item == topic_action.action:
+                    assert not item.email
+
+    notify(
+        user_id=user.id,
+        topic=topic_action.topic,
+        key="1",
+        action=topic_action.action,
+        avatar_key=None,
+        icon="person",
+        title=f"**Dummy** accepted your friend request",
+        link=urls.user_link(username="dummy"),
+    )
+    queue_job("handle_email_notifications", empty_pb2.Empty())
+
+    with patch("couchers.email.queue_email") as mock:
+        while process_job():
+            pass
+
+    assert mock.call_count == 0
