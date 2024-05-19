20d6ae5f9af1bd0f888783bbd29dda3fa251fcd4
Bakeiro <davidbaqueiro@outlook.com>
2024-05-19 14:25:43 +0200

    story

---
+++ app/web/features/auth/password/SetNewPassword.stories.tsx
@@ -0,0 +1,34 @@
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { Meta, Story } from "@storybook/react";
+import { mockedService } from "stories/serviceMocks";
+import React from "react";
+
+import SetNewPassword from "./SetNewPassword";
+
+export default {
+  component: SetNewPassword,
+  title: "Me/Auth/SetNewPasswordPage",
+} as Meta;
+
+interface SetNewPasswordArgs {
+  shouldSucceed?: boolean;
+}
+
+const Template: Story<SetNewPasswordArgs> = ({ shouldSucceed = true } = {}) => {
+  setMocks({ shouldSucceed });
+  return <SetNewPassword />;
+};
+
+export const Success = Template.bind({});
+
+export const Failed = Template.bind({});
+Failed.args = {
+  shouldSucceed: false,
+};
+
+function setMocks({ shouldSucceed }: Required<SetNewPasswordArgs>) {
+  mockedService.account.resetPassword = () =>
+    shouldSucceed
+      ? Promise.resolve(new Empty())
+      : Promise.reject(new Error("API error"));
+}
