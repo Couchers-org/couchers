3ae97b81aca4636fc0fe7e05e3cad6a8da8fd9bd
Bakeiro <davidbaqueiro@outlook.com>
2024-05-21 16:13:30 +0200

    small change

--- app/web/features/auth/password/index.ts
+++ app/web/features/auth/password/index.ts
@@ -1,4 +1,3 @@
 export { default as ChangePassword } from "./ChangePassword";
-export { default as CompleteResetPassword } from "./CompleteResetPassword";
 export { default as ResetPassword } from "./ResetPassword";
 export { default as SetNewPassword } from "./SetNewPassword";
