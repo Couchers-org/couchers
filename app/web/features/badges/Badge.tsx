88cf3452598f04b3569a3670871da3b5fd70ed2e
Tristan Schrader <t0rdos@pm.me>
2024-05-30 11:33:56 -0700

    feat: ToolChipLink + Badge components w/ routing
    I thought having a clickable chip with a tooltip is a common scenario we
    might want to make use of later, so I extracted this functionality
    originally built out in the features/profile/view/Badges file into a
    ToolChipLink component.
    
    The Badge component is then just a wrapper around that where you pass in
    a Badge resource. This uses a URL scheme for badges at /badges/{id}

---
+++ app/web/features/badges/Badge.tsx
@@ -0,0 +1,19 @@
+import ToolChipLink, { type ToolChipLinkProps } from "components/ToolChipLink";
+import { Badge as BadgeType } from "proto/resources_pb";
+import { routeToBadge } from "routes";
+
+export interface BadgeProps {
+  badge: BadgeType.AsObject;
+  toolChipLinkProps: ToolChipLinkProps;
+}
+
+export default function Badge({ badge, toolChipLinkProps = {} }: BadgeProps) {
+  const toolChipLink = {
+      label: badge.name,
+      description: badge.description,
+      href: routeToBadge(badge.id),
+      color: badge.color,
+      ...toolChipLinkProps,
+  }
+  return <ToolChipLink {...toolChipLink} />;
+}
