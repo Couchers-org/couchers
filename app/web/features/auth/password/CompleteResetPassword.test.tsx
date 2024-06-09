acc019982183ee7ae13671be530afd5e05c1807a
David Baqueiro Santerbás <davidbaqueiro@outlook.com>
2024-06-09 04:17:29 +0200

    feat: 4079 password reset (#4327)
    * implement new page
    
    * tokens and minor changes
    
    * added test
    
    * format
    
    * story
    
    * missing changes
    
    * small change
    
    * small change
    
    * Small changes
    
    * changes
    
    * Update .env.localdev
    
    * Update .env.development
    
    * tokens change
    
    * new changes
    
    * url change

--- app/web/features/auth/password/CompleteResetPassword.test.tsx
+++ app/web/features/auth/password/CompleteResetPassword.test.tsx
@@ -1,68 +1,154 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { Empty } from "google-protobuf/google/protobuf/empty_pb";
-import mockRouter from "next-router-mock";
-import { loginRoute } from "routes";
-import { service } from "service";
-import wrapper from "test/hookWrapper";
 import { MockedService, t } from "test/utils";
+import { AuthRes } from "proto/auth_pb";
+import { useRouter } from "next/router";
+import wrapper from "test/hookWrapper";
+import { service } from "service";
 
-import CompleteResetPassword from "./CompleteResetPassword";
+import CompletePasswordReset from "./CompleteResetPassword";
 
-const completePasswordResetMock = service.account
-  .completePasswordReset as MockedService<
-  typeof service.account.completePasswordReset
+const CompletePasswordResetMock = service.account
+  .CompletePasswordResetV2 as MockedService<
+  typeof service.account.CompletePasswordResetV2
 >;
 
-function renderPage() {
-  mockRouter.setCurrentUrl("?token=P4w0rdR3seTtok3n");
-  render(<CompleteResetPassword />, { wrapper });
-}
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
 
-describe("CompleteResetPassword", () => {
-  it("shows the loading state on initial load", async () => {
-    completePasswordResetMock.mockImplementation(
-      () => new Promise(() => void 0)
+const mockUseRouter = useRouter as jest.Mock;
+
+describe("CompletePasswordReset page", () => {
+  beforeEach(() => {
+    CompletePasswordResetMock.mockResolvedValue(new AuthRes());
+
+    mockUseRouter.mockReturnValue({
+      query: { token: "aaa123" },
+    });
+  });
+
+  it("shows the set new password form correctly", async () => {
+    render(<CompletePasswordReset />, { wrapper });
+
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+        name: t("auth:change_password_form.title"),
+      })
+    ).toBeVisible();
+
+    expect(
+      screen.getByText(t("auth:change_password_form.subtitle"))
+    ).toBeVisible();
+
+    expect(
+      screen.getByRole("button", { name: t("global:submit") })
+    ).toBeVisible();
+  });
+
+  it("shows a warning when empty token", () => {
+    mockUseRouter.mockReturnValue({
+      query: { token: "" },
+    });
+
+    render(<CompletePasswordReset />, { wrapper });
+
+    expect(
+      screen.queryByText(t("auth:change_password_form.token_error"))
+    ).toBeInTheDocument();
+  });
+
+  it("don't show a warning when valid token", () => {
+    render(<CompletePasswordReset />, { wrapper });
+
+    expect(
+      screen.queryByText(t("auth:change_password_form.token_error"))
+    ).not.toBeInTheDocument();
+  });
+
+  it("does not submit if empty form", async () => {
+    render(<CompletePasswordReset />, { wrapper });
+
+    userEvent.click(screen.getByRole("button", { name: t("global:submit") }));
+
+    await waitFor(() => {
+      expect(CompletePasswordResetMock).not.toHaveBeenCalled();
+    });
+  });
+
+  it("does not submit if password don't match", async () => {
+    render(<CompletePasswordReset />, { wrapper });
+
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.new_password")),
+      "1111"
     );
-    renderPage();
 
-    expect(await screen.findByRole("progressbar")).toBeVisible();
-  });
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.confirm_password")),
+      "2222"
+    );
 
-  describe("when the reset password completes successfully", () => {
-    beforeEach(async () => {
-      completePasswordResetMock.mockResolvedValue(new Empty());
-      renderPage();
-    });
+    userEvent.click(screen.getByRole("button", { name: t("global:submit") }));
 
-    it("shows the success alert", async () => {
-      const successAlert = await screen.findByRole("alert");
-      expect(successAlert).toBeVisible();
-      expect(successAlert).toHaveTextContent(t("auth:reset_password_success"));
-      expect(completePasswordResetMock).toHaveBeenCalledTimes(1);
-      expect(completePasswordResetMock).toHaveBeenLastCalledWith(
-        "P4w0rdR3seTtok3n"
-      );
-    });
-
-    it("shows a link that takes you to the login page when clicked", async () => {
-      userEvent.click(
-        await screen.findByRole("link", { name: t("auth:login_prompt") })
-      );
-
-      expect(mockRouter.pathname).toBe(loginRoute);
+    await waitFor(() => {
+      expect(CompletePasswordResetMock).not.toHaveBeenCalled();
     });
   });
 
-  it("shows an error alert if the reset password process failed to complete", async () => {
+  it("submits the reset password request successfully", async () => {
+    mockUseRouter.mockReturnValue({
+      query: { token: "aaa123" },
+    });
+
+    render(<CompletePasswordReset />, { wrapper });
+
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.new_password")),
+      "1111"
+    );
+
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.confirm_password")),
+      "1111"
+    );
+
+    userEvent.click(screen.getByRole("button", { name: t("global:submit") }));
+
+    expect(
+      await screen.findByText(
+        t("auth:change_password_form.reset_password_success")
+      )
+    ).toBeVisible();
+
+    expect(CompletePasswordResetMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("shows an error alert if the reset password request failed", async () => {
     jest.spyOn(console, "error").mockReturnValue(undefined);
-    completePasswordResetMock.mockRejectedValue(new Error("Invalid token"));
-    renderPage();
+    CompletePasswordResetMock.mockRejectedValue(new Error("GRPC error"));
 
-    const errorAlert = await screen.findByRole("alert");
-    expect(errorAlert).toBeVisible();
-    expect(errorAlert).toHaveTextContent(
-      t("auth:reset_password_error", { message: "Invalid token" })
+    mockUseRouter.mockReturnValue({
+      query: { token: "aaa123" },
+    });
+
+    render(<CompletePasswordReset />, { wrapper });
+
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.new_password")),
+      "1111"
     );
+
+    userEvent.type(
+      screen.getByLabelText(t("auth:change_password_form.confirm_password")),
+      "1111"
+    );
+
+    userEvent.click(screen.getByRole("button", { name: t("global:submit") }));
+
+    expect(
+      screen.queryByText(t("auth:change_password_form.reset_password_success"))
+    ).not.toBeInTheDocument();
   });
 });
