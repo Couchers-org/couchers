9fefb84d277bf730c2ae401f8624a65fa3f9820b
Aapeli <git@aapelivuorinen.com>
2024-07-15 18:35:59 -0400

    Add auth check

--- app/web/features/auth/useAuthStore.ts
+++ app/web/features/auth/useAuthStore.ts
@@ -4,7 +4,7 @@ import { GLOBAL } from "i18n/namespaces";
 import Sentry from "platform/sentry";
 import { clearStorage, usePersistedState } from "platform/usePersistedState";
 import { AuthRes, SignupFlowRes } from "proto/auth_pb";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useQueryClient } from "react-query";
 import { service } from "service";
 import isGrpcError from "service/utils/isGrpcError";
@@ -131,11 +131,49 @@ export default function useAuthStore() {
         }
         setLoading(false);
       },
+      async checkAuthStatus() {
+        setLoading(true);
+        try {
+          console.log("Checking auth status");
+          const res = await service.user.getAuthState();
+          if (res.loggedIn) {
+            console.log("Turns out we are logged in")!;
+            const auth = res.authRes!;
+            setUserId(auth.userId);
+            Sentry.setUser({ id: auth.userId.toString() });
+
+            //this must come after setting the userId, because calling setQueryData
+            //will also cause that query to be background fetched, and it needs
+            //userId to be set.
+            setJailed(auth.jailed);
+            setAuthenticated(true);
+          } else {
+            setAuthenticated(false);
+            setUserId(null);
+            Sentry.setUser({ id: undefined });
+          }
+        } catch (e) {
+          Sentry.captureException(e, {
+            tags: {
+              component: "auth/useAuthStore",
+              action: "checkAuthStatus",
+            },
+          });
+          setError(isGrpcError(e) ? e.message : fatalErrorMessage.current);
+        }
+        setLoading(false);
+      },
     }),
     //note: there should be no dependenices on the state or t, or
     //some useEffects will break. Eg. the token login in Login.tsx
     [setAuthenticated, setJailed, setUserId, setFlowState, queryClient]
   );
+
+  useEffect(() => {
+    if (!authenticated && typeof window !== "undefined") {
+      authActions.checkAuthStatus();
+    }
+  }, [authenticated, authActions]);
 
   return {
     authActions,
