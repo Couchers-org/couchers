16b31954afe5d67a2a20fdf04cdb842dbe1c5a15
Nicole Abramowski <nabramow@Nicoles-MacBook-Air.local>
2024-08-22 14:49:51 -0700

    Finish design

--- app/web/features/profile/edit/notificationSettings/EditNotificationSettingsPage.tsx
+++ app/web/features/profile/edit/notificationSettings/EditNotificationSettingsPage.tsx
@@ -1,7 +1,16 @@
 import { Typography } from "@material-ui/core";
 import makeStyles from "utils/makeStyles";
 import { List } from "@material-ui/core";
-import { SinglePersonIcon, CouchIconFilled } from "components/Icons";
+import {
+  SinglePersonIcon,
+  CouchFilledIcon,
+  ChatBubbleIcon,
+  EventIcon,
+  PenIcon,
+  ReminderIcon,
+  AccountSettingsIcon,
+  AccountSecurityIcon,
+} from "components/Icons";
 import NotificationSettingsListItem from "./NotificationSettingsListItem";
 
 const useStyles = makeStyles((theme) => ({
@@ -59,7 +68,71 @@ export default function EditNotificationSettingsPage() {
           ]}
           title="Host Requests"
           type="host"
-          icon={<CouchIconFilled fontSize="large" color="action" />}
+          icon={<CouchFilledIcon fontSize="large" color="action" />}
+        />
+        <NotificationSettingsListItem
+          actions={[
+            "Notifications when someone sends you a message",
+            "Notifications when you miss messages in a chat",
+          ]}
+          title="Messages"
+          type="message"
+          icon={<ChatBubbleIcon fontSize="large" color="action" />}
+        />
+        <NotificationSettingsListItem
+          actions={[
+            "Notifications when an event approved by the moderators is created in your community",
+            "Notifications when a user creates any event in your community (not checked by an admin)",
+            "Notifications when an event you are attending is updated",
+            "Notifications when an event you are attending is canceled",
+            "Notifications when an event you are attending is deleted",
+            "Notifications when someone invites you to co-organize an event",
+          ]}
+          title="Events"
+          type="event"
+          icon={<EventIcon fontSize="large" color="action" />}
+        />
+        <NotificationSettingsListItem
+          actions={[
+            "Notifications when you receive a reference from someone who hosted you or you hosted",
+            "Notifications when you received a reference from a friend",
+          ]}
+          title="References"
+          type="reference"
+          icon={<PenIcon fontSize="large" color="action" />}
+        />
+        <NotificationSettingsListItem
+          actions={[
+            "Notifications to remind you to write a reference for someone you hosted or who hosted you",
+            "Notifications to remind you to complete your profile after signing up",
+          ]}
+          title="Reminders"
+          icon={<ReminderIcon fontSize="large" color="action" />}
+          type="reminder"
+        />
+        <NotificationSettingsListItem
+          actions={[
+            "Notifications when a badge is added or removed from your account",
+            "Motifications when your donation is received",
+          ]}
+          title="Account Settings"
+          icon={<AccountSettingsIcon fontSize="large" color="action" />}
+          type="account-settings"
+        />
+                <NotificationSettingsListItem
+          actions={[
+            "Notifications when your password is changed",
+            "Motifications when your password reset is initiated or completed",
+            "Notifications when your email is changed or your new email is verified",
+            "Notifications when you initiate account deletion, your account is deleted or you account is recoved (undeleted)",
+            "Notifications when an api key is created for your account",
+            "Notifications when your phone number is changed or verified",
+            "Notifications when your birthdate is changed",
+            "Notifications when the gender displayed on your profile is changed",
+          ]}
+          title="Account Security"
+          icon={<AccountSecurityIcon fontSize="large" color="action" />}
+          type="account-security"
         />
       </List>
     </div>
