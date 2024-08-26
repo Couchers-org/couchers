6f318b58d8cee6674d1dee31cb31c6308d19ba14
Aapeli <git@aapelivuorinen.com>
2024-08-26 11:59:07 -0400

    Send metrics to our VictoriaMetrics instead of Grafana Cloud

--- app/prometheus/entrypoint.sh
+++ app/prometheus/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/bin/sed -i 's/{GRAFANA_CLOUD_TOKEN}/'$GRAFANA_CLOUD_TOKEN'/' /etc/prometheus/prometheus.yml
+/bin/sed -i 's/{VICTORIAMETRICS_API_KEY}/'$VICTORIAMETRICS_API_KEY'/' /etc/prometheus/prometheus.yml
 /bin/sed -i 's/{PROMETHEUS_ENVIRONMENT}/'$PROMETHEUS_ENVIRONMENT'/' /etc/prometheus/prometheus.yml
 /bin/prometheus --config.file=/etc/prometheus/prometheus.yml \
              --storage.tsdb.path=/prometheus \
