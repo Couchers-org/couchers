0bf8f824ce4088670781371ce6a8d2ac70db3824
Aapeli <git@aapelivuorinen.com>
2024-08-28 13:26:52 -0400

    Re-copy service

---
+++ app/native/service/donations.ts
@@ -0,0 +1,13 @@
+import { InitiateDonationReq } from "proto/donations_pb";
+
+import client from "./client";
+
+export async function initiateDonation(amount: number, recurring: boolean) {
+  const req = new InitiateDonationReq();
+
+  req.setAmount(amount);
+  req.setRecurring(recurring);
+
+  const res = await client.donations.initiateDonation(req);
+  return res.getStripeCheckoutSessionId();
+}
