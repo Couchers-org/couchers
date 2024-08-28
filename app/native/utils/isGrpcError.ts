18cc3d626ad30cfa02ecf447c201b1ef22c85b52
Aapeli <git@aapelivuorinen.com>
2024-08-28 13:26:26 -0400

    Clean up native stuff

--- app/native/utils/isGrpcError.ts
+++
@@ -1,8 +0,0 @@
-import { RpcError } from "grpc-web";
-
-export default function isGrpcError(e: unknown): e is RpcError {
-  if (typeof e === "object" && e) {
-    return "message" in e && "code" in e;
-  }
-  return false;
-}
