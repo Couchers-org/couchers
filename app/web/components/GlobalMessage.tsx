e20d154a2b1b5308b4de4a1ad6be3e1dee1d4d02
Aapeli <git@aapelivuorinen.com>
2024-09-03 14:32:30 -0400

    Only fetch GM once every 5 min and make it dismissable

--- app/web/components/GlobalMessage.tsx
+++ app/web/components/GlobalMessage.tsx
@@ -1,13 +1,32 @@
 import { Alert as MuiAlert } from "@material-ui/lab/";
-import React, { useEffect, useState } from "react";
+import { usePersistedState } from "platform/usePersistedState";
+import React, { useEffect } from "react";
 
 interface GlobalMessageData {
+  // the unix timestamp (as a string with milliseconds) when the message was issued
+  epoch: string;
   severity: "success" | "info" | "warning" | "error";
   message: string;
 }
 
+const TIME_BETWEEN_CHECKS_MS = 300_000; // 5 min
+
 export function GlobalMessage() {
-  const [data, setData] = useState<GlobalMessageData | null>(null);
+  // data from the global message file
+  const [data, setData] = usePersistedState<GlobalMessageData | null>(
+    "globalmessage.data",
+    null
+  );
+  // last time we queried it
+  const [lastCheck, setLastCheck] = usePersistedState<number | null>(
+    "globalmessage.lastcheck",
+    null
+  );
+  // the epoch value of the last message we dismissed
+  const [dismissedEpoch, setDismissedEpoch] = usePersistedState<string | null>(
+    "globalmessage.dismissed",
+    null
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,16 +38,26 @@ export function GlobalMessage() {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         setData(await response.json());
+        setLastCheck(new Date().getTime());
       } catch (error) {
         console.error("Error fetching global message:", error);
       }
     };
+    if (
+      !lastCheck ||
+      new Date().getTime() - lastCheck > TIME_BETWEEN_CHECKS_MS
+    ) {
+      fetchData();
+    }
+  }, [setData, setLastCheck, lastCheck]);
 
-    fetchData();
-  }, []);
+  const dismiss = () => {
+    if (!data) return;
+    setDismissedEpoch(data!.epoch);
+  };
 
-  return data && data.message ? (
-    <MuiAlert severity={data.severity}>
+  return data && data.epoch && data.epoch != dismissedEpoch ? (
+    <MuiAlert severity={data.severity} onClose={dismiss}>
       <span dangerouslySetInnerHTML={{ __html: data.message }} />
     </MuiAlert>
   ) : null;
