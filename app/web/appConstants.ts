373c073e08d718ffb9db2e2a1fb437f7cacb54b8
Bakeiro <davidbaqueiro@outlook.com>
2024-09-05 09:52:18 +0200

    not used consts

--- app/web/appConstants.ts
+++ app/web/appConstants.ts
@@ -6,8 +6,6 @@ export const supportEmail = "support@couchers.org";
 
 export const userLocationMaxRadius = 2000;
 export const userLocationMinRadius = 50;
-export const userLocationDefault = new LngLat(-0.1, 51.5);
-export const userLocationDefaultRadius = 500;
 
 export const pingInterval = 10000;
 
