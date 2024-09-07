2ac21c90ac4b3bc766ba9466c18e6f76e815d47e
Aapeli <git@aapelivuorinen.com>
2024-09-07 18:05:41 -0400

    Implement notifications for more community features

---
+++ app/backend/src/couchers/migrations/versions/33944292a259_add_thread_notifications.py
@@ -0,0 +1,26 @@
+"""Add thread notifications
+
+Revision ID: 33944292a259
+Revises: 2fc64cf68321
+Create Date: 2024-09-07 22:41:29.750442
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "33944292a259"
+down_revision = "2fc64cf68321"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("ALTER TYPE notificationtopicaction ADD VALUE 'event__comment'")
+    op.execute("ALTER TYPE notificationtopicaction ADD VALUE 'discussion__create'")
+    op.execute("ALTER TYPE notificationtopicaction ADD VALUE 'discussion__comment'")
+    op.execute("ALTER TYPE notificationtopicaction ADD VALUE 'thread__reply'")
+
+
+def downgrade():
+    raise Exception("Can't downgrade")
