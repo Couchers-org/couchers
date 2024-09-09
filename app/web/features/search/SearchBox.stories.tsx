29021d91fdbcf704db4cbee82831e313fa6e5e7b
Nicole Abramowski <nabramow@Nicoles-MacBook-Air.local>
2024-09-08 22:28:17 -0700

    Fix stories type errors

--- app/web/features/search/SearchBox.stories.tsx
+++ app/web/features/search/SearchBox.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from "@storybook/react";
 import SearchBox from "features/search/SearchBox";
+import { LngLat } from "maplibre-gl";
 
 export default {
   component: SearchBox,
@@ -10,9 +11,14 @@ const Template: Story<any> = () => {
   return (
     <>
       <SearchBox
-        searchType={""}
+        searchType={"keyword"}
         setSearchType={() => {}}
-        locationResult={[]}
+        locationResult={{
+          name: "",
+          simplifiedName: "",
+          location: new LngLat(0, 0),
+          bbox: [0, 0, 0, 0],
+        }}
         setLocationResult={() => {}}
         setQueryName={() => {}}
         queryName={undefined}
