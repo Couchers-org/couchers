8bf7552beafb92812a305b93a1bd3b9b3d63ef0b
Nicole Abramowski <nabramow@gmail.com>
2024-09-15 23:03:02 -0700

    Remove duplicate functions

--- app/web/features/auth/notifications/notificationUtils.ts
+++ app/web/features/auth/notifications/notificationUtils.ts
@@ -1,35 +1,3 @@
-import { t } from "i18next";
-
-const getBrowserInstructions = (givenUserAgent: string) => {
-  const normalizedUserAgent = givenUserAgent.toLowerCase();
-  const userAgent = normalizedUserAgent.includes("chrome")
-    ? "chrome"
-    : normalizedUserAgent.includes("firefox")
-    ? "firefox"
-    : normalizedUserAgent.includes("safari")
-    ? "safari"
-    : "generic";
-
-  switch (userAgent) {
-    case "chrome":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.chrome"
-      );
-    case "firefox":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.firefox"
-      );
-    case "safari":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.safari"
-      );
-    default:
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.generic"
-      );
-  }
-};
-
 const getCurrentSubscription = async () => {
   let registration = await navigator.serviceWorker.getRegistration();
 
@@ -45,34 +13,4 @@ const getCurrentSubscription = async () => {
   return registration?.pushManager.getSubscription();
 };
 
-const getOSInstructions = (givenUserAgent: string) => {
-  const normalizedUserAgent = givenUserAgent.toLowerCase();
-  const userAgent = normalizedUserAgent.includes("mac")
-    ? "mac"
-    : normalizedUserAgent.includes("windows")
-    ? "windows"
-    : normalizedUserAgent.includes("linux")
-    ? "linux"
-    : "generic";
-
-  switch (userAgent) {
-    case "mac":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.macos"
-      );
-    case "windows":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.windows"
-      );
-    case "linux":
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.linux.gnome"
-      );
-    default:
-      return t(
-        "notification_settings.push_notifications.permission_denied.instructions.generic"
-      );
-  }
-};
-
-export { getBrowserInstructions, getCurrentSubscription, getOSInstructions };
+export { getCurrentSubscription };
