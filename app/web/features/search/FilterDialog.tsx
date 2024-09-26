df5f3fd51e4fd8cb15786a630d447b80f7d858d9
Bakeiro <davidbaqueiro@outlook.com>
2024-09-26 16:24:09 +0200

    ..

--- app/web/features/search/FilterDialog.tsx
+++ app/web/features/search/FilterDialog.tsx
@@ -135,7 +135,6 @@ export default function FilterDialog({
   const { control, register, errors } = useForm<FilterModalFormData>({
     mode: "onBlur",
   });
-  
 
   const MenuProps = {
     PaperProps: {
