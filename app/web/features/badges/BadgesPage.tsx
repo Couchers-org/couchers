157879f65c4471ac4d13c4ed461c634f62ac10e0
Aapeli <git@aapelivuorinen.com>
2024-11-25 18:33:13 -0500

    Use new mui way for styling

--- app/web/features/badges/BadgesPage.tsx
+++ app/web/features/badges/BadgesPage.tsx
@@ -1,35 +1,39 @@
-import { Divider, List, ListItem, Typography } from "@mui/material";
 import CenteredSpinner from "components/CenteredSpinner/CenteredSpinner";
+import {
+  Divider,
+  DividerProps,
+  List,
+  ListItem,
+  styled,
+  Typography,
+} from "@mui/material";
 import HtmlMeta from "components/HtmlMeta";
 import PageTitle from "components/PageTitle";
 import Badge from "features/badges/Badge";
 import { useBadges } from "features/badges/hooks";
 import { useTranslation } from "i18n";
 import { GLOBAL, PROFILE } from "i18n/namespaces";
-import makeStyles from "utils/makeStyles";
 
-const useStyles = makeStyles((theme) => ({
-  divider: {
-    margin: theme.spacing(2),
-  },
-  flex: {
-    display: "flex",
-    gap: theme.spacing(2),
-    alignItems: "start",
-  },
-  content: {
-    padding: theme.spacing(2),
-    alignSelf: "stretch",
-    width: "100%",
-  },
-  description: {
-    paddingTop: theme.spacing(0.5),
-  },
-  center: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+const StyledDivider = styled(Divider)<DividerProps>(({ theme }) => ({
+  margin: theme.spacing(2),
+}));
+
+const FlexDiv = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: theme.spacing(2),
+  alignItems: "start",
+}));
+
+const ContentDiv = styled("div")(({ theme }) => ({
+  padding: theme.spacing(2),
+  alignSelf: "stretch",
+  width: "100%",
+}));
+
+const CenteredDiv = styled(ContentDiv)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 export interface BadgesPageProps {
@@ -39,15 +43,16 @@ export interface BadgesPageProps {
 export default function BadgesPage({ badgeId = undefined }: BadgesPageProps) {
   const { t } = useTranslation([GLOBAL, PROFILE]);
   const { badges, isLoading: isBadgesLoading } = useBadges();
-  const classes = useStyles();
 
   return (
     <>
       <HtmlMeta title={t("global:nav.badges")} />
       <PageTitle>{t("profile:badges.title")}</PageTitle>
-      <Typography variant="body1">{t("profile:badges.subtitle")}</Typography>
-      <Divider className={classes.divider} />
-      <div className={classes.flex}>
+      <Typography variant="subtitle1">
+        {t("profile:badges.subtitle")}
+      </Typography>
+      <StyledDivider />
+      <FlexDiv>
         <List>
           {badges &&
             Object.values(badges).map((badge) => (
@@ -56,33 +61,33 @@ export default function BadgesPage({ badgeId = undefined }: BadgesPageProps) {
               </ListItem>
             ))}
         </List>
-        <Divider orientation="vertical" className={classes.divider} flexItem />
+        <StyledDivider orientation="vertical" flexItem />
         {badgeId ? (
-          <div className={classes.content}>
+          <ContentDiv>
             {isBadgesLoading ? (
               <CenteredSpinner />
             ) : badges && badgeId in badges ? (
               <>
-                <div className={classes.flex}>
+                <FlexDiv>
                   <Badge badge={badges[badgeId]} />
-                  <Typography variant="body1" className={classes.description}>
+                  <Typography variant="body1">
                     {badges[badgeId].description}
                   </Typography>
-                </div>
-                <Divider className={classes.divider} />
+                </FlexDiv>
+                <StyledDivider />
               </>
             ) : (
               <>Badge not found</>
             )}
-          </div>
+          </ContentDiv>
         ) : (
-          <div className={`${classes.content} ${classes.center}`}>
+          <CenteredDiv>
             <Typography variant="subtitle1">
               {t("profile:badges.click_on_left")}
             </Typography>
-          </div>
+          </CenteredDiv>
         )}
-      </div>
+      </FlexDiv>
     </>
   );
 }
