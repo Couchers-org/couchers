d0e1d82cb5bcb0d102f18ecee90a22c1314b3cbb
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2024-12-13 13:38:51 -0800

    Added report center route to routes.ts

--- app/web/routes.ts
+++ app/web/routes.ts
@@ -181,3 +181,6 @@ export const howToInviteCommunityUrl =
   "https://help.couchers.org/hc/couchersorg-help-center/articles/1720304409-how-does-the-invite-the-community-feature-work";
 export const howToMakeGreatProfileUrl =
   "https://help.couchers.org/hc/couchersorg-help-center/articles/1725919197-how-do-i-create-a-great-profile";
+
+export const reportRoute =
+  "https://help.couchers.org/hc/couchersorg-help-center/articles/1715125657-how-do-i-report-someone-or-something-that-violates-the-community-guidelines-or-terms-of-use-to_u";
