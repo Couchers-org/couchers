f74f4c441fbde589ad54eab9ffacdbb120ad3bcf
Aapeli <git@aapelivuorinen.com>
2024-12-13 18:39:08 -0500

    Tweak SV

--- app/web/features/auth/verification/StrongVerification.tsx
+++ app/web/features/auth/verification/StrongVerification.tsx
@@ -8,9 +8,10 @@ import {
   DialogContentText,
   DialogTitle,
 } from "components/Dialog";
+import StyledLink from "components/StyledLink";
 import { RpcError } from "grpc-web";
 import { Trans, useTranslation } from "i18n";
-import { AUTH } from "i18n/namespaces";
+import { AUTH, GLOBAL } from "i18n/namespaces";
 import { useRouter } from "next/router";
 import {
   GetAccountInfoRes,
@@ -18,15 +19,11 @@ import {
 } from "proto/account_pb";
 import { useState } from "react";
 import { useMutation } from "react-query";
+import { tosRoute } from "routes";
 import { service } from "service";
 
-type ChangePhoneProps = {
-  accountInfo: GetAccountInfoRes.AsObject;
-  className?: string;
-};
-
 function StartStrongVerificationButton() {
-  const { t } = useTranslation(AUTH);
+  const { t } = useTranslation([GLOBAL, AUTH]);
 
   const [open, setOpen] = useState(false);
 
@@ -49,7 +46,7 @@ function StartStrongVerificationButton() {
     <>
       <Dialog aria-labelledby="strong-verification-start-dialog" open={open}>
         <DialogTitle id="strong-verification-start-dialog">
-          {t("strong_verification.title")}
+          {t("auth:strong_verification.title")}
         </DialogTitle>
         <DialogContent>
           {error && (
@@ -58,29 +55,54 @@ function StartStrongVerificationButton() {
             </DialogContentText>
           )}
           <DialogContentText>
-            {t("strong_verification.information_text1")}
+            {t("auth:strong_verification.information_text1")}
           </DialogContentText>
           <DialogContentText>
-            {t("strong_verification.information_text2")}
+            {t("auth:strong_verification.information_text2")}
+          </DialogContentText>
+          <DialogContentText variant="body2">
+            <Trans i18nKey="auth:strong_verification.information_text3">
+              You can read more about how we and Iris ID process and store your
+              data in our{" "}
+              <StyledLink href={tosRoute} target="_blank">
+                Terms of Service
+              </StyledLink>
+              . As per the{" "}
+              <StyledLink
+                href="https://passportreader.app/privacy"
+                target="_blank"
+              >
+                Privacy policy
+              </StyledLink>
+              , they will delete your personal information within 24 hours.
+            </Trans>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={() => startStrongVerification()} loading={isLoading}>
-            {t("strong_verification.begin_button")}
+            {t("auth:strong_verification.begin_button")}
+          </Button>
+          <Button variant="outlined" onClick={() => setOpen(false)}>
+            {t("global:cancel")}
           </Button>
         </DialogActions>
       </Dialog>
       <Button loading={isLoading} onClick={() => setOpen(true)}>
-        {t("strong_verification.begin_button")}
+        {t("auth:strong_verification.start_button")}
       </Button>
     </>
   );
 }
 
+type StrongVerificationProps = {
+  accountInfo: GetAccountInfoRes.AsObject;
+  className?: string;
+};
+
 export default function StrongVerification({
   className,
   accountInfo,
-}: ChangePhoneProps) {
+}: StrongVerificationProps) {
   const { t } = useTranslation(AUTH);
 
   return (
