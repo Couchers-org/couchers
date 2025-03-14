ea555d71a9944c8da5cf8280ff8d7197a5689003
Nicole Abramowski <nabramow@gmail.com>
2025-03-13 21:43:45 -0700

    Remove smoking_allowed

--- app/backend/src/couchers/servicers/search.py
+++ app/backend/src/couchers/servicers/search.py
@@ -489,8 +489,6 @@ class Search(search_pb2_grpc.SearchServicer):
             statement = statement.where(User.has_housemates == request.has_housemates.value)
         if request.HasField("wheelchair_accessible"):
             statement = statement.where(User.wheelchair_accessible == request.wheelchair_accessible.value)
-        if request.HasField("smoking_allowed"):
-            statement = statement.where(User.smoking_allowed == request.smoking_allowed.value)
         if request.HasField("smokes_at_home"):
             statement = statement.where(User.smokes_at_home == request.smokes_at_home.value)
         if request.HasField("drinking_allowed"):
