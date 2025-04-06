2be1e62a85c91015f1e7f6b6e0e4d7d8e901f351
Aapeli <git@aapelivuorinen.com>
2025-04-06 18:23:21 -0400

    Revert to anons for migration test

--- app/backend/src/couchers/migrations/versions/b16903ba2c18_add_response_rate_view.py
+++ app/backend/src/couchers/migrations/versions/b16903ba2c18_add_response_rate_view.py
@@ -20,33 +20,33 @@ def upgrade():
         """
         CREATE MATERIALIZED VIEW user_response_rates AS
         SELECT users.id AS user_id,
-            COALESCE(grouped.requests, 0) AS requests,
-            COALESCE(grouped.response_rate, 0.) AS response_rate,
-            grouped.avg_response_time AS avg_response_time,
-            grouped.response_time_33p AS response_time_33p,
-            grouped.response_time_66p AS response_time_66p
+            COALESCE(anon_1.requests, 0) AS requests,
+            COALESCE(anon_1.response_rate, 0.) AS response_rate,
+            anon_1.avg_response_time AS avg_response_time,
+            anon_1.response_time_33p AS response_time_33p,
+            anon_1.response_time_66p AS response_time_66p
             FROM users
             LEFT OUTER JOIN (
-                SELECT responses.user_id AS user_id,
+                SELECT anon_2.user_id AS user_id,
                 count(*) AS requests,
-                count(responses.response_time) / CAST(count(*) AS NUMERIC) AS response_rate,
-                avg(responses.response_time) AS avg_response_time,
+                count(anon_2.response_time) / CAST(count(*) AS NUMERIC) AS response_rate,
+                avg(anon_2.response_time) AS avg_response_time,
                 PERCENTILE_DISC(0.33) WITHIN GROUP (
-                    ORDER BY COALESCE(responses.response_time, interval '1000 days')
+                    ORDER BY COALESCE(anon_2.response_time, make_interval(secs => (86400000.0)::double precision)))
                 ) AS response_time_33p,
                 PERCENTILE_DISC(0.66) WITHIN GROUP (
-                    ORDER BY COALESCE(responses.response_time, interval '1000 days')
+                    ORDER BY COALESCE(anon_2.response_time, make_interval(secs => (86400000.0)::double precision)))
                 ) AS response_time_66p
                 FROM (
                     SELECT host_requests.host_user_id AS user_id,
-                    hr_responded.time - hr_created.time AS response_time
+                    anon_3.time - anon_4.time AS response_time
                     FROM host_requests
                     JOIN (
                         SELECT messages.conversation_id AS conversation_id,
                         messages.time AS time
                         FROM messages
                         WHERE messages.message_type = 'chat_created'
-                    ) AS hr_created ON hr_created.conversation_id = host_requests.id
+                    ) AS anon_4 ON anon_4.conversation_id = host_requests.id
                     LEFT OUTER JOIN (
                         SELECT messages.conversation_id AS conversation_id,
                         messages.author_id AS author_id,
@@ -54,17 +54,17 @@ def upgrade():
                         FROM messages
                         GROUP BY messages.conversation_id,
                         messages.author_id
-                    ) AS hr_responded ON hr_responded.conversation_id = host_requests.id
-                    AND hr_responded.author_id = host_requests.host_user_id
+                    ) AS anon_3 ON anon_3.conversation_id = host_requests.id
+                    AND anon_3.author_id = host_requests.host_user_id
                     UNION ALL
                     SELECT activeness_probes.user_id AS user_id,
                     CASE
                         WHEN (activeness_probes.response != 'expired') THEN activeness_probes.responded - activeness_probes.probe_initiated
                     END AS response_time
                     FROM activeness_probes
-                ) AS responses
-                GROUP BY responses.user_id
-            ) AS grouped ON grouped.user_id = users.id;
+                ) AS anon_2
+                GROUP BY anon_2.user_id
+            ) AS anon_1 ON anon_1.user_id = users.id;
 
         CREATE UNIQUE INDEX uq_user_response_rates_id ON user_response_rates(user_id);
     """
