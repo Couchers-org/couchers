2fd33c3f5ea4cd79c15adfe17846dbdef0786196
ColleenLillian <185909905+ColleenLillian@users.noreply.github.com>
2025-04-17 18:57:37 +0200

    Add pages for communities route

---
+++ app/web/pages/communities/index.tsx
@@ -0,0 +1,6 @@
+import CommunitiesPage from "features/communities/CommunitiesPage/CommunitiesPage";
+import React from "react";
+
+export default function CommunitiesRoute() {
+  return <CommunitiesPage />;
+}
