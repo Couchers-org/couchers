5e1a3a0d5ee5b41515a2da6aba19acbc0f9a4d7b
Pablo <pcolturi@proton.me>
2025-04-17 09:54:50 +0200

    revert order by node.id

--- app/backend/src/couchers/servicers/communities.py
+++ app/backend/src/couchers/servicers/communities.py
@@ -414,7 +414,7 @@ class Communities(communities_pb2_grpc.CommunitiesServicer):
                 .where(ClusterSubscription.user_id == user_id)
                 .where(Cluster.is_official_cluster)
                 .where(Node.id >= next_node_id)
-                .order_by(Cluster.id)
+                .order_by(Node.id)
                 .limit(page_size + 1)
             )
             .scalars()
