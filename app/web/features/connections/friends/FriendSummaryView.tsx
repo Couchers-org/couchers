8b5ac451b57f91c0e29cce3878d5943451836691
Nicole Abramowski <nabramow@gmail.com>
2025-05-19 18:33:48 -0700

    WIP

--- app/web/features/connections/friends/FriendSummaryView.tsx
+++ app/web/features/connections/friends/FriendSummaryView.tsx
@@ -1,27 +1,36 @@
 import { MoreHoriz, PersonRemove } from "@mui/icons-material";
 import {
   Alert,
+  DialogActions,
+  DialogContent,
   IconButton,
   Menu,
   MenuItem,
   styled,
   Typography,
 } from "@mui/material";
-import ConfirmationDialogWrapper from "components/ConfirmationDialogWrapper";
+import { Dialog, DialogTitle } from "components/Dialog";
 import UserSummary from "components/UserSummary";
-import { friendIdsKey, liteUserKey, liteUsersKey } from "features/queryKeys";
-import { RpcError } from "grpc-web";
+import Button from "components/Button";
+import { friendIdsKey } from "features/queryKeys";
 import { useTranslation } from "i18n";
-import { CONNECTIONS } from "i18n/namespaces";
-import { GetLiteUsersRes, LiteUser } from "proto/api_pb";
+import { CONNECTIONS, GLOBAL } from "i18n/namespaces";
+import { LiteUser } from "proto/api_pb";
 import { useState } from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { removeFriend } from "service/api";
 import { theme } from "theme";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { SetMutationError } from ".";
 
 interface FriendSummaryViewProps {
   children?: React.ReactNode;
   friend?: LiteUser.AsObject;
+}
+
+interface RemoveFriendProps {
+  friendId: number;
+  setMutationError: SetMutationError;
 }
 
 export const FRIEND_ITEM_TEST_ID = "friend-item";
@@ -33,69 +42,51 @@ const StyledFriendItem = styled("div")(({ theme }) => ({
 const MenuWrapper = styled("div")(({ theme }) => ({
   display: "flex",
   justifyContent: "flex-end",
+  flexDirection: "column",
 }));
 
 function FriendSummaryView({ children, friend }: FriendSummaryViewProps) {
-  const { t } = useTranslation([CONNECTIONS]);
+  const { t } = useTranslation([GLOBAL, CONNECTIONS]);
+  const queryClient = useQueryClient();
 
   const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLButtonElement | null>(
     null,
   );
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
   const isMenuOpen = Boolean(menuAnchorEl);
 
-  const queryClient = useQueryClient();
+  const { mutate: removeFriendMutate, isLoading: isRemoving } = useMutation<
+    Empty,
+    Error,
+    { friendId: number },
+    { previousFriendIds?: number[] }
+  >(({ friendId }) => removeFriend(friendId), {
+    onMutate: async ({ friendId }) => {
+      setError(null);
+      await queryClient.cancelQueries(friendIdsKey);
 
-  //@TODO(NA): This causes an unknown backend error and doesn't remove friend
-  const { error, mutate: removeFriendMutation } = useMutation<void, RpcError>(
-    async () => {
-      if (!friend) {
-        throw new Error("Friend not found");
+      const previousFriendIds =
+        queryClient.getQueryData<number[]>(friendIdsKey);
+      const newFriendIds = previousFriendIds?.filter((id) => id !== friendId);
+
+      if (newFriendIds) {
+        queryClient.setQueryData<number[]>(friendIdsKey, newFriendIds);
       }
-      await removeFriend(friend?.userId);
-    },
-    {
-      onSuccess() {
-        // 1. Remove the friend ID from the cached list
-        queryClient.setQueryData<number[]>(
-          friendIdsKey,
-          (prevData) => prevData?.filter((id) => id !== friend?.userId) ?? [],
-        );
 
-        // 2. Remove `liteUsers` cache entries that include the deleted ID
-        queryClient
-          .getQueryCache()
-          .findAll({ queryKey: ["liteUsers"] }) // base key must match `liteUsersKey()`
-          .forEach((query) => {
-            const existingKey = query.queryKey;
-            const userIds = (existingKey as any[])[1] as number[]; // liteUsersKey([ids]) = ['liteUsers', [1,2,3]]
-            if (
-              friend?.userId !== undefined &&
-              !userIds?.includes(friend.userId)
-            )
-              return;
-
-            // Remove the user from the list of responses
-            queryClient.setQueryData<GetLiteUsersRes.AsObject>(
-              existingKey,
-              (oldData) => {
-                const defaultData: GetLiteUsersRes.AsObject = {
-                  responsesList: [],
-                };
-                const data = oldData ?? defaultData;
-                return {
-                  ...data,
-                  responsesList: data.responsesList.filter(
-                    (response) => response.user?.userId !== friend?.userId,
-                  ),
-                };
-              },
-            );
-          });
-        queryClient.invalidateQueries([friendIdsKey, "liteUsers"]);
-      },
+      return { previousFriendIds };
     },
-  );
+    onError: (err, _, context) => {
+      setError(err);
+      if (context?.previousFriendIds) {
+        queryClient.setQueryData(friendIdsKey, context.previousFriendIds);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(friendIdsKey);
+    },
+  });
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>): void => {
     setMenuAnchorEl(event.currentTarget);
@@ -105,8 +96,19 @@ function FriendSummaryView({ children, friend }: FriendSummaryViewProps) {
     setMenuAnchorEl(null);
   };
 
-  const handleRemoveFriend = async () => {
-    removeFriendMutation();
+  const handleRemoveFriend = () => {
+    if (friend?.userId !== undefined) {
+      removeFriendMutate({ friendId: friend.userId });
+      handleDialogClose();
+    }
+  };
+
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
   };
 
   const menu = (
@@ -116,94 +118,109 @@ function FriendSummaryView({ children, friend }: FriendSummaryViewProps) {
           {error.message}
         </Alert>
       )}
-      <ConfirmationDialogWrapper
-        title={t("connections:remove_friend_confirmation_dialog.title")}
-        message={t("connections:remove_friend_confirmation_dialog.message", {
-          name: friend?.name,
-        })}
-        onConfirm={handleRemoveFriend}
-        confirmButtonLabel={t(
-          "connections:remove_friend_confirmation_dialog.confirm",
-        )}
-      >
-        {(setIsOpen) => (
-          <>
-            <IconButton
-              aria-controls={
-                isMenuOpen ? "friend-item--more-options" : undefined
-              }
-              aria-haspopup="true"
-              aria-expanded={isMenuOpen ? "true" : undefined}
-              id="friend-item--more-options"
-              data-testid="friend-item--more-options"
-              onClick={handleMenuOpen}
-              sx={{ width: 32, height: 32 }}
-            >
-              <MoreHoriz fontSize="small" />
-            </IconButton>
-            <Menu
-              anchorEl={menuAnchorEl}
-              id="friend-item--more-options"
-              data-testid="friend-item--more-options"
-              open={isMenuOpen}
-              onClose={handleMenuClose}
-              onClick={handleMenuClose}
-              slotProps={{
-                paper: {
-                  elevation: 0,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    "&::before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                    },
-                  },
+      <>
+        <IconButton
+          aria-controls={isMenuOpen ? "friend-item--more-options" : undefined}
+          aria-haspopup="true"
+          aria-expanded={isMenuOpen ? "true" : undefined}
+          id="friend-item--more-options"
+          data-testid="friend-item--more-options"
+          onClick={handleMenuOpen}
+          sx={{ width: 32, height: 32 }}
+        >
+          <MoreHoriz fontSize="small" />
+        </IconButton>
+        <Menu
+          anchorEl={menuAnchorEl}
+          id="friend-item--more-options"
+          data-testid="friend-item--more-options"
+          open={isMenuOpen}
+          onClose={handleMenuClose}
+          onClick={handleMenuClose}
+          slotProps={{
+            paper: {
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
                 },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                "&::before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: "right", vertical: "top" }}
+          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        >
+          <MenuItem onClick={handleDialogOpen}>
+            <PersonRemove fontSize="small" />
+            <Typography
+              variant="body2"
+              sx={{ marginLeft: theme.spacing(1), fontWeight: 500 }}
             >
-              <MenuItem onClick={() => setIsOpen(true)}>
-                <PersonRemove fontSize="small" />
-                <Typography
-                  variant="body2"
-                  sx={{ marginLeft: theme.spacing(1), fontWeight: 500 }}
-                >
-                  {t("connections:remove_friend")}
-                </Typography>
-              </MenuItem>
-            </Menu>
-          </>
-        )}
-      </ConfirmationDialogWrapper>
+              {t("connections:remove_friend")}
+            </Typography>
+          </MenuItem>
+        </Menu>
+      </>
     </MenuWrapper>
   );
 
   return friend ? (
-    <StyledFriendItem data-testid={FRIEND_ITEM_TEST_ID}>
-      <UserSummary
-        headlineComponent="h3"
-        user={friend}
-        menu={menu}
-      ></UserSummary>
-      {children}
-    </StyledFriendItem>
+    <>
+      <StyledFriendItem data-testid={FRIEND_ITEM_TEST_ID}>
+        <UserSummary
+          headlineComponent="h3"
+          user={friend}
+          menu={menu}
+        ></UserSummary>
+        {children}
+      </StyledFriendItem>
+      <Dialog
+        aria-labelledby="friend-item--confirmation-dialog"
+        open={isDialogOpen}
+        onClose={handleDialogClose}
+      >
+        <DialogTitle id="friend-item--confirmation-dialog">
+          {t("connections:remove_friend_confirmation_dialog.title")}
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="body2">
+            {t("connections:remove_friend_confirmation_dialog.message", {
+              name: friend?.name,
+            })}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={handleDialogClose}>
+            {t("global:cancel")}
+          </Button>
+          <Button
+            variant="contained"
+            loading={isRemoving}
+            onClick={handleRemoveFriend}
+          >
+            {t("connections:remove_friend_confirmation_dialog.confirm")}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   ) : null;
 }
 
