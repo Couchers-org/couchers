334ae1becbbabb93073fb26a8f254b356cb3a7a5
Aapeli <git@aapelivuorinen.com>
2025-05-21 12:25:07 -0400

    Lint

--- app/web/features/auth/jail/LocationSection.tsx
+++ app/web/features/auth/jail/LocationSection.tsx
@@ -64,7 +64,9 @@ export default function LocationSection({
         {t("auth:jail.location_section.title")}
       </Typography>
       <div className={className}>
-        <Alert severity="warning">{t("auth:jail.location_section.explanation")}</Alert>
+        <Alert severity="warning">
+          {t("auth:jail.location_section.explanation")}
+        </Alert>
         {error && <Alert severity="error">{error}</Alert>}
         <Controller
           name="location"
