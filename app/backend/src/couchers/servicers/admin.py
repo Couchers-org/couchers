d29970dbcfaf19f90691cb28296916e8ba8719bc
Aapeli <git@aapelivuorinen.com>
2025-06-01 13:42:50 -0400

    Implement RecoverDeletedUser

--- app/backend/src/couchers/servicers/admin.py
+++ app/backend/src/couchers/servicers/admin.py
@@ -302,6 +302,13 @@ class Admin(admin_pb2_grpc.AdminServicer):
         user.is_deleted = True
         return _user_to_details(session, user)
 
+    def RecoverDeletedUser(self, request, context, session):
+        user = session.execute(select(User).where_username_or_email_or_id(request.user)).scalar_one_or_none()
+        if not user:
+            context.abort(grpc.StatusCode.NOT_FOUND, errors.USER_NOT_FOUND)
+        user.is_deleted = False
+        return _user_to_details(session, user)
+
     def CreateApiKey(self, request, context, session):
         user = session.execute(select(User).where_username_or_email_or_id(request.user)).scalar_one_or_none()
         if not user:
