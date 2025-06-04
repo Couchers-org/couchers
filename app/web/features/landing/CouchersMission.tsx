acb5b588c400fc8a704dfd61be48b925d12c6e7d
Pablo <pcolturi@proton.me>
2025-06-04 13:08:57 +0200

    logic for description w multiple lines

--- app/web/features/landing/CouchersMission.tsx
+++ app/web/features/landing/CouchersMission.tsx
@@ -4,9 +4,9 @@ import { StyledButton } from "features/auth/useAuthStyles";
 import { useTranslation } from "i18n";
 import { GLOBAL, LANDING } from "i18n/namespaces";
 import Link from "next/link";
-import { useState } from "react";
+import { Fragment,useState } from "react";
 import { useInView } from "react-intersection-observer";
-import { donationsRoute, roadmapRoute, volunteerRoute } from "routes";
+import { donationsRoute, missionRoute, roadmapRoute, volunteerRoute } from "routes";
 import { theme } from "theme";
 
 const StyledSpacer = styled("div")(({ theme }) => ({
@@ -19,19 +19,40 @@ const StyledButtonContainer = styled("div")({
   gap: "24px",
 });
 
+const TranslationKeys: Record<string, string[]> = {
+  non_profit_structure: [
+    "non_profit_structure_description_line1",
+    "non_profit_structure_description_line2",
+    "empty_line",
+    "non_profit_structure_description_line3",
+    "non_profit_structure_description_line4",
+    "empty_line",
+    "non_profit_structure_description_line5",
+  ],
+  community_first: [
+    "community_first_description",
+  ],
+  member_accountability: [
+    "member_accountability_description",
+  ],
+  improved_review_system: [
+    "improved_review_system_description",
+  ],
+}
+
 const CouchersMission = () => {
   const { t } = useTranslation([LANDING, GLOBAL]);
   const { ref, inView } = useInView({ triggerOnce: true });
-
   const [selectedItem, setSelectedItem] = useState("non_profit_structure");
 
-  const renderGridItem = (itemName: string, itemTitle: string) => {
+  const missionBubble = (itemName: string, itemTitle: string) => {
     return (
       <Grid
         item
         xs={3.7}
         md={2}
         display="flex"
+        alignItems="center"
         onClick={() => {setSelectedItem(itemName);}}
         sx={{
           backgroundColor: selectedItem === itemName ? theme.palette.primary.main : theme.palette.primary.light,
@@ -60,11 +81,45 @@ const CouchersMission = () => {
         }}
       >
         <Box display="flex" flexDirection="column" width="100%">
-          <Typography variant="h6" gutterBottom>
+          <Typography variant="h6" align="center" gutterBottom>
             {t(itemTitle)}
           </Typography>
         </Box>
       </Grid>
+    )
+  }
+  
+  const missionDescription = (selectedItem: string) => {
+    return (
+      <Box 
+        display="flex" flexDirection="column" width="100%">
+        <Typography align="justify" variant="body2">
+          {(TranslationKeys[selectedItem] || []).map((element, index) => {
+            let content
+            switch (element) {
+              case "empty_line":
+                content = <br />
+                break
+              case "non_profit_structure_description_line5":
+                content = (
+                  <Fragment>
+                    {t("non_profit_structure_description_line5")}
+                    <StyledLink href={missionRoute}>
+                      {t("read_here")}
+                    </StyledLink>
+                  </Fragment>
+                )
+                break
+              default:
+                content = t(element);
+            }
+            return <Fragment key={index}>
+                {content}
+                {(index < TranslationKeys[selectedItem].length - 1) && <br />}
+              </Fragment>
+          })}
+        </Typography>
+      </Box>
     )
   }
 
@@ -88,12 +143,12 @@ const CouchersMission = () => {
               flexWrap: { xs: "wrap", md: "nowrap" }
             }}
           >
-            {renderGridItem("non_profit_structure", "non_profit_structure_title")}
-            {renderGridItem("community_first", "community_first_title")}
-            {renderGridItem("member_accountability", "member_accountability_title")}
-            {renderGridItem("improved_review_system", "improved_review_system_title")}
-            {renderGridItem("better_host_matching", "better_host_matching_title")}
-            {renderGridItem("built_it_right", "built_it_right_title")}
+            {missionBubble("non_profit_structure", "non_profit_structure_title")}
+            {missionBubble("community_first", "community_first_title")}
+            {missionBubble("member_accountability", "member_accountability_title")}
+            {missionBubble("improved_review_system", "improved_review_system_title")}
+            {missionBubble("better_host_matching", "better_host_matching_title")}
+            {missionBubble("built_it_right", "built_it_right_title")}
           </Grid>
           <Grid
             container
@@ -113,11 +168,7 @@ const CouchersMission = () => {
                 minWidth: 0,
               }}
             >
-              <Box display="flex" flexDirection="column" width="100%">
-                <Typography variant="body2">
-                  {t(`${selectedItem}_description`)}
-                </Typography>
-              </Box>
+              {missionDescription(selectedItem)}
             </Grid>
           </Grid>
 
