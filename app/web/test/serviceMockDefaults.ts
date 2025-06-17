9957e6251065d049d1d2af2f8538f9753937afdf
Yannic Spreen-Ledebur <yannic.sl@gmx.de>
2025-06-17 23:37:34 +0200

    Fix frontend tests

--- app/web/test/serviceMockDefaults.ts
+++ app/web/test/serviceMockDefaults.ts
@@ -87,6 +87,7 @@ export async function listGroupChats() {
         title: "groupchattitle",
         // created?: google_protobuf_timestamp_pb.Timestamp.AsObject,
         unseenMessageCount: 0,
+        canMessage: true,
       },
     ],
     noMore: true,
@@ -108,6 +109,7 @@ export async function listHostRequests() {
       status: HostRequestStatus.HOST_REQUEST_STATUS_PENDING,
       toDate: "2020/12/06",
       hostUserId: 2,
+      canMessage: true,
     },
   ];
 }
