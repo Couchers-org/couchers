24532dd9390d18ad3e2f5e1136ce048673264d14
Aapeli <git@aapelivuorinen.com>
2025-06-22 16:15:24 -0400

    Fix typo

--- app/backend/src/couchers/migrations/versions/3937eb39a4ca_add_host_request_archive_status.py
+++ app/backend/src/couchers/migrations/versions/3937eb39a4ca_add_host_request_archive_status.py
@@ -18,10 +18,10 @@ depends_on = None
 
 def upgrade():
     op.add_column(
-        "host_requests", sa.Column("is_host_archived", sa.Boolean(), server_default=sa.text("0"), nullable=False)
+        "host_requests", sa.Column("is_host_archived", sa.Boolean(), server_default=sa.text("false"), nullable=False)
     )
     op.add_column(
-        "host_requests", sa.Column("is_surfer_archived", sa.Boolean(), server_default=sa.text("0"), nullable=False)
+        "host_requests", sa.Column("is_surfer_archived", sa.Boolean(), server_default=sa.text("false"), nullable=False)
     )
 
 
