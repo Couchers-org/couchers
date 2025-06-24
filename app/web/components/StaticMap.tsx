41cccf74fb57fc8910614673902ea9621f932d93
Nicole Abramowski <nabramow@gmail.com>
2025-06-24 13:49:58 -0700

    Change pins source and allow zoom

--- app/web/components/StaticMap.tsx
+++ app/web/components/StaticMap.tsx
@@ -7,7 +7,6 @@ import {
   SOURCE_CLUSTERED_USERS_ID,
   unclusteredPointLayer,
 } from "features/search/utils/mapLayers";
-import { RequestParameters } from "maplibre-gl";
 import React from "react";
 import { Layer, Map as MaplibreMap, Source } from "react-map-gl/maplibre";
 
@@ -20,26 +19,13 @@ const MapWrapper = styled("div")(({ theme }) => ({
 }));
 
 const StaticMap = () => {
-  /*
-    Allows sending cookies (counted as sensitive "credentials") on cross-origin requests when we grab GeoJSON/other data from the API.
-    Those APIs will return an error if the session cookie is not set as these APIs are secure and not public.
-    */
-  const transformRequest = (url: string): RequestParameters => {
-    if (url.startsWith(API_BASE_URL)) {
-      return {
-        credentials: "include",
-        url,
-      };
-    }
-    return { url };
-  };
-
   return (
     <MapWrapper>
       <MaplibreMap
         id="map"
         initialViewState={{ zoom: 0.75 }}
         minZoom={0}
+        maxZoom={7}
         style={{
           height: "100%",
           width: "100%",
@@ -47,16 +33,12 @@ const StaticMap = () => {
         mapStyle="https://cdn.couchers.org/maps/couchers-basemap-style-v1.json"
         interactiveLayerIds={clusterLayer.id ? [clusterLayer.id] : []}
         hash={true}
-        transformRequest={transformRequest}
-        scrollZoom={false}
-        doubleClickZoom={false}
-        touchZoomRotate={false}
       >
         <Source
           id={SOURCE_CLUSTERED_USERS_ID}
           cluster={true}
           clusterRadius={50}
-          data={API_BASE_URL + "/geojson/users"}
+          data={API_BASE_URL + "/geojson/public-users"}
           promoteId="id"
           type={"geojson"}
         >
