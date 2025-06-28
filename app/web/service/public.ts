33391c420f4d98d7943e508e5692193e946d739d
Nicole Abramowski <nabramow@gmail.com>
2025-06-28 12:56:27 -0700

    WIP

--- app/web/service/public.ts
+++ app/web/service/public.ts
@@ -4,9 +4,5 @@ import client from "./client";
 
 export async function getSignupPageInfo() {
   const res = await client.public.getSignupPageInfo(new Empty());
-  const objectRes = res.toObject();
-
-  console.log("OBJECT RES", objectRes);
-
-  return objectRes;
+  return res.toObject();
 }
