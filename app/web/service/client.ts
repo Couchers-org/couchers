abf06b18bac3a8b88d8925f458b9ac0579158963
Nicole Abramowski <nabramow@gmail.com>
2025-06-29 10:45:24 -0700

    Experiment to see if mismatch

--- app/web/service/client.ts
+++ app/web/service/client.ts
@@ -82,7 +82,7 @@ const opts = {
 
 const publicOpts = {
   unaryInterceptors: [timeoutInterceptor], //<-- No auth for public API
-  withCredentials: false, // <-- No cookies for public API
+  withCredentials: true, // <-- No cookies for public API
   transport: "fetch", // Use fetch transport for public API
 };
 
