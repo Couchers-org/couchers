23db2213fc4e9159f9f5235dc47cee507c0e9d44
banflam <rishiagrawal2002@gmail.com>
2025-06-30 15:06:44 -0400

    fix: fixed the bug where current and past request icons would just be a gray color instead of red or green based on their acceptance status

--- app/web/features/messages/requests/HostRequestStatusIcon.tsx
+++ app/web/features/messages/requests/HostRequestStatusIcon.tsx
@@ -18,23 +18,21 @@ export default function HostRequestStatusIcon({
   let icon = null;
   let color = null;
 
-  const isRequestPast = dayjs(hostRequest.toDate).isBefore(dayjs().format("L"));
-
   if (s === HostRequestStatus.HOST_REQUEST_STATUS_ACCEPTED) {
     icon = <CheckIcon fontSize="inherit" />;
-    color = isRequestPast ? "grey.500" : "gray";
+    color = "green";
   } else if (s === HostRequestStatus.HOST_REQUEST_STATUS_REJECTED) {
     icon = <CrossIcon fontSize="inherit" />;
-    color = isRequestPast ? "grey.500" : "red";
+    color = "red";
   } else if (s === HostRequestStatus.HOST_REQUEST_STATUS_PENDING) {
     icon = <QuestionIcon fontSize="inherit" />;
-    color = isRequestPast ? "grey.500" : "gray";
+    color = "gray";
   } else if (s === HostRequestStatus.HOST_REQUEST_STATUS_CANCELLED) {
     icon = <CrossIcon fontSize="inherit" />;
-    color = isRequestPast ? "grey.500" : "gray";
+    color = "red";
   } else if (s === HostRequestStatus.HOST_REQUEST_STATUS_CONFIRMED) {
     icon = <CheckIcon fontSize="inherit" />;
-    color = isRequestPast ? "grey.500" : "green";
+    color = "green";
   } else throw new Error(`Unhandled host request case: ${s}`);
 
   return (
