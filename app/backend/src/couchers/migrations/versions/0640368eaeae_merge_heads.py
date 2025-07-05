fab3116169f2eaa150f258e7807d8706f62aed98
Andrei Kolesnik <andrei.kolesnik.usla@gmail.com>
2025-07-05 08:44:18 -0700

    Pass ruff check

--- app/backend/src/couchers/migrations/versions/0640368eaeae_merge_heads.py
+++ app/backend/src/couchers/migrations/versions/0640368eaeae_merge_heads.py
@@ -5,14 +5,10 @@ Revises: 14585a4e1868, f176b9395ba1
 Create Date: 2025-07-05 08:06:14.483948
 
 """
-from alembic import op
-import sqlalchemy as sa
-import geoalchemy2
-
 
 # revision identifiers, used by Alembic.
-revision = '0640368eaeae'
-down_revision = ('14585a4e1868', 'f176b9395ba1')
+revision = "0640368eaeae"
+down_revision = ("14585a4e1868", "f176b9395ba1")
 branch_labels = None
 depends_on = None
 
