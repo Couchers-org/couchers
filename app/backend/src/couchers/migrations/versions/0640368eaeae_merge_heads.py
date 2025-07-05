5e27e596bd7e5dc2f4a97713abb526dd7c3585b9
Andrei Kolesnik <andrei.kolesnik.usla@gmail.com>
2025-07-05 08:08:40 -0700

    Merge Alembic heads 14585a4e1868 and f176b9395ba1 to resolve multiple head revisions

---
+++ app/backend/src/couchers/migrations/versions/0640368eaeae_merge_heads.py
@@ -0,0 +1,25 @@
+"""Merge heads
+
+Revision ID: 0640368eaeae
+Revises: 14585a4e1868, f176b9395ba1
+Create Date: 2025-07-05 08:06:14.483948
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import geoalchemy2
+
+
+# revision identifiers, used by Alembic.
+revision = '0640368eaeae'
+down_revision = ('14585a4e1868', 'f176b9395ba1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
