a3b0b1d5748f180474fd12bd15bc488f1a0e8d31
Nicole Abramowski <nabramow@gmail.com>
2025-07-11 15:51:31 -0700

    Fix prettier to only look at relevant files and not node_modules

--- app/web/components/LanguagePickerSelect.test.tsx
+++ app/web/components/LanguagePickerSelect.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen, within } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
 import { service } from "service";
