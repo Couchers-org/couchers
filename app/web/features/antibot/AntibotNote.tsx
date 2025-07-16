e75575c0064db1762c0dc75b7899b59e9de1b201
Nicole Abramowski <nabramow@gmail.com>
2025-07-15 21:58:36 -0700

    Some fixes

--- app/web/features/antibot/AntibotNote.tsx
+++ app/web/features/antibot/AntibotNote.tsx
@@ -1,17 +1,20 @@
 import StyledLink from "components/StyledLink";
+import { useTranslation } from "i18n";
+import { GLOBAL } from "i18n/namespaces";
 
 export default function AntibotNote() {
+  const { t } = useTranslation([GLOBAL]);
   return (
     <>
-      This site is protected by reCAPTCHA and the Google{" "}
+      {t("antibot.note")}{" "}
       <StyledLink target="_blank" href="https://policies.google.com/privacy">
-        Privacy Policy
+        {t("antibot.privacy_policy")}
       </StyledLink>{" "}
       and{" "}
       <StyledLink target="_blank" href="https://policies.google.com/terms">
-        Terms of Service
+        {t("antibot.terms_of_service")}
       </StyledLink>{" "}
-      apply.
+      {t("antibot.apply")}.
     </>
   );
 }
