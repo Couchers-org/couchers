197bf544d9c34aa1e68e327c699ecde14f8cc2f3
Aapeli <git@aapelivuorinen.com>
2025-07-26 10:23:44 -0400

    Clean up frontend

--- app/web/features/auth/QuickLink.tsx
+++ app/web/features/auth/QuickLink.tsx
@@ -16,7 +16,7 @@ export interface UnsubscribeParams {
   sig?: string;
 }
 
-export default function Unsubscribe() {
+export default function QuickLink() {
   const { t } = useTranslation([AUTH, GLOBAL]);
 
   const router = useRouter();
@@ -32,7 +32,7 @@ export default function Unsubscribe() {
   } = useMutation<UnsubscribeRes.AsObject, RpcError, UnsubscribeParams>(
     async ({ payload, sig }) => {
       if (payload === undefined || sig === undefined) {
-        throw Error(t("auth:unsubscribe.missing_payload_or_sig"));
+        throw Error(t("auth:quick_links.missing_payload_or_sig"));
       }
       return await service.auth.unsubscribe(payload, sig);
     },
@@ -40,18 +40,18 @@ export default function Unsubscribe() {
 
   return (
     <>
-      <HtmlMeta title={t("auth:unsubscribe.title")} />
-      <PageTitle>{t("auth:unsubscribe.title")}</PageTitle>
+      <HtmlMeta title={t("auth:quick_links.title")} />
+      <PageTitle>{t("auth:quick_links.title")}</PageTitle>
       {error && (
         <Alert severity="error">
-          {t("auth:unsubscribe.error_message", {
+          {t("auth:quick_links.error_message", {
             message: error.message,
           })}
         </Alert>
       )}
       {isSuccess && <Alert severity="success">{data!.response}</Alert>}
       <Button onClick={() => unsubscribe({ payload, sig })} loading={isLoading}>
-        {t("auth:unsubscribe.button_text")}
+        {t("auth:quick_links.button_text")}
       </Button>
     </>
   );
--- app/web/pages/unsubscribe.tsx
+++ app/web/pages/unsubscribe.tsx
@@ -1,12 +1,14 @@
+// This is a legacy page and can be removed in 2026!
+// new links go to quick-link.tsx
 import { appGetLayout } from "components/AppRoute";
-import Unsubscribe from "features/auth/Unsubscribe";
+import QuickLink from "features/auth/QuickLink";
 import { AUTH, GLOBAL } from "i18n/namespaces";
 import { translationStaticProps } from "i18n/server-side-translations";
 
 export const getStaticProps = translationStaticProps([GLOBAL, AUTH]);
 
 export default function UnsubscribePage() {
-  return <Unsubscribe />;
+  return <QuickLink />;
 }
 
 UnsubscribePage.getLayout = appGetLayout({ isPrivate: false });
