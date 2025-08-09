82b1d8c2fab4ca586f7cf1fd11a235eafa2bccb7
Felix Balda <private@felixbalda.com>
2025-08-09 15:59:45 +0200

    Convert more MUI to styled

--- app/web/features/donations/DonationsBox.tsx
+++ app/web/features/donations/DonationsBox.tsx
@@ -9,7 +9,6 @@ import {
   styled,
   Typography,
 } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
 import Alert from "components/Alert";
 import Button from "components/Button";
 import StyledLink from "components/StyledLink";
@@ -21,184 +20,20 @@ import { RpcError } from "grpc-web";
 import { Trans, useTranslation } from "i18n";
 import { DONATIONS } from "i18n/namespaces";
 import { useRouter } from "next/router";
-import { useMemo, useRef, useState } from "react";
+import React, { PropsWithChildren, useMemo, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import { service } from "service";
 import { theme } from "theme";
 
-const useStyles = makeStyles((theme) => ({
-  donationsBox: {
-    padding: theme.spacing(2),
-    border: `2px solid ${theme.palette.grey[200]}`,
-    borderRadius: theme.shape.borderRadius * 2,
-  },
-
-  donationsBoxRow: {
-    gridTemplateColumns: "repeat( auto-fit, minmax(160px, 1fr) )",
-    gridAutoRows: "2.75rem",
-    display: "grid",
-    gap: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-  },
-
-  donationsBoxSubRow: {
-    gridTemplateColumns: "repeat( auto-fit, minmax(72px, 1fr) )",
-    display: "grid",
-    gap: theme.spacing(2),
-  },
-
-  buttonSecondary: {
-    "&&": {
-      boxShadow: "initial",
-      color: theme.palette.grey[600],
-      cursor: "pointer",
-      fontWeight: 700,
-      fontSize: theme.typography.button.fontSize,
-      transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-      justifyContent: "center",
-      alignItems: "center",
-      border: `2px solid ${theme.palette.grey[200]}`,
-      borderRadius: "0.5rem",
-      backgroundColor: theme.palette.grey[200],
-      margin: "initial",
-      height: "100%",
-      width: "100%",
-      display: "flex",
-    },
-
-    "&:hover": {
-      border: `2px solid ${theme.palette.primary.main}`,
-      backgroundColor: theme.palette.background.paper,
-      color: theme.palette.primary.main,
-      transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-    },
-  },
-
-  buttonSecondaryRadio: {
-    "&&": {
-      boxShadow: "initial",
-      margin: 0,
-    },
-    "&:hover > .MuiFormControlLabel-label": {
-      border: `2px solid ${theme.palette.primary.main}`,
-      backgroundColor: theme.palette.background.paper,
-      color: theme.palette.primary.main,
-      transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-    },
-    "& > .MuiRadio-root": {
-      position: "absolute",
-      left: "-10000px",
-    },
-    "& > .MuiFormControlLabel-label": {
-      color: theme.palette.grey[600],
-      fontWeight: 700,
-      fontSize: theme.typography.button.fontSize,
-      transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-      justifyContent: "center",
-      alignItems: "center",
-      border: `2px solid ${theme.palette.grey[200]}`,
-      borderRadius: "0.5rem",
-      backgroundColor: theme.palette.grey[200],
-      margin: "initial",
-      height: "100%",
-      width: "100%",
-      display: "flex",
-    },
-    "& > .Mui-checked ~.MuiFormControlLabel-label": {
-      border: `2px solid ${theme.palette.primary.main}`,
-      backgroundColor: theme.palette.background.paper,
-    },
-  },
-
-  buttonSecondaryActive: {
-    "&&": {
-      border: `2px solid ${theme.palette.primary.main}`,
-      backgroundColor: theme.palette.background.paper,
-    },
-  },
-
-  buttonMain: {
-    backgroundColor: theme.palette.primary.main,
-    "&&": {
-      borderRadius: "0.5rem",
-      boxShadow: "initial",
-    },
-    "&:hover": {
-      opacity: 0.4,
-      backgroundColor: theme.palette.primary.main,
-    },
-    "& .MuiButton-label": {
-      color: theme.palette.background.paper,
-      transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-    },
-  },
-
-  buttonMainText: {
-    color: theme.palette.background.paper,
-    fontWeight: 700,
-    transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-  },
-
-  formGroup: {
-    width: "100%",
-  },
-
-  marginY2: {
-    margin: theme.spacing(2, 0),
-  },
-
-  marginBottom2: {
-    marginBottom: theme.spacing(2),
-  },
-
-  inputWrapper: {
-    position: "relative",
-    "&::before": {
-      content: `'${DONATIONS_BOX_CURRENCY}'`,
-      position: "absolute",
-      top: "50%",
-      transform: "translateY(-50%)",
-      left: theme.spacing(1),
-      color: theme.palette.grey[600],
-      fontWeight: 700,
-      fontSize: theme.typography.button.fontSize,
-    },
-  },
-
-  inputNumber: {
-    width: "100%",
-    height: "100%",
-    border: `2px solid ${theme.palette.grey[200]}`,
-    paddingLeft: theme.spacing(2),
-    color: theme.palette.grey[600],
-    fontWeight: 700,
-    fontSize: theme.typography.button.fontSize,
-    "&&": {
-      borderRadius: theme.shape.borderRadius * 2,
-      boxShadow: "initial",
-    },
-    "&:hover": {
-      border: `2px solid ${theme.palette.primary.main}`,
-    },
-    "&:focus-visible": {
-      border: `2px solid ${theme.palette.primary.main}`,
-      outline: "none",
-      boxShadow: "none",
-    },
-  },
-
-  inputNumberActive: {
-    "&&": {
-      border: `2px solid ${theme.palette.primary.main}`,
-    },
-  },
-}));
+const SUB_GRID_ITEM_AMOUNT = 2;
 
 const StyledForm = styled("form")(() => ({
   padding: theme.spacing(2),
   border: `2px solid ${theme.palette.grey[200]}`,
   borderRadius: theme.shape.borderRadius * 2,
+  display: "flex",
+  flexDirection: "column",
 }));
 
 const StyledFormGroup = styled(FormControl)(() => ({
@@ -253,30 +88,27 @@ const StyledLabelledRadioButton = styled(RadioFormControlLabel)(() => ({
 }));
 
 const StyledDivider = styled(Divider)(() => ({
-  margin: theme.spacing(2, 0),
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2),
 }));
-
-// gridTemplateColumns: "repeat( auto-fit, minmax(160px, 1fr) )",
-//   gridAutoRows: "2.75rem",
-//   display: "grid",
-//   gap: theme.spacing(2),
-//   marginBottom: theme.spacing(2),
-
-// const StyledAmountGrid = styled("div")(() => ({
-//   display: "grid",
-//   gridTemplateColumns: "repeat(4, 1fr)",
-// }));
 
 const StyledAmountGrid = styled("div")(() => ({
   display: "grid",
-  gridTemplateColumns: "repeat(auto-fit, minmax(81px, 1fr))",
-  // gridTemplateRows: "repeat(auto-fit, auto)",
-  // alignItems: "stretch",
+  gridTemplateColumns: "repeat(auto-fit, minmax(160px, 1fr))",
+  gridAutoRows: "2.75rem",
   gap: theme.spacing(2),
   marginBottom: theme.spacing(2),
 }));
 
-const StyledAmountButton = styled("button")(() => ({
+const StyledAmountSubGrid = styled("div")(() => ({
+  display: "grid",
+  gridTemplateColumns: "repeat( auto-fit, minmax(72px, 1fr))",
+  gap: theme.spacing(2),
+}));
+
+const StyledAmountButton = styled("button", {
+  shouldForwardProp: (propName) => propName !== "isActive",
+})<{ isActive: boolean }>(({ isActive }) => ({
   boxShadow: "initial",
   color: theme.palette.grey[600],
   cursor: "pointer",
@@ -285,14 +117,15 @@ const StyledAmountButton = styled("button")(() => ({
   transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
   justifyContent: "center",
   alignItems: "center",
-  border: `2px solid ${theme.palette.grey[200]}`,
+  border: `2px solid ${isActive ? theme.palette.primary.main : theme.palette.grey[200]}`,
   borderRadius: "0.5rem",
-  backgroundColor: theme.palette.grey[200],
+  backgroundColor: isActive
+    ? theme.palette.background.paper
+    : theme.palette.grey[200],
   margin: "initial",
-  height: "44px",
+  height: "100%",
   width: "100%",
   display: "flex",
-
   "&:hover": {
     border: `2px solid ${theme.palette.primary.main}`,
     backgroundColor: theme.palette.background.paper,
@@ -315,10 +148,12 @@ const StyledAmountInputWrapper = styled("div")(() => ({
   },
 }));
 
-const StyledAmountInput = styled("input")(() => ({
+const StyledAmountInput = styled("input", {
+  shouldForwardProp: (propName) => propName !== "isActive",
+})<{ isActive: boolean }>(({ isActive }) => ({
   width: "100%",
-  height: "44px",
-  border: `2px solid ${theme.palette.grey[200]}`,
+  height: "100%",
+  border: `2px solid ${isActive ? theme.palette.primary.main : theme.palette.grey[200]}`,
   paddingLeft: theme.spacing(2),
   color: theme.palette.grey[600],
   fontWeight: 700,
@@ -335,6 +170,43 @@ const StyledAmountInput = styled("input")(() => ({
     outline: "none",
     boxShadow: "none",
   },
+}));
+
+function AmountGrid(props: PropsWithChildren) {
+  const children = React.Children.toArray(props.children);
+  const subGrids = new Array(children.length / SUB_GRID_ITEM_AMOUNT)
+    .fill(0)
+    .map((_, index) => (
+      <StyledAmountSubGrid key={index}>
+        {children.slice(
+          index * SUB_GRID_ITEM_AMOUNT,
+          (index + 1) * SUB_GRID_ITEM_AMOUNT,
+        )}
+      </StyledAmountSubGrid>
+    ));
+
+  return <StyledAmountGrid>{subGrids}</StyledAmountGrid>;
+}
+
+const StyledSubmitButton = styled(Button)(() => ({
+  backgroundColor: theme.palette.primary.main,
+
+  borderRadius: "0.5rem",
+  boxShadow: "initial",
+  height: "2.75rem",
+  marginBottom: theme.spacing(2),
+  "&:hover": {
+    opacity: 0.4,
+    backgroundColor: theme.palette.primary.main,
+  },
+  "& .MuiButton-label": {
+    color: theme.palette.background.paper,
+    transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
+  },
+  color: theme.palette.background.paper,
+  fontWeight: 700,
+  transition: `color ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
+  alignSelf: "stretch",
 }));
 
 export interface DonationFormData {
@@ -440,12 +312,7 @@ export default function DonationsBox() {
           {t("donations_box.alert.warning_message")}
         </Alert>
       )}
-      <Typography
-        // className={classes.marginBottom2}
-        variant="h3"
-      >
-        {t("donations_box.title")}
-      </Typography>
+      <Typography variant="h3">{t("donations_box.title")}</Typography>
       <Controller
         control={control}
         name="recurring"
@@ -481,209 +348,72 @@ export default function DonationsBox() {
       <Typography variant="body2" gutterBottom>
         {t("donations_recurrence_explainer")}
       </Typography>
-
-      {/* <Divider className={classes.marginY2} /> */}
       <StyledDivider />
-
       <Controller
         name="amount"
         control={control}
         defaultValue={DONATIONS_BOX_VALUES[2]}
         render={({ field }) => (
-          <StyledAmountGrid>
-            {/* <div
-            // lassName={classes.donationsBoxSubRow}
-            > */}
-            {DONATIONS_BOX_VALUES.map((value) => {
-              return (
-                <StyledAmountButton
-                  key={value}
-                  type="button"
-                  onClick={handleDonationAmountClick({
-                    amount: value,
-                    onChange: field.onChange,
-                  })}
-                >
-                  {formatDonationValue(value)}
-                </StyledAmountButton>
-              );
-            })}
-
-            {/* <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[0],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[0] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[0])}
-              </button>
-              <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[1],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[1] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[1])}
-              </button>
-            </div>
-
-            <div
-            // className={classes.donationsBoxSubRow}
-            >
-              <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[2],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[2] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[2])}
-              </button>
-              <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[3],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[3] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[3])}
-              </button>
-            </div>
-
-            <div
-            // className={classes.donationsBoxSubRow}
-            >
-              <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[4],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[4] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[4])}
-              </button>
-              <button
-                type="button"
-                onClick={handleDonationAmountClick({
-                  amount: DONATIONS_BOX_VALUES[5],
-                  onChange: field.onChange,
-                })}
-                // className={classNames(classes.buttonSecondary, {
-                //   [classes.buttonSecondaryActive]:
-                //     field.value === DONATIONSBOX_VALUES[5] &&
-                //     isPredefinedAmount,
-                // })}
-              >
-                {formatDonationValue(DONATIONS_BOX_VALUES[5])}
-              </button>
-            </div>
-
-            <div
-            // className={classes.donationsBoxRow}
-            >
-              <div
-              // className={classes.donationsBoxSubRow}
-              >
-                <button
-                  type="button"
-                  onClick={handleDonationAmountClick({
-                    amount: DONATIONS_BOX_VALUES[6],
-                    onChange: field.onChange,
-                  })}
-                  // className={classNames(classes.buttonSecondary, {
-                  //   [classes.buttonSecondaryActive]:
-                  //     field.value === DONATIONSBOX_VALUES[6] &&
-                  //     isPredefinedAmount,
-                  // })}
-                >
-                  {formatDonationValue(DONATIONS_BOX_VALUES[6])}
-                </button> */}
-            <StyledAmountInputWrapper>
-              <StyledAmountInput
-                {...field}
-                value={Number(field.value)}
-                ref={customAmountInput}
-                type="number"
-                min="1"
-                onChange={(e) => {
-                  field.onChange(
-                    typeof e.target.valueAsNumber === "number"
-                      ? e.target.valueAsNumber
-                      : DONATIONS_BOX_VALUES[0],
-                  );
-                  setIsPredefinedAmount(false);
-                }}
-                // className={classNames(classes.inputNumber, {
-                //   [classes.inputNumberActive]: !isPredefinedAmount,
-                // })}
-                id="amount"
-              />
-            </StyledAmountInputWrapper>
-            {/* </div> */}
-          </StyledAmountGrid>
-          // </div>
+          <AmountGrid>
+            {[
+              ...DONATIONS_BOX_VALUES.map((value) => {
+                return (
+                  <StyledAmountButton
+                    key={value}
+                    type="button"
+                    isActive={isPredefinedAmount && value === field.value}
+                    onClick={handleDonationAmountClick({
+                      amount: value,
+                      onChange: field.onChange,
+                    })}
+                  >
+                    {formatDonationValue(value)}
+                  </StyledAmountButton>
+                );
+              }),
+              <StyledAmountInputWrapper key={"input"}>
+                <StyledAmountInput
+                  {...field}
+                  isActive={!isPredefinedAmount}
+                  value={Number(field.value)}
+                  ref={customAmountInput}
+                  type="number"
+                  min="1"
+                  onChange={(e) => {
+                    field.onChange(
+                      typeof e.target.valueAsNumber === "number"
+                        ? e.target.valueAsNumber
+                        : DONATIONS_BOX_VALUES[0],
+                    );
+                    setIsPredefinedAmount(false);
+                  }}
+                  id="amount"
+                />
+              </StyledAmountInputWrapper>,
+            ]}
+          </AmountGrid>
         )}
       />
-
       <Typography variant="body2" paragraph>
-        <Trans t={t} i18nKey="donations_box.helper_text">
-          You will be redirected to Stripe, our payment processor, to complete
-          your donation. To cancel or update an existing donation at any time,{" "}
-          <StyledLink
-            href="#"
-            onClick={async (e) => {
-              e.preventDefault();
-              router.push(await service.donations.getDonationPortalLink());
-            }}
-          >
-            click here to go to the donation portal
-          </StyledLink>
-          .{t("donations_box.helper_text")}
-        </Trans>
+        <Trans
+          t={t}
+          i18nKey="donations_box.helper_text"
+          components={{
+            2: (
+              <StyledLink
+                href="#"
+                onClick={async (e) => {
+                  e.preventDefault();
+                  router.push(await service.donations.getDonationPortalLink());
+                }}
+              />
+            ),
+          }}
+        />
       </Typography>
-
-      <div
-      // className={classes.donationsBoxRow}
-      >
-        <Button
-          type="submit"
-          loading={isLoading}
-          onClick={onSubmit}
-          // classes={{
-          //   root: classes.buttonMain,
-          //   label: classes.buttonMainText,
-          // }}
-        >
-          {t("donations_box.action_button_label")}
-        </Button>
-      </div>
+      <StyledSubmitButton type="submit" loading={isLoading} onClick={onSubmit}>
+        {t("donations_box.action_button_label")}
+      </StyledSubmitButton>
     </StyledForm>
   );
 }
