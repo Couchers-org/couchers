4d94107c6553f4492ce311fbeaab6c9059dad420
Nicole Abramowski <nabramow@gmail.com>
2025-08-23 21:36:36 -0700

    Upgrade Sentry

---
+++ app/web/instrumentation-client.ts
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5594adb1a53e41bfbb9f2cc5c91e2dbd@o782870.ingest.sentry.io/5887585",
+  environment: process.env.NEXT_PUBLIC_COUCHERS_ENV,
+  release: process.env.NEXT_PUBLIC_VERSION,
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [],
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
+
+// This export will instrument router navigations, and is only relevant if you enable tracing.
+// `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- app/web/pages/_app.tsx
+++ app/web/pages/_app.tsx
@@ -16,7 +16,6 @@ import { ReactQueryClientProvider } from "features/reactQueryClient";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import nextI18nextConfig from "next-i18next.config";
-import Sentry from "platform/sentry";
 import React, { ReactNode, useEffect } from "react";
 import TagManager from "react-gtm-module";
 import { polyfill } from "seamless-scroll-polyfill";
@@ -41,11 +40,6 @@ function MyApp({ Component, pageProps }: AppWithLayoutProps) {
 
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_COUCHERS_ENV === "prod") {
-      Sentry.init({
-        dsn: "https://5594adb1a53e41bfbb9f2cc5c91e2dbd@o782870.ingest.sentry.io/5887585",
-        environment: process.env.NEXT_PUBLIC_COUCHERS_ENV,
-        release: process.env.NEXT_PUBLIC_VERSION,
-      });
       TagManager.initialize({ gtmId: "GTM-PXP3896" });
     }
   }, []);
