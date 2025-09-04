f85faa8edd7e4b629cde26562bd5bccc679df717
Felix Balda <private@felixbalda.com>
2025-09-04 09:01:58 +0200

    React query fix

--- app/web/features/team/useVolunteerInfo.tsx
+++ app/web/features/team/useVolunteerInfo.tsx
@@ -1,14 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
 import { volunteerInfoQueryKey } from "features/queryKeys";
 import { RpcError } from "grpc-web";
 import { GetMyVolunteerInfoRes } from "proto/account_pb";
-import { useQuery } from "react-query";
 import { service } from "service";
 
 export default function useVolunteerInfo() {
-  const accountInfoQuery = useQuery<GetMyVolunteerInfoRes.AsObject, RpcError>(
-    volunteerInfoQueryKey,
-    service.account.getMyVolunteerInfo,
-  );
+  const accountInfoQuery = useQuery<GetMyVolunteerInfoRes.AsObject, RpcError>({
+    queryKey: [volunteerInfoQueryKey],
+    queryFn: service.account.getMyVolunteerInfo,
+  });
 
   return accountInfoQuery;
 }
