c0bb95b097c3d6b943ae190eb5707872f919409d
Aapeli <git@aapelivuorinen.com>
2025-11-29 09:02:23 -0500

    Clean up

--- app/backend/src/couchers/jobs/enqueue.py
+++ app/backend/src/couchers/jobs/enqueue.py
@@ -3,13 +3,11 @@ Background jobs
 """
 
 import logging
-from datetime import timedelta
 
 from google.protobuf.message import Message
 from sqlalchemy.orm import Session
 
 from couchers.models import BackgroundJob
-from couchers.utils import now
 
 logger = logging.getLogger(__name__)
 
@@ -20,14 +18,12 @@ def queue_job(
     payload: Message,
     max_tries: int | None = None,
     priority: int | None = None,
-    delay: timedelta | None = None,
 ) -> None:
-    job = BackgroundJob(
-        job_type=job_type,
-        payload=payload.SerializeToString(),
-        max_tries=max_tries,
-        priority=priority,
+    session.add(
+        BackgroundJob(
+            job_type=job_type,
+            payload=payload.SerializeToString(),
+            max_tries=max_tries,
+            priority=priority,
+        )
     )
-    if delay is not None:
-        job.next_attempt_after = now() + delay
-    session.add(job)
