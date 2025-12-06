28e01dc9e267c0d89bb40c2db0b5857b777ac5e3
Nicole Abramowski <nabramow@gmail.com>
2025-12-06 21:53:29 +0000

    Upgrade next and integrate mui

--- app/web/pages/_document.tsx
+++ app/web/pages/_document.tsx
@@ -1,35 +1,54 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import {
+  documentGetInitialProps,
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+} from "@mui/material-nextjs/v15-pagesRouter";
+import {
+  DocumentContext,
+  DocumentProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 import { theme } from "theme";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang={this.props.locale ?? "en"}>
-        <Head>
-          <link rel="preconnect" href="https://cdn.couchers.org" />
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/logo512.png" />
-          <link
-            rel="stylesheet"
-            href="https://cdn.couchers.org/fonts/ubuntu/ubuntu.css"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.couchers.org/fonts/wordmark/wordmark.css"
-          />
-          {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && (
-            <script
-              async
-              src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-            ></script>
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument(
+  props: DocumentProps & DocumentHeadTagsProps,
+) {
+  return (
+    <Html lang={props.locale ?? "en"}>
+      <Head>
+        <DocumentHeadTags {...props} />
+
+        <link rel="preconnect" href="https://cdn.couchers.org" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo512.png" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.couchers.org/fonts/ubuntu/ubuntu.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.couchers.org/fonts/wordmark/wordmark.css"
+        />
+        {process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && (
+          <script
+            async
+            src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          ></script>
+        )}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
