8da3b882a382931a772fa5d60d7afe6c48582231
Aapeli <git@aapelivuorinen.com>
2025-12-14 08:31:56 +1000

    Fix up host req notifs

--- app/backend/src/couchers/jobs/handlers.py
+++ app/backend/src/couchers/jobs/handlers.py
@@ -390,6 +390,7 @@ def send_request_notifications(payload: empty_pb2.Empty) -> None:
                 select(User, HostRequest, func.max(Message.id))
                 .where(User.id == user_id)
                 .join(HostRequest, HostRequest.host_user_id == User.id)
+                .where_moderated_content_visible_to_user_column(HostRequest, HostRequest.host_user_id)
                 .where_users_column_visible(context, HostRequest.surfer_user_id)
                 .join(Message, Message.conversation_id == HostRequest.conversation_id)
                 .where(Message.id > HostRequest.host_last_seen_message_id)
