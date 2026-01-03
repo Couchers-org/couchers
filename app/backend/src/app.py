e7d3a0fb2db519499a2c71bf54ac7b39c50ea4e1
Alexey <alexeywastaken@proton.me>
2026-01-03 11:08:41 +0000

    Init sentry and check db connection in worker processes (#7584)

--- app/backend/src/app.py
+++ app/backend/src/app.py
@@ -4,8 +4,6 @@ import sys
 from os import environ
 from tempfile import TemporaryDirectory
 
-from couchers.i18n.i18n import get_i18next
-
 # these two lines need to be at the top of the file before we span child processes
 # this temp dir will be destroyed when prometheus_multiproc_dir is destroyed, aka at the end of the program
 prometheus_multiproc_dir = TemporaryDirectory()
@@ -20,6 +18,7 @@ from sqlalchemy.sql import text
 from couchers.config import check_config, config
 from couchers.db import apply_migrations, session_scope
 from couchers.experimentation import setup_experimentation
+from couchers.i18n.i18n import get_i18next
 from couchers.jobs.worker import start_jobs_scheduler, start_jobs_worker
 from couchers.metrics import create_prometheus_server
 from couchers.server import create_main_server, create_media_server
@@ -45,39 +44,39 @@ def log_unhandled_exception(exc_type, exc_value, exc_traceback):
 
 sys.excepthook = log_unhandled_exception
 
+if config["SENTRY_ENABLED"]:
+    # Sends exception tracebacks to Sentry, a cloud service for collecting exceptions
+    sentry_sdk.init(
+        config["SENTRY_URL"],
+        traces_sample_rate=0.0,
+        environment=config["COOKIE_DOMAIN"],
+        release=config["VERSION"],
+        default_integrations=False,
+        integrations=[
+            # we need to manually list out the integrations, there is no other way of disabling
+            # the global excepthook integration we want to disable that because it seems to be
+            # picking up already handled gRPC errors (e.g. grpc.StatusCode.NOT_FOUND)
+            argv.ArgvIntegration(),
+            atexit.AtexitIntegration(),
+            dedupe.DedupeIntegration(),
+            sentry_logging.LoggingIntegration(),
+            modules.ModulesIntegration(),
+            stdlib.StdlibIntegration(),
+            threading.ThreadingIntegration(),
+        ],
+    )
 
+logger.info("Checking DB connection")
+with session_scope() as session:
+    res = session.execute(text("SELECT 42;"))
+    if list(res) != [(42,)]:
+        raise Exception("Failed to connect to DB")
+
+
+# In other processes __name__ is __mp_main__
 if __name__ == "__main__":
-    if config["SENTRY_ENABLED"]:
-        # Sends exception tracebacks to Sentry, a cloud service for collecting exceptions
-        sentry_sdk.init(
-            config["SENTRY_URL"],
-            traces_sample_rate=0.0,
-            environment=config["COOKIE_DOMAIN"],
-            release=config["VERSION"],
-            default_integrations=False,
-            integrations=[
-                # we need to manually list out the integrations, there is no other way of disabling
-                # the global excepthook integration we want to disable that because it seems to be
-                # picking up already handled gRPC errors (e.g. grpc.StatusCode.NOT_FOUND)
-                argv.ArgvIntegration(),
-                atexit.AtexitIntegration(),
-                dedupe.DedupeIntegration(),
-                sentry_logging.LoggingIntegration(),
-                modules.ModulesIntegration(),
-                stdlib.StdlibIntegration(),
-                threading.ThreadingIntegration(),
-            ],
-        )
-
     # used to export metrics
     create_prometheus_server(8000)
-
-    logger.info("Checking DB connection")
-
-    with session_scope() as session:
-        res = session.execute(text("SELECT 42;"))
-        if list(res) != [(42,)]:
-            raise Exception("Failed to connect to DB")
 
     logger.info("Running DB migrations")
 
