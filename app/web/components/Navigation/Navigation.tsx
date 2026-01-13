d78bc7ac0e624f8b07d8593d9a91cc4a42febf42
Nicole Abramowski <nabramow@gmail.com>
2026-01-13 16:39:59 +0700

    Clean up nav and add merch shop

--- app/web/components/Navigation/Navigation.tsx
+++ app/web/components/Navigation/Navigation.tsx
@@ -41,6 +41,7 @@ import {
   routeToProfile,
   searchRoute,
   settingsRoute,
+  shopRoute,
   signupRoute,
   volunteerRoute,
 } from "routes";
@@ -97,10 +98,6 @@ const loggedInNavMenu = (
   t: TFunction<"global", undefined>,
   pingData: PingData,
 ): Array<MenuItemProps> => [
-  {
-    name: t("nav.dashboard"),
-    route: dashboardRoute,
-  },
   {
     name: t("nav.messages"),
     route: messagesRoute,
@@ -170,12 +167,8 @@ const loggedInMenuDropDown = (
   },
   {
     type: "link",
-    name: t("nav.messages"),
-    route: messagesRoute,
-    notificationCount:
-      (pingData?.unseenMessageCount ?? 0) +
-      (pingData?.unseenReceivedHostRequestCount ?? 0) +
-      (pingData?.unseenSentHostRequestCount ?? 0),
+    name: t("nav.dashboard"),
+    route: dashboardRoute,
   },
   {
     type: "link",
@@ -209,6 +202,11 @@ const loggedInMenuDropDown = (
     name: t("report.label"),
     dialogComponent: ReportDialog,
     dialogLabel: t("report.label"),
+  },
+  {
+    type: "link",
+    name: t("nav.merch_shop"),
+    route: shopRoute,
     hasBottomDivider: true,
   },
   {
