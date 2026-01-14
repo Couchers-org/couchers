76827b16f5266df96f9eca824a3ea7bbbe4ac99a
AlexVCS <alexcurtisslep@gmail.com>
2026-01-14 14:35:36 -0500

    add reminder card layout/overall structure

--- app/web/features/dashboard/ReminderItem.tsx
+++ app/web/features/dashboard/ReminderItem.tsx
@@ -2,8 +2,10 @@
 //   reminder: any;
 // }
 
-import { Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { Box, Typography } from "@mui/material";
 import Button from "components/Button";
+import IconButton from "components/IconButton";
 import Link from "next/link";
 
 export default function ReminderItem() {
@@ -12,29 +14,73 @@ export default function ReminderItem() {
     <>
       {/* {JSON.stringify(reminder)}
       <div>{reminder}</div> */}
-      {/* <Box
+      <Box
         sx={{
-          bgcolor: "#fff5e4",
-        }}
-        maxWidth="lg"
-      > */}
-      <button>×</button>
-
-      <Typography
-        variant="h1"
-        component="h2"
-        sx={{
-          marginBottom: "16px",
+          backgroundColor: "#FFF4E6",
+          padding: "24px",
+          position: "relative",
+          minHeight: "200px",
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          maxWidth: "300px",
         }}
       >
-        Title
-      </Typography>
-      <p>Description</p>
+        <IconButton
+          // onClick={onClose}
+          aria-label="close"
+          sx={{
+            position: "absolute",
+            top: 8,
+            right: 8,
+            color: "#A68966",
+          }}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
 
-      <Button component={Link} color="primary" href={""}>
-        Button
-      </Button>
-      {/* </Box> */}
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: 800,
+              color: "#333",
+              lineHeight: 1.2,
+              marginBottom: "16px",
+              fontSize: "1.5rem",
+            }}
+          >
+            title
+          </Typography>
+
+          <Typography
+            variant="body1"
+            sx={{
+              color: "#444",
+              lineHeight: 1.5,
+              marginBottom: "24px",
+            }}
+          >
+            description
+          </Typography>
+        </Box>
+
+        <Button
+          component={Link}
+          href={"href"}
+          fullWidth
+          variant="contained"
+          sx={{
+            backgroundColor: "primary",
+            color: "text.primary",
+            fontWeight: 700,
+            borderRadius: "8px",
+            padding: "10px 0",
+          }}
+        >
+          button Text
+        </Button>
+      </Box>
     </>
   );
 }
