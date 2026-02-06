25e4e6ac3e18fb79af5adb644bae87bab6744636
Tristan Labelle <github@tristanlabelle.com>
2026-02-06 20:18:59 +0800

    can_notify_deleted_user

--- app/backend/src/couchers/notifications/utils.py
+++ app/backend/src/couchers/notifications/utils.py
@@ -4,4 +4,13 @@ enum_from_topic_action: dict[tuple[str, str], NotificationTopicAction] = {
     (item.topic, item.action): item for item in NotificationTopicAction
 }
 
-ACCOUNT_DELETION_TOPIC = NotificationTopicAction.account_deletion__start.topic
+
+_DELETED_USER_NOTIFICATIONS = {
+    NotificationTopicAction.account_deletion__start,
+    NotificationTopicAction.account_deletion__complete,
+    NotificationTopicAction.account_deletion__recovered,
+}
+
+
+def can_notify_deleted_user(topic_action: NotificationTopicAction) -> bool:
+    return topic_action in _DELETED_USER_NOTIFICATIONS
