72cb04841a51442abddc8abd6eb5edf69ec65ade
Nicole <nabramow@gmail.com>
2026-02-22 03:07:03 +0000

    Update app/backend/src/couchers/migrations/versions/981fb62b20ce_add_public_trips.py
    Co-authored-by: couchersbot[bot] <179870305+couchersbot[bot]@users.noreply.github.com>

--- app/backend/src/couchers/migrations/versions/981fb62b20ce_add_public_trips.py
+++ app/backend/src/couchers/migrations/versions/981fb62b20ce_add_public_trips.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "981fb62b20ce"
-down_revision = "e9190b051324"
+down_revision = "0138"
 branch_labels = None
 depends_on = None
 
