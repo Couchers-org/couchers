95c45a9bf8bbca0ad3d8307bc1153c30cafa6198
Alexey <forestbiiird@gmail.com>
2026-02-24 17:35:29 +0000

    TEST

--- app/web/pages/badges/index.tsx
+++ app/web/pages/badges/index.tsx
@@ -5,6 +5,7 @@ import { GetStaticProps } from "next";
 import nextI18nextConfig from "next-i18next.config";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(
