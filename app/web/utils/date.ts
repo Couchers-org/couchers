380dbea480cfaa1a6c869225c64612cca107614e
Tristan Labelle <github@tristanlabelle.com>
2026-02-28 17:02:03 +0800

    Localize event date/times

--- app/web/utils/date.ts
+++ app/web/utils/date.ts
@@ -1,8 +1,10 @@
 // format a date
 import { Timestamp } from "google-protobuf/google/protobuf/timestamp_pb";
 
-import { Dayjs } from "./dayjs";
+import daysjs, { Dayjs } from "./dayjs";
 import { dayMillis } from "./timeAgo";
+import { TFunction } from "i18next";
+import { ConstructionOutlined } from "@mui/icons-material";
 
 const monthFormatter = (locale: string) =>
   new Intl.DateTimeFormat(locale, {
@@ -29,8 +31,118 @@ const numNights = (date1: string, date2: string) => {
   return diffDays;
 };
 
+/// Localizes a date, optionally with a time and day of week.
+function localizeDate(
+  date: Date | Dayjs,
+  locale: string,
+  options: {
+    includeDayOfWeek?: boolean;
+    includeTime?: boolean;
+    includeSeconds?: boolean;
+    long?: boolean;
+  } = {},
+): string {
+  let intlOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: options.long ? "long" : "short",
+    day: "numeric",
+  };
+  if (options.includeDayOfWeek) {
+    intlOptions.weekday = options.long ? "long" : "short";
+  }
+  if (options.includeTime) {
+    intlOptions.hour = "numeric";
+    intlOptions.minute = "numeric";
+    if (options.includeSeconds) {
+      intlOptions.second = "numeric";
+    }
+  }
+  if (daysjs.isDayjs(date)) {
+    const anyDate: any = date;
+    const timezone: string | undefined = anyDate?.$x?.timezone; // Daysjs stores its timezone here
+    if (timezone) {
+      intlOptions.timeZone = timezone;
+    }
+    date = date.toDate();
+  }
+  const format = Intl.DateTimeFormat(locale, intlOptions);
+  return format.format(date);
+}
+
+/// Localizes only the time component of a date.
+function localizeTime(
+  time: Date | Dayjs,
+  locale: string,
+  options: {
+    includeSeconds?: boolean;
+  } = {},
+): string {
+  let intlOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+  };
+  if (options.includeSeconds) {
+    intlOptions.second = "numeric";
+  }
+  if (daysjs.isDayjs(time)) {
+    intlOptions.timeZone = getDayjsTimezone(time);
+    time = time.toDate();
+  }
+  const format = Intl.DateTimeFormat(locale, intlOptions);
+  return format.format(time);
+}
+
+/// Localizes a range of dates as a string.
+function localizeDateTimeRange(
+  start: Dayjs,
+  end: Dayjs,
+  locale: string,
+  t: TFunction,
+  options: {
+    includeDayOfWeek?: boolean;
+    includeSeconds?: boolean;
+    long?: boolean;
+  } = {},
+): string {
+  if (isSameDate(start, end)) {
+    const stringKey =
+      "global:datetime_formats.datetime_range_sameday_" +
+      (options.long ? "long" : "short");
+    return t(stringKey, {
+      date: localizeDate(start, locale, {
+        includeDayOfWeek: options.includeDayOfWeek,
+        includeTime: false,
+        long: options.long,
+      }),
+      startTime: localizeTime(start, locale),
+      endTime: localizeTime(end, locale),
+    });
+  } else {
+    const stringKey =
+      "global:datetime_formats.datetime_range_multiday_" +
+      (options.long ? "long" : "short");
+    return t(stringKey, {
+      startDateTime: localizeDate(start, locale, {
+        includeDayOfWeek: options.includeDayOfWeek,
+        includeTime: true,
+        long: options.long,
+      }),
+      endDateTime: localizeDate(end, locale, {
+        includeDayOfWeek: options.includeDayOfWeek,
+        includeTime: true,
+        long: options.long,
+      }),
+    });
+  }
+}
+
 function timestamp2Date(timestamp: Timestamp.AsObject): Date {
   return new Date(Math.floor(timestamp.seconds * 1e3 + timestamp.nanos / 1e6));
+}
+
+function getDayjsTimezone(date: Dayjs): string | undefined {
+  // There is no API to get the value, but the state exists and impacts formatting.
+  return (date as any)?.$x?.timezone;
 }
 
 function isSameDate(date1: Dayjs, date2: Dayjs): boolean {
@@ -50,6 +162,9 @@ export {
   dateFormatter,
   dateTimeFormatter,
   isSameOrFutureDate,
+  localizeDate,
+  localizeDateTimeRange,
+  localizeTime,
   monthFormatter,
   numNights,
   timestamp2Date,
