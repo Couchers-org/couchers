2dcd76f3efb4c75db1e8589f2299391c9274f6d2
AlexVCS <alexcurtisslep@gmail.com>
2026-03-23 08:53:08 -0400

    comment out code to hopefully remove Vercel error

--- app/web/features/dashboard/ReminderCarousel.tsx
+++ app/web/features/dashboard/ReminderCarousel.tsx
@@ -70,7 +70,9 @@ export default function ReminderCarousel() {
           >
             {reminders.map((reminder, i) => (
               <Box key={i} sx={{ minWidth: "100%", flexShrink: 0 }}>
-                <ReminderItem data={reminder} />
+                <ReminderItem
+                // data={reminder}
+                />
               </Box>
             ))}
           </Box>
