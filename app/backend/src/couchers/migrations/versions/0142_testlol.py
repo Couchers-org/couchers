8ee4c411fd3f5dc56d5f350ca71f649f44bb2d6d
Alexey <forestbiiird@gmail.com>
2026-04-12 09:11:35 +0100

    Test asd

---
+++ app/backend/src/couchers/migrations/versions/0142_testlol.py
@@ -0,0 +1,27 @@
+"""Add mypostcard_job_id and rename country to country_code in postal_verification_attempts
+
+Revision ID: 0142
+Revises: 0141
+Create Date: 2026-03-18 12:00:00.000000
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0142"
+down_revision = "0141"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("postal_verification_attemptsasdf", sa.Column("mypostcard_job_id", sa.Integer(), nullable=True))
+    op.add_column("postal_verification_attemptsasdf", sa.Column("mypostcard_job_id", sa.Integer(), nullable=True))
+    op.alter_column("postal_verification_attemptsasdf", "country", new_column_name="country_code")
+
+
+def downgrade() -> None:
+    op.alter_column("postal_verification_attemptsasdf", "country_code", new_column_name="country")
+    op.drop_column("postal_verification_attemptsasdf", "mypostcard_job_id")
