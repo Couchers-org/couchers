b91895da3e7d4bd1793a7fc049bb856f984e22bc
Valeria <waleria.shadrina@gmail.com>
2026-04-28 12:37:44 +0200

    Add marginTop to StyledForm in ChangeEmail and ChangePassword components

--- app/web/features/auth/email/ChangeEmail.tsx
+++ app/web/features/auth/email/ChangeEmail.tsx
@@ -21,6 +21,7 @@ import { service } from "service";
 import { lowercaseAndTrimField } from "utils/validation";
 
 const StyledForm = styled("form")(({ theme }) => ({
+  marginTop: theme.spacing(2),
   marginBottom: theme.spacing(2),
   display: "flex",
   flexDirection: "column",
--- app/web/features/auth/password/ChangePassword.tsx
+++ app/web/features/auth/password/ChangePassword.tsx
@@ -34,6 +34,7 @@ interface ChangePasswordProps {
 }
 
 const StyledForm = styled("form")(({ theme }) => ({
+  marginTop: theme.spacing(2),
   marginBottom: theme.spacing(2),
   display: "flex",
   flexDirection: "column",
