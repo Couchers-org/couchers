afb8d6f5fa97ddd2de56520ef564a02da00e1f5f
Nicole Abramowski <nabramow@gmail.com>
2026-06-02 18:46:45 +0100

    Fix only_active and add is_statuses

--- app/backend/src/couchers/servicers/requests.py
+++ app/backend/src/couchers/servicers/requests.py
@@ -64,6 +64,8 @@ hostrequeststatus2api = {
     HostRequestStatus.cancelled: conversations_pb2.HOST_REQUEST_STATUS_CANCELLED,
 }
 
+api2hostrequeststatus = {v: k for k, v in hostrequeststatus2api.items()}
+
 hostrequestquality2sql = {
     requests_pb2.HOST_REQUEST_QUALITY_UNSPECIFIED: HostRequestQuality.high_quality,
     requests_pb2.HOST_REQUEST_QUALITY_LOW: HostRequestQuality.okay_quality,
@@ -464,7 +466,12 @@ class Requests(requests_pb2_grpc.RequestsServicer):
                     HostRequest.status == HostRequestStatus.confirmed,
                 )
             )
-            statement = statement.where(HostRequest.end_time <= func.now())
+            statement = statement.where(HostRequest.end_time >= func.now())
+
+        if request.is_statuses:
+            statement = statement.where(
+                HostRequest.status.in_([api2hostrequeststatus[s] for s in request.is_statuses])
+            )
 
         statement = statement.order_by(Message.id.desc()).limit(pagination + 1)
         results = session.execute(statement).all()
