b2ccc9a72b9d1a128676d3ca797801dee65858e0
Darren Vong <darrenvong@users.noreply.github.com>
2026-06-04 23:17:54 +0100

    feat: make donation page publicly accessible

--- app/web/features/donations/constants.ts
+++ app/web/features/donations/constants.ts
@@ -1,3 +1,5 @@
 export const DONATIONS_BOX_CURRENCY = "$";
 export const DONATIONS_BOX_VALUES = [3, 5, 10, 15, 50, 100, 500];
 export const BENEFACTOR_EMAIL = "benefactor@couchers.org";
+export const SUPPORT_EMAIL = "support@couchers.org";
+export const ANONYMOUS_DONATION_MIN_AMOUNT = 50;
