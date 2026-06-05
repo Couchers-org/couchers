3ad5368950afa4b1f0247514fcec2e52ab7fd84e
Darren Vong <darrenvong@users.noreply.github.com>
2026-06-05 13:22:00 +0100

    Enable dev mode in non-prod environment to help debugging

--- app/web/features/experimentation/FeatureFlagProvider.tsx
+++ app/web/features/experimentation/FeatureFlagProvider.tsx
@@ -15,6 +15,7 @@ configureCache({ staleTTL: CACHE_STALE_TTL_MS });
 const growthbook = new GrowthBook({
   apiHost: process.env.NEXT_PUBLIC_GROWTHBOOK_API_HOST,
   clientKey: process.env.NEXT_PUBLIC_GROWTHBOOK_CLIENT_KEY,
+  enableDevMode: process.env.NEXT_PUBLIC_COUCHERS_ENV !== "prod",
   trackingCallback: (experiment, result) => {
     return reportExposure({
       experimentKey: experiment.key,
