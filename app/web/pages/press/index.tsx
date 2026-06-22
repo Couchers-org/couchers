46671b2a334b780fd603797ebb2a79c0245d750e
Valeria <waleria.shadrina@gmail.com>
2026-06-22 11:04:58 +0200

    Make Press page visible to unauthenticated users

--- app/web/pages/press/index.tsx
+++ app/web/pages/press/index.tsx
@@ -1,4 +1,5 @@
 import { appGetLayout } from "components/AppRoute";
+import Press from "features/press/Press";
 import {
   CONNECTIONS,
   DASHBOARD,
@@ -10,8 +11,6 @@ import {
 import { GetStaticProps } from "next";
 import nextI18nextConfig from "next-i18next.config";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
-import Press from "../../features/press/Press";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
@@ -35,4 +34,7 @@ export default function PressPage() {
   return <Press />;
 }
 
-PressPage.getLayout = appGetLayout();
+PressPage.getLayout = appGetLayout({
+  isPrivate: false,
+  bottomMargin: "80px",
+});
