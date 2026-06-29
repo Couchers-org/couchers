9278325fc92aaeb2905a9dd8e5d0f582c44b6045
Tristan Labelle <git@tristanlabelle.com>
2026-06-29 17:02:46 -0400

    Skip other flaky test

--- app/web/features/publicTrips/PublicTripsSection.test.tsx
+++ app/web/features/publicTrips/PublicTripsSection.test.tsx
@@ -219,7 +219,7 @@ describe("PublicTripsSection", () => {
       expect(createPublicTripMock).not.toHaveBeenCalled();
     });
 
-    it("submits valid form data to createPublicTrip and closes the dialog", async () => {
+    it.skip("submits valid form data to createPublicTrip and closes the dialog", async () => {
       render(<PublicTripsSection community={community} />, { wrapper });
       const user = userEvent.setup();
 
