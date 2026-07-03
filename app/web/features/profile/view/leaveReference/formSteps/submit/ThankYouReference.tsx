52d3c226721ec4736e4b0d9a5b2182b443e05f6f
Tristan Labelle <github@tristanlabelle.com>
2026-07-03 08:03:06 -0400

    Frontend/i18n: Remove inline <Trans> text (#9211)

--- app/web/features/profile/view/leaveReference/formSteps/submit/ThankYouReference.tsx
+++ app/web/features/profile/view/leaveReference/formSteps/submit/ThankYouReference.tsx
@@ -32,17 +32,17 @@ const ThankYouReference = () => {
         {t("profile:leave_reference.thank_you_references_available")}
       </Typography>
       <Typography sx={{ marginTop: theme.spacing(3) }}>
-        <Trans i18nKey="profile:leave_reference.thank_you_donation_prompt">
-          Consider{" "}
-          <StyledLink
-            href={`${donationsRoute}?utm_source=leave-reference-thank-you`}
-            sx={{ fontWeight: 600 }}
-          >
-            making a donation
-          </StyledLink>
-          ! Couchers.org is a 501(c)(3) non-profit organization and we need
-          donations to keep our servers running
-        </Trans>
+        <Trans
+          i18nKey="profile:leave_reference.thank_you_donation_prompt"
+          components={{
+            2: (
+              <StyledLink
+                href={`${donationsRoute}?utm_source=leave-reference-thank-you`}
+                sx={{ fontWeight: 600 }}
+              />
+            ),
+          }}
+        />
       </Typography>
       <Typography sx={{ marginTop: theme.spacing(3) }}>
         {t("profile:leave_reference.thank_you_support")}
