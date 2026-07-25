96e59cb5aa6b5c00bed2163caec9d9bc26e153c8
Tristan Labelle <github@tristanlabelle.com>
2026-07-25 14:24:11 -0400

    match -> search

--- app/backend/src/tests/test_events.py
+++ app/backend/src/tests/test_events.py
@@ -1985,7 +1985,7 @@ def test_GetEventCalendarFile(db, moderator: Moderator):
         assert "DESCRIPTION:Dummy content." in ics_string
         assert "LOCATION:Near Null Island" in ics_string
         assert "STATUS:CANCELLED" not in ics_string
-        pre_cancel_sequence = int(re.match(r"SEQUENCE:(\d+)", ics_string).group(1))
+        pre_cancel_sequence = int(re.search(r"SEQUENCE:(\d+)", ics_string).group(1))
 
         api.CancelEvent(events_pb2.CancelEventReq(event_id=event_id))
 
@@ -1993,7 +1993,7 @@ def test_GetEventCalendarFile(db, moderator: Moderator):
         ics_string = file_res.data.decode("utf-8")
         assert "SUMMARY:Dummy Title" in ics_string  # Other props have not changed
         assert "STATUS:CANCELLED" in ics_string
-        post_cancel_sequence = int(re.match(r"SEQUENCE:(\d+)", ics_string).group(1))
+        post_cancel_sequence = int(re.search(r"SEQUENCE:(\d+)", ics_string).group(1))
         # Ideally the sequence number are strictly ascending, but they are based on timestamps so in tests they could be equal.
         assert post_cancel_sequence >= pre_cancel_sequence
 
