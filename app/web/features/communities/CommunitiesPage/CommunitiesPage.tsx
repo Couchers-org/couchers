d16ba4aa88820f04979aa88fc56c94672c084c1e
nc225 <nnc11225@gmail.com>
2026-08-02 10:32:36 -0400

    clean up en.json

--- app/web/features/communities/CommunitiesPage/CommunitiesPage.tsx
+++ app/web/features/communities/CommunitiesPage/CommunitiesPage.tsx
@@ -9,6 +9,7 @@ import { Trans, useTranslation } from "i18n";
 import { DASHBOARD, GLOBAL } from "i18n/namespaces";
 import { useState } from "react";
 import { communityCreationFormURL, helpCenterCommunityBuilderURL } from "routes";
+
 import NewCommunities from "../NewCommunities";
 import CommunitySearch from "./CommunitySearch";
 
