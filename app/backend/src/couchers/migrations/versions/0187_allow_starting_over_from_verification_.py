e3a0bf0a5575729f30813cdecb9044488ff41437
nc225 <nnc11225@gmail.com>
2026-08-23 23:57:40 -0400

    update migration files

--- app/backend/src/couchers/migrations/versions/0187_allow_starting_over_from_verification_.py
+++ app/backend/src/couchers/migrations/versions/0187_allow_starting_over_from_verification_.py
@@ -15,18 +15,39 @@ down_revision = "0186"
 branch_labels = None
 depends_on = None
 
-
 def upgrade() -> None:
-    op.add_column("signup_flows", sa.Column("signup_cancelled", sa.Boolean(), nullable=False))
-    op.drop_constraint(op.f("uq_signup_flows_username"), "signup_flows", type_="unique")
+    op.add_column(
+        "signup_flows",
+        sa.Column(
+            "signup_cancelled",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.false(),
+        ),
+    )
+
+    op.drop_constraint(
+        op.f("uq_signup_flows_username"),
+        "signup_flows",
+        type_="unique",
+    )
+
     op.create_index(
         "uq_signup_flows_username",
         "signup_flows",
         ["username"],
         unique=True,
-        postgresql_where=sa.text("username IS NOT NULL AND signup_cancelled = false"),
+        postgresql_where=sa.text(
+            "username IS NOT NULL AND signup_cancelled = false"
+        ),
     )
 
+    op.alter_column(
+        "signup_flows",
+        "signup_cancelled",
+        server_default=None,
+    )
+    
 
 def downgrade() -> None:
     op.drop_index(
