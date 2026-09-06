ead52689640a50c20adf66778a32652125a7f68c
Tristan Labelle <github@tristanlabelle.com>
2026-09-06 11:30:43 -0400

    generate_protos.sh vs ci_generate_protos.sh

--- app/ci_generate_protos.sh
+++ app/ci_generate_protos.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Generates protobuf bindings and copies them to all source trees that need them.
+# Assumes this is run by CI inside of the GRPC container.
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
-make -C "$SCRIPT_DIR/proto"
+"$SCRIPT_DIR/proto/generate.sh"
 "$SCRIPT_DIR/copy_generated_protos.sh"
 
 echo "OK"
