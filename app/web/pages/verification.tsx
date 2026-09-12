a55dec88a91955c5b395de1747543513c7ac7f0e
Nicole Abramowski <nabramow@gmail.com>
2026-09-12 22:50:51 +0100

    Update the translation namespaces after latest merge

--- app/web/pages/verification.tsx
+++ app/web/pages/verification.tsx
@@ -1,12 +1,13 @@
 import { appGetLayout } from "components/AppRoute";
 import Verification from "features/auth/verification/VerificationPage";
 import { appServerSideTranslations } from "i18n/appServerSideTranslations";
-import { AUTH, GLOBAL } from "i18n/namespaces";
+import { DEFAULT_LOCALE } from "i18n/locales";
+import { AUTH } from "i18n/namespaces";
 import { GetStaticProps } from "next";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
-    ...(await appServerSideTranslations(locale ?? "en", [AUTH, GLOBAL])),
+    ...(await appServerSideTranslations(locale ?? DEFAULT_LOCALE, [AUTH])),
   },
 });
 
