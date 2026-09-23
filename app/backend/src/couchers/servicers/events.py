7f6f0fb0f105143121736d4f9b6c111168d1b388
nc225 <nnc11225@gmail.com>
2026-09-23 13:34:22 -0400

    simplifying community invite requested backend

--- app/backend/src/couchers/servicers/events.py
+++ app/backend/src/couchers/servicers/events.py
@@ -102,11 +102,7 @@ def _community_invite_requested(session: Session, occurrence: EventOccurrence, u
     return (
         session.execute(
             select(EventCommunityInviteRequest.id)
-            .join(
-                EventOccurrence,
-                EventOccurrence.id == EventCommunityInviteRequest.occurrence_id,
-            )
-            .where(EventOccurrence.event_id == occurrence.id)
+            .where(EventCommunityInviteRequest.occurrence_id == occurrence.id)
             .where(EventCommunityInviteRequest.user_id == user_id)
             .limit(1)
         ).scalar_one_or_none()
@@ -154,7 +150,7 @@ def event_to_pb(session: Session, occurrence: EventOccurrence, context: Couchers
 
     can_moderate = _can_moderate_event(session, event, context.user_id)
     can_edit = _can_edit_event(session, event, context.user_id)
-    community_invite_requested = _community_invite_requested(session, event, context.user_id) if can_edit else False
+    community_invite_requested = _community_invite_requested(session, occurrence, context.user_id) if can_edit else False
 
     going_count = session.execute(
         where_users_column_visible(
